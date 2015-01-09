10546850aff64fa015a36975e3ed809db00f37a8
guochen <guobao.chen@oracle.com>
2015-01-09 17:16:25 +0800

    add error handling for dashboards

---
+++ dashboards-core/src/main/java/oracle/sysman/emaas/platform/dashboards/core/DashboardErrorConstants.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2015 Oracle
+ * All rights reserved.
+ *
+ * $$File: $$
+ * $$DateTime: $$
+ * $$Author: $$
+ * $$Revision: $$
+ */
+
+package oracle.sysman.emaas.platform.dashboards.core;
+
+/**
+ * @author guobaochen
+ */
+public class DashboardErrorConstants
+{
+	public static final Integer ERROR_CODE_UI_ERROR = 10000;
+	public static final Integer ERROR_CODE_DASHBOARD_SAME_NAME = 10001;
+
+	public static final Integer ERROR_CODE_RESOURCE_ERROR = 20000;
+	public static final Integer ERROR_CODE_DASHBOARD_NOT_FOUND = 20001;
+
+	public static final Integer ERROR_CODE_SECURITY_ERROR = 30000;
+	public static final Integer ERROR_CODE_DASHBOARD_ACCESS_NOT_ALLOWED = 30001;
+	public static final Integer ERROR_CODE_NOT_DELETE_SYSTEM_DASHBOARD = 30002;
+}
