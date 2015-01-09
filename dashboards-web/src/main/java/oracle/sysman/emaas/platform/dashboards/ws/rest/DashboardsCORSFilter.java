7600aa71b8bc0390526b554290e9e42d76b956af
wenjzhu <wenjie.zhu@oracle.com>
2015-01-09 16:06:13 +0800

    reft dev env

---
+++ dashboards-web/src/main/java/oracle/sysman/emaas/platform/dashboards/ws/rest/DashboardsCORSFilter.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2015 Oracle
+ * All rights reserved.
+ *
+ * $$File: $$
+ * $$DateTime: $$
+ * $$Author: $$
+ * $$Revision: $$
+ */
+
+package oracle.sysman.emaas.platform.dashboards.ws.rest;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Support across domain access CORS: Cross-Origin Resource Sharing Reference: http://enable-cors.org/ http://www.w3.org/TR/cors/
+ * http://en.wikipedia.org/wiki/Cross-origin_resource_sharing
+ *
+ * @author miayu
+ */
+public class DashboardsCORSFilter implements Filter
+{
+	@Override
+	public void destroy()
+	{
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+	ServletException
+	{
+		HttpServletResponse hRes = (HttpServletResponse) response;
+		hRes.addHeader("Access-Control-Allow-Origin", "*");
+		hRes.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); //add more methods as necessary
+		hRes.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException
+	{
+	}
+
+}
