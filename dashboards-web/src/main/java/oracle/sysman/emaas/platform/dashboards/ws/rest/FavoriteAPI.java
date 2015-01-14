42aaf2d05f22b395ca1de8677489346aab9fdbdc
wenjzhu <wenjie.zhu@oracle.com>
2015-01-14 15:12:46 +0800

    find all favorites

--- dashboards-web/src/main/java/oracle/sysman/emaas/platform/dashboards/ws/rest/FavoriteAPI.java
+++ dashboards-web/src/main/java/oracle/sysman/emaas/platform/dashboards/ws/rest/FavoriteAPI.java
@@ -10,7 +10,16 @@
 
 package oracle.sysman.emaas.platform.dashboards.ws.rest;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import oracle.sysman.emaas.platform.dashboards.core.DashboardManager;
+import oracle.sysman.emaas.platform.dashboards.core.model.Dashboard;
 
 /**
  * @author wenjzhu
@@ -22,6 +31,15 @@ public class FavoriteAPI extends APIBase
 	public FavoriteAPI()
 	{
 		super();
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAllFavoriteDashboards()
+	{
+		DashboardManager manager = DashboardManager.getInstance();
+		List<Dashboard> pd = manager.getFavoriteDashboards(getTenantId());
+		return Response.ok(getJsonUtil().toJson(pd)).build();
 	}
 
 }
