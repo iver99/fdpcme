817b7124a3163f5cc6ef52f4725f4ccf7ea7237c
Sachin Dhamdhere <sachin.dhamdhere@oracle.com>
2015-01-22 21:28:09 -0800

    Forgot to update metadata rb with name change

--- dashboards-chef/dashboardService-api/metadata.rb
+++ dashboards-chef/dashboardService-api/metadata.rb
@@ -1,4 +1,4 @@
-name             'dashboardService'
+name             'dashboardService-api'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
--- dashboards-chef/dashboardService-ui/metadata.rb
+++ dashboards-chef/dashboardService-ui/metadata.rb
@@ -1,4 +1,4 @@
-name             'dashboardUI'
+name             'dashboardService-ui'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
