d1c809b78e785634b5fbacb74f5a1e305fddc8ba
Miao Yu <miao.yu@oracle.com>
2015-02-26 21:48:40 +0800

    remove registry url from common bar parameters (home page only now)

--- dashboards-ui/dashboardsui-web/public_html/js/homeMain.js
+++ dashboards-ui/dashboardsui-web/public_html/js/homeMain.js
@@ -142,7 +142,7 @@ require(['dbs/dbsmodel',
  
             function HeaderViewModel() {
                 var self = this;
-                self.registryUrl = dfu.getRegistryUrl();//"http://adc00pos.us.oracle.com:7001/registry/servicemanager/registry/v1/";
+//                self.registryUrl = dfu.getRegistryUrl();//"http://adc00pos.us.oracle.com:7001/registry/servicemanager/registry/v1/";
 //                self.authToken = dfu.getAuthToken();//"Basic d2VibG9naWM6d2VsY29tZTE=";
                 self.userName = dfu.getUserName();
                 self.tenantName = dfu.getTenantName();
