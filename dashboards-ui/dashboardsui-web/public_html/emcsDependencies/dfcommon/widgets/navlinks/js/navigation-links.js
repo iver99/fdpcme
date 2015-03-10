59a8ad86864f55caf83fae0a1ae2a015a2845db4
alvin.duan <alvin.duan@oracle.com>
2015-03-10 10:59:05 +0800

    Clean up codes for branding bar dropdown links.

--- dashboards-ui/dashboardsui-web/public_html/emcsDependencies/dfcommon/widgets/navlinks/js/navigation-links.js
+++ dashboards-ui/dashboardsui-web/public_html/emcsDependencies/dfcommon/widgets/navlinks/js/navigation-links.js
@@ -2,22 +2,11 @@ define(['knockout', 'jquery', '../../../js/util/df-util'],
         function (ko, $, dfumodel) {
             function NavigationLinksViewModel(params) {
                 var self = this;
-//                var quickLinkList = [];
-//                var recentList = [];
-//                var favoriteList = [];
-//                var adminList = [];
-//                var analyzerList = [];
                 var dfHomeUrl = null;
-//                var maxRecentSize = 10;
-//                var maxFavoriteSize = 5;
                 var userName = $.isFunction(params.userName) ? params.userName() : params.userName;
                 var tenantName = $.isFunction(params.tenantName) ? params.tenantName() : params.tenantName;
-//                var dashboardUrl = null;
                 var dfu = new dfumodel(userName, tenantName);
                 
-//                self.quickLinks = ko.observableArray();
-//                self.favorites = ko.observableArray();
-//                self.recents = ko.observableArray();
                 self.adminLinks = ko.observableArray();
                 self.visualAnalyzers = ko.observableArray();
                 
@@ -37,10 +26,6 @@ define(['knockout', 'jquery', '../../../js/util/df-util'],
                 self.refresh = function() {
                     refreshLinks();
                 };
-                
-//                function getDashboardOpenLink(dashboardId) {
-//                    return document.location.protocol + '//' + document.location.host + '/emsaasui/emcpdfui/builder.html?dashboardId=' + dashboardId;
-//                };
                 
                 self.truncateString =function(str, length) {
                     if (str && length > 0 && str.length > length)
@@ -70,9 +55,6 @@ define(['knockout', 'jquery', '../../../js/util/df-util'],
                 */
                 function discoverLinks() {
                     var fetchServiceLinks = function(data) {
-//                        if (data.quickLinks && data.quickLinks.length > 0) {
-//                            self.quickLinks(data.quickLinks);
-//                        }
                         if (data.visualAnalyzers && data.visualAnalyzers.length > 0) {
                             var analyzers = data.visualAnalyzers;
                             var analyzerList = [];
@@ -94,7 +76,6 @@ define(['knockout', 'jquery', '../../../js/util/df-util'],
                         },
                         error: function(xhr, textStatus, errorThrown){
                             console.log('Failed to get service instances by URL: '+serviceUrl);
-//                            self.quickLinks(quickLinkList);
                             self.visualAnalyzers([]);
                             self.adminLinks([]);
                         },
@@ -103,66 +84,12 @@ define(['knockout', 'jquery', '../../../js/util/df-util'],
                 };
                 
                 function refreshLinks() {
-//                    recentList = [];
-//                    favoriteList = [];
-//                    analyzerList = [];
-//                    adminList = [];
                     dfHomeUrl = dfu.discoverDFHomeUrl();
                     
                     //Fetch available quick links and administration links from service manager registry
                     if (self.visualAnalyzers().length === 0 || self.adminLinks().length === 0) {
                         discoverLinks();
                     }
-                    
-//                    //Fetch favorite dashboards
-//                    if (dashboardUrl === null)
-//                        dashboardUrl = dfu.discoverDFRestApiUrl();
-//                    if (dashboardUrl && dashboardUrl !== '') {
-//                        var favoritesUrl = dashboardUrl + 'dashboards/favorites';
-//                        var favoriteCnt = 0;
-//                        $.ajax({
-//                            url: favoritesUrl,
-//                            headers: dfu.getDashboardsRequestHeader(),
-//                            success: function(data, textStatus) {
-//                                if (data && data instanceof Array) {
-//                                    for (i = 0; i < data.length && favoriteCnt < maxFavoriteSize; i++) {
-//                                        data[i].href = getDashboardOpenLink(data[i].id);
-//                                        favoriteList.push(data[i]);
-//                                        favoriteCnt++;
-//                                    }
-//                                }
-//                                self.favorites(favoriteList);
-//                            },
-//                            error: function(xhr, textStatus, errorThrown){
-//                                console.log('Error when fetching favorite dashboards!');
-//                                self.favorites(favoriteList);
-//                            },
-//                            async: true
-//                        });
-//                        
-//                        //Fetch recent dashboards 
-//                        var recentDashboardsUrl = dashboardUrl + 'dashboards';
-//                        var recentCnt = 0;
-//                        $.ajax({
-//                            url: recentDashboardsUrl,
-//                            headers: dfu.getDashboardsRequestHeader(),
-//                            success: function(data, textStatus) {
-//                                if (data && data.dashboards instanceof Array) {
-//                                    for (i = 0; i < data.dashboards.length && recentCnt < maxRecentSize; i++) {
-//                                        data.dashboards[i].href = getDashboardOpenLink(data.dashboards[i].id);
-//                                        recentList.push(data.dashboards[i]);
-//                                        recentCnt++;
-//                                    }
-//                                }
-//                                self.recents(recentList);
-//                            },
-//                            error: function(xhr, textStatus, errorThrown){
-//                                console.log('Error when fetching recent dashboards!');
-//                                self.recents(recentList);
-//                            },
-//                            async: true
-//                        });
-//                    }
                 };                          
             }
             return NavigationLinksViewModel;
