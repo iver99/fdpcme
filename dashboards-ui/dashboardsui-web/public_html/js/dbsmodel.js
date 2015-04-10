16ca292e877b60dd72801f9837981e5699ace4bf
wenjzhu <wenjie.zhu@oracle.com>
2015-04-10 16:23:22 +0800

    EMCPDF-462

--- dashboards-ui/dashboardsui-web/public_html/js/dbsmodel.js
+++ dashboards-ui/dashboardsui-web/public_html/js/dbsmodel.js
@@ -115,7 +115,7 @@ function(dsf, oj, ko, $, dfu, pfu)
                     }
                 },
                 error: function() {
-                    if (console.log) console.log("Preference to Show Welcome Dialog is not set. The defualt value 'true' is applied.");
+                    oj.Logger.info("Preference of Show Welcome Dialog is not set. The defualt value 'true' is applied.");
                 }
             });
         })();
@@ -185,6 +185,9 @@ function(dsf, oj, ko, $, dfu, pfu)
                         $('#cbtn-tooltip').ojPopup('open', "#cbtn");
                     }
                 }
+            },
+            'error': function(jqXHR, textStatus, errorThrown) {
+                oj.Logger.error("Error when fetching data for paginge data source. " + (jqXHR ? jqXHR.responseText : ""));
             }
         } );
                 
@@ -216,8 +219,12 @@ function(dsf, oj, ko, $, dfu, pfu)
                             var _m = "";
                             if (jqXHR && jqXHR[0] && jqXHR[0].responseJSON && jqXHR[0].responseJSON.errorMessage)
                             {
-                                _m = jqXHR[0].responseJSON.errorMessage;
+                                 _m = jqXHR[0].responseJSON.errorMessage;
+                            }else if (jqXHR && jqXHR.responseJSON && jqXHR.responseJSON.errorMessage)
+                            {
+                                _m = jqXHR.responseJSON.errorMessage;
                             }
+                            oj.Logger.error("Error when deleting dashboard. " + (jqXHR ? jqXHR.responseText : ""));
                             self.confirmDialogModel.show(getNlsString('COMMON_TEXT_ERROR'), getNlsString('COMMON_BTN_OK'), 
                                     getNlsString('DBS_HOME_CFM_DLG_DELETE_DSB_ERROR') + " " +_m,
                                     function () {self.confirmDialogModel.close();});
@@ -283,7 +290,15 @@ function(dsf, oj, ko, $, dfu, pfu)
                             var _m = getNlsString('COMMON_SERVER_ERROR');
                             if (jqXHR && jqXHR[0] && jqXHR[0].responseJSON && jqXHR[0].responseJSON.errorMessage)
                             {
-                                _m = jqXHR[0].responseJSON.errorMessage;
+                                 _m = jqXHR[0].responseJSON.errorMessage;
+                            }else if (jqXHR && jqXHR.responseJSON && jqXHR.responseJSON.errorMessage)
+                            {
+                                _m = jqXHR.responseJSON.errorMessage;
+                            }
+                            else
+                            {
+                                // a server error record
+                                 oj.Logger.error("Error when creating dashboard. " + (jqXHR ? jqXHR.responseText : ""));
                             }
                             _trackObj = new oj.InvalidComponentTracker();
                             self.tracker(_trackObj);
@@ -326,8 +341,9 @@ function(dsf, oj, ko, $, dfu, pfu)
                         var _e = $(".dbs-summary-container[aria-dashboard=\""+_id+"\"]");
                         if (_e && _e.length > 0) _e.dbsDashboardPanel("refresh");
                     },
-                    error: function() {
+                    error: function(jqXHR, textStatus, errorThrown) {
                         //console.log("Error on update dashboard");
+                        oj.Logger.error("Error when updating dashboard. " + (jqXHR ? jqXHR.responseText : ""));
                     }
                 });
             }
@@ -335,15 +351,7 @@ function(dsf, oj, ko, $, dfu, pfu)
         
         self.getDashboard = function (id)
         {
-            /*
-            if (id !== 0 && !id) return null;
-            for (var _i = 0 ; _i < self.dbsArray.length; _i++)
-            {
-                if (id === self.dbsArray[_i].id)
-                {
-                    return self.dbsArray[_i];
-                }
-            }*/
+           
         };
         
     };
