48359c9778dd3043d4ef96517bce7bba21a90426
alvin.duan <alvin.duan@oracle.com>
2015-04-10 15:09:59 +0800

    Catch errors and add logs for branding bar.

--- dashboards-ui/dashboardsui-web/public_html/emcsDependencies/dfcommon/js/util/df-util.js
+++ dashboards-ui/dashboardsui-web/public_html/emcsDependencies/dfcommon/js/util/df-util.js
@@ -1,8 +1,9 @@
 define(['knockout',
-        'jquery'
+        'jquery',
+        'ojs/ojcore'
     ],
     
-    function(ko, $)
+    function(ko, $, oj)
     {
         function DashboardFrameworkUtility(userName, tenantName) {
             var self = this;
@@ -30,12 +31,12 @@ define(['knockout',
              */
             self.buildFullUrl=function(root, path){
                 if (root===null || root===undefined){
-                    console.log("Warning: root is null, path="+path);
+                    oj.Logger.warn("Warning: root is null, path="+path);
                     return path;
                 }
                 
                 if (path===null || path===undefined){
-                    console.log("Warning: path is null, root="+root);
+                    oj.Logger.warn("Warning: path is null, root="+root);
                     return root;
                 }
                 
@@ -60,16 +61,15 @@ define(['knockout',
              * @param {String} serviceName
              * @param {String} version
              * @param {String} rel
-             * @param {String} smUrl
              * @returns {String} result
              */
             self.discoverUrl = function(serviceName, version, rel){
                 if (serviceName===null || serviceName===undefined){
-                    console.log("Error: Failed to discovery URL, serviceName="+serviceName);
+                    oj.Logger.error("Error: Failed to discovery URL, serviceName="+serviceName);
                     return null;
                 }
                 if (version===null || version===undefined){
-                    console.log("Error: Failed to discovery URL, version="+version);
+                    oj.Logger.error("Error: Failed to discovery URL, version="+version);
                     return null;
                 }
 
@@ -84,36 +84,37 @@ define(['knockout',
                         result = data;
                     },
                     error:function(xhr, textStatus, errorThrown){
-                        console.log("Error: URL not found due to error: "+textStatus);
+                        oj.Logger.error("Error: URL not found due to error: "+textStatus);
                     },
                     async:false
                 });
                 
                 if (result){
                     if (typeof rel==="string"){
-                        console.log("Link found by serviceName="+serviceName+", version="+version+", rel="+rel); 
+                        oj.Logger.info("Link found by serviceName="+serviceName+", version="+version+", rel="+rel);
                         return result.href;
                     }else{
-                        console.log("EndPoint found by serviceName="+serviceName+", version="+version+", rel="+rel);
+                        oj.Logger.info("EndPoint found by serviceName="+serviceName+", version="+version+", rel="+rel);
                         return result.href;
                     }
                 }
-                console.log("Warning: URL not found by serviceName="+serviceName+", version="+version+", rel="+rel); 
+                
+                oj.Logger.error("Error: URL not found by serviceName="+serviceName+", version="+version+", rel="+rel);
                 return null;
             };
 
             self.discoverLinkWithRelPrefix = function(serviceName, version, rel){
                 if (typeof serviceName!=="string"){
-                    console.log("Error: Failed to discovery Link (with Rel Prefix), serviceName="+serviceName);
+                    oj.Logger.error("Error: Failed to discovery Link (with Rel Prefix), serviceName="+serviceName);
                     return null;
                 }
                 if (typeof version!=="string"){
-                    console.log("Error: Failed to discovery Link (with Rel Prefix), version="+version);
+                    oj.Logger.error("Error: Failed to discovery Link (with Rel Prefix), version="+version);
                     return null;
                 }
 
                 if (typeof rel!=="string"){
-                    console.log("Error: Failed to discovery Link (with Rel Prefix), rel="+rel);
+                    oj.Logger.error("Error: Failed to discovery Link (with Rel Prefix), rel="+rel);
                     return null;                    
                 }
                 var result =null;
@@ -124,16 +125,16 @@ define(['knockout',
                         result = data;
                     },
                     error:function(xhr, textStatus, errorThrown){
-                        console.log("Error: Link (with Rel Prefix) not found due to error: "+textStatus);
+                        oj.Logger.error("Error: Link (with Rel Prefix) not found due to error: "+textStatus);
                     },
                     async:false
                 });
                 
                 if (result){
-                    console.log("Link (with Rel Prefix) found by serviceName="+serviceName+", version="+version+", rel="+rel); 
+                    oj.Logger.info("Link (with Rel Prefix) found by serviceName="+serviceName+", version="+version+", rel="+rel);
                     return result.href;
                 }
-                console.log("Warning: Link (with Rel Prefix) not found by serviceName="+serviceName+", version="+version+", rel="+rel); 
+                oj.Logger.error("Error: Link (with Rel Prefix) not found by serviceName="+serviceName+", version="+version+", rel="+rel);
                 return null;
             };
             
@@ -156,7 +157,7 @@ define(['knockout',
                 if (url){
                     return url;
                 }else{
-                    console.log("Failed to discover Dashboard Home");
+                    oj.Logger.error("Failed to discover Dashboard Home.");
                     return null;
                 }
             };    
@@ -172,7 +173,7 @@ define(['knockout',
                 if (url){
                     return url;
                 }else{
-                    console.log("Failed to discovery DF REST API end point");
+                    oj.Logger.error("Failed to discover DF REST API end point.");
                     return null;
                 }
             };
@@ -183,9 +184,10 @@ define(['knockout',
              */
             self.getDefaultHeader = function() {
                 var defHeader = {
+//                    'Authorization': 'Basic d2VibG9naWM6d2VsY29tZTE=',
                     "X-USER-IDENTITY-DOMAIN-NAME":self.tenantName,
                     "X-REMOTE-USER":self.tenantName+'.'+self.userName};
-                console.log("Sent Header: "+JSON.stringify(defHeader));
+                oj.Logger.info("Sent Header: "+JSON.stringify(defHeader));
                 return defHeader;
             };
             
@@ -206,11 +208,11 @@ define(['knockout',
              */
             self.getSubscribedApplications = function() {
                 if (!self.tenantName) {
-                    console.error("Specified tenant name is empty, and the query won't be executed");
+                    oj.Logger.error("Specified tenant name is empty, and the query won't be executed.");
                     return null;
                 }
                 if (!self.userName) {
-                    console.error("Specified user name is empty, and the query won't be executed");
+                    oj.Logger.error("Specified user name is empty, and the query won't be executed.");
                     return null;
                 }
                 
@@ -227,12 +229,12 @@ define(['knockout',
                         result = data.applications;
                     },
                     error:function(xhr, textStatus, errorThrown){
-                        console.log("Failed to get subscribed applicatoins due to error: "+textStatus);
+                        oj.Logger.error("Failed to get subscribed applicatoins due to error: "+textStatus);
                     },
                     async:false
                 });
                 return result;
-            };
+            }; 
             
             /**
              * Check subscribed applications and call callback function with subscribed application names in an string array or null for none
@@ -242,16 +244,16 @@ define(['knockout',
              */
             self.checkSubscribedApplications = function(callbackFunc) {
                 if (!$.isFunction(callbackFunc)){
-                    console.error("invalid callback function: "+callbackFunc);
+                    oj.Logger.error("Invalid callback function: "+callbackFunc);
                     return;
                 } 
                 
                 if (!self.tenantName) {
-                    console.error("Specified tenant name is empty, and the query won't be executed");
+                    oj.Logger.error("Specified tenant name is empty, and the query won't be executed.");
                     return;
                 }
                 if (!self.userName) {
-                    console.error("Specified user name is empty, and the query won't be executed");
+                    oj.Logger.error("Specified user name is empty, and the query won't be executed.");
                     return;
                 }
                 
@@ -267,12 +269,31 @@ define(['knockout',
                         callbackFunc(data.applications);
                     },
                     error:function(xhr, textStatus, errorThrown){
-                        console.log("Failed to get subscribed applicatoins due to error: "+textStatus);
+                        oj.Logger.error("Failed to get subscribed applicatoins due to error: "+textStatus);
                         callbackFunc(null);
                     },
                     async:true
                 });
-            };            
+            };
+            
+            /**
+             * Get request header for Saved Search Framework API call
+             * @returns {Object} 
+             */
+            self.getSavedSearchServiceRequestHeader=function() {
+                return self.getDefaultHeader();
+            };
+            
+            /**
+             * Discover available Saved Search service URL
+             * @returns {String} url
+             */            
+            self.discoverSavedSearchServiceUrl = function() {
+//                return 'http://slc08upg.us.oracle.com:7001/savedsearch/v1/';
+//                return 'http://slc06wfs.us.oracle.com:7001/savedsearch/v1/';
+                return self.discoverUrl('SavedSearch', '0.1');
+            };
+            
         }
         
         return DashboardFrameworkUtility;
