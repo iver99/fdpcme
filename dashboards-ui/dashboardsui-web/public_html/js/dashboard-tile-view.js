c1784461b420567c08003225cbf2c3017bb53294
Guobao Chen <guobao.chen@oracle.com>
2015-04-13 13:43:00 +0800

    implement EMCPDF-435/479

--- dashboards-ui/dashboardsui-web/public_html/js/dashboard-tile-view.js
+++ dashboards-ui/dashboardsui-web/public_html/js/dashboard-tile-view.js
@@ -796,7 +796,7 @@ define(['knockout',
             };
             
             self.showAddWidgetTooltip = function() {
-                if (tilesViewModel.isEmpty()) {
+                if (tilesViewModel.isEmpty() && dashboard && dashboard.systemDashboard && !dashboard.systemDashboard()) {
                    $('#add-widget-tooltip').ojPopup('open', "#add-widget-button");
                 }
             };
