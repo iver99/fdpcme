1f66fb5e10b33b96efcec29b4cdf64838ba891ce
yic <charles.c.chen@oracle.com>
2015-04-15 01:46:32 -0700

    change to a new dir

--- uitest/src/test/java/oracle/sysman/emaas/platform/dashboards/test/ui/util/WidgetPageId.java
+++
@@ -1,8 +0,0 @@
-package oracle.sysman.emaas.platform.dashboards.test.ui.util;
-
-public class WidgetPageId {
-	
-	public static final String widgetName = "Listeners Overview";
-	public static final String searchBoxID = "searchTxt";
-	
-}
