0f248f868729b4a1f39458ee781fb638ce24c777
Guobao Chen <guobao.chen@oracle.com>
2015-04-16 15:16:32 +0800

    correct the issue that action buttong sometimes does not work

--- dashboards-ui/dashboardsui-web/public_html/js/dashboard-tile-model.js
+++ dashboards-ui/dashboardsui-web/public_html/js/dashboard-tile-model.js
@@ -644,22 +644,16 @@ define(['knockout',
                 for (var i = 0; i < self.tileRemoveCallbacks.length; i++) {
                     self.tileRemoveCallbacks[i]();
                 }
-                
-                self.postTileMenuClicked(tile);
             };
             
             self.broadenTile = function(tile) {
                 if (tile.width() <= 3)
                     tile.width(tile.width() + 1);
-                
-                self.postTileMenuClicked(tile);
             };
             
             self.narrowTile = function(tile) {
                 if (tile.width() > 1)
                     tile.width(tile.width() - 1);
-                
-                self.postTileMenuClicked(tile);
             };
             
             self.calculateTilesRowHeight = function() {
@@ -716,14 +710,6 @@ define(['knockout',
                 }
             };
             
-            self.postTileMenuClicked = function(tile) {
-                $("#tileMenu" + tile.clientGuid).hide();
-                if ($('#actionButton' + tile.clientGuid).hasClass('oj-selected')) {
-                    $('#actionButton' + tile.clientGuid).removeClass('oj-selected');
-                    $('#actionButton' + tile.clientGuid).addClass('oj-default');
-                }
-            };
-            
             self.maximize = function(tile) {
                 for (var i = 0; i < self.dashboard.tiles().length; i++) {
                     var eachTile = self.dashboard.tiles()[i];
@@ -738,8 +724,6 @@ define(['knockout',
                 self.tileOriginalHeight = $('.dbd-tile-maximized .dbd-tile-element').height();
                 $('.dbd-tile-maximized .dbd-tile-element').height(maximizedTileHeight);
                 $('#add-widget-button').ojButton('option', 'disabled', true);
-                
-                self.postTileMenuClicked(tile);
             };
             
             self.initializeMaximization = function() {
@@ -764,8 +748,6 @@ define(['knockout',
                 }
                 self.tilesView.enableSortable();
                 self.tilesView.enableDraggable();
-                
-                self.postTileMenuClicked(tile);
             };
             
             self.configure = function(tile){
@@ -777,8 +759,6 @@ define(['knockout',
             self.changeUrl = function(tile) {
                 urlEditView.setEditedTile(tile);
                 $('#urlChangeDialog').ojDialog('open');
-                
-                self.postTileMenuClicked(tile);
             };
             
             self.fireDashboardItemChangeEventTo = function (widget, dashboardItemChangeEvent) {
