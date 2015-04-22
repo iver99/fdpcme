4ff1e6eb87e8ae7d4ba896906e1a10056f79b736
yic <charles.c.chen@oracle.com>
2015-04-21 20:25:46 -0700

    use a new widget,since last one is not present

--- dashboards-uitest/src/test/java/oracle/sysman/emaas/platform/dashboards/test/ui/util/WidgetPageId.java
+++ dashboards-uitest/src/test/java/oracle/sysman/emaas/platform/dashboards/test/ui/util/WidgetPageId.java
@@ -2,7 +2,9 @@ package oracle.sysman.emaas.platform.dashboards.test.ui.util;
 
 public class WidgetPageId {
 	
-	public static final String widgetName = "Listeners Overview";
+	public static final String widgetName = "Top 10 Listeners by Load";//Listeners Overview";
 	public static final String searchBoxID = "searchTxt";
+	public static final String leftNavigatorBtnID="//div[@id='ojDialogWrapper-ui-id-13']/div[5]/button";
+	public static final String rightNavigatorBtnID="//div[@id='ojDialogWrapper-ui-id-13']/div[5]/button[2]";
 	
 }
