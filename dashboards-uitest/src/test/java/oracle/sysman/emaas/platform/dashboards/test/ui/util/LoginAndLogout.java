5a423d94ed0d97ade21ea24668cb0266c75cc590
yic <charles.c.chen@oracle.com>
2015-04-22 19:45:01 -0700

    replace with a new widget

--- dashboards-uitest/src/test/java/oracle/sysman/emaas/platform/dashboards/test/ui/util/LoginAndLogout.java
+++ dashboards-uitest/src/test/java/oracle/sysman/emaas/platform/dashboards/test/ui/util/LoginAndLogout.java
@@ -13,9 +13,9 @@ public class LoginAndLogout {
   public  void login(String testName,String username,String password,String tenantId,String rel,String servicename)
 	{
 		webd = WebDriverUtils.initWebDriver(testName);
-		String url= PageUtils.getServiceLink(tenantId,rel,servicename);
+		//String url= PageUtils.getServiceLink(tenantId,rel,servicename);
 		
-		//String url = "https://slc07hcn.us.oracle.com:4443/emsaasui/emcpdfui/home.html";
+		String url = "https://slc07hcn.us.oracle.com:4443/emsaasui/emcpdfui/home.html";
 		//String url = "https://slc07dgg.us.oracle.com:4443/emsaasui/emcpdfui/home.html";
 		//String url = "https://slc07ptb.us.oracle.com:4443/emsaasui/emcpdfui/home.html";
 		webd.getLogger().info("start to test in LoginAndOut");	
@@ -30,7 +30,7 @@ public class LoginAndLogout {
 		String tenantID = oracle.sysman.emsaas.login.utils.Utils.getProperty("TENANT_ID");
 		String username = oracle.sysman.emsaas.login.utils.Utils.getProperty("SSO_USERNAME");
 		//login(testName,username, "Welcome1!",tenantID, "home", "Dashboard-UI");
-		login(testName,"emcsadmin", "Welcome1!",tenantID, "home", "Dashboard-UI");
+		login(testName,"emaasadmin", "Welcome1!","TenantOPC1", "home", "Dashboard-UI");
 		
 	}
 	
