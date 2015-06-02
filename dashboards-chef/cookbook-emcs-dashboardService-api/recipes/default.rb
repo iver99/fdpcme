57a1e321a8add18ee01d5baf009064801f38a7f6
Sachin Dhamdhere <sachin.dhamdhere@oracle.com>
2015-06-02 06:07:36 -0700

    JIRA:EMCPDF-626
    Cutover to use common recipes for managed WLS server (instead of admin server)
    
    * Removed the ear deploy recipes which USED to perform datasource setup & app deploy - both functions are provided by common recipes from LCM team
    * Changed json to specify correct recipes for undeploy, start, stop operations

--- dashboards-chef/cookbook-emcs-dashboardService-api/recipes/default.rb
+++ dashboards-chef/cookbook-emcs-dashboardService-api/recipes/default.rb
@@ -1,12 +1,36 @@
 
-#Cookbook Name::dataService
+#Cookbook Name::dashboardboardService-api
 #
-# This recipe calls schema recipe to create the DB schema and datasource recipe to create datasource and deploy ear file in weblogic server
+# This recipe creates the service manager properties file & then calls common recipes to ...
+#			1. perform WLS managed server setup
+#			2. setup data source for this app
+#			3. perform app deploy to managed server setup in #1
+#	
+#	Refer https://confluence.oraclecorp.com/confluence/display/EMS/Cutover+to+deployment+on+WLS+Managed+Servers for details
 #
 
-#Recipe to create schema
+#Recipe to create schema - this is completely re-entrant, run as many times
 include_recipe 'cookbook-emcs-dashboardService-api::dashboard_schema'
 
-#Recipe to create Datasource
-include_recipe 'cookbook-emcs-dashboardService-api::dashboard_eardeploy'
+#Block to create the servicemanager.properties file
+bash "create_servicemanger_properties_file"  do
+  code <<-EOH
+		echo "Creating SM properties file in #{node["apps_dir"]}/#{node["SAAS_servicename"]}/init"
+    mkdir -p #{node["apps_dir"]}/#{node["SAAS_servicename"]}/init
+    cd #{node["apps_dir"]}/#{node["SAAS_servicename"]}/init
+    echo "version=#{node["SAAS_API_version"]}" > servicemanager.properties
+    echo "serviceName=Dashboard-API" >> servicemanager.properties
+    echo "registryUrls=$SAAS_REGISTRY_URLS" >> servicemanager.properties
+    echo "serviceUrls=$SAAS_REGISTRY_URLS" >> servicemanager.properties
+    echo "characteristics=SAAS_deploymentUuid:#{node["SAAS_deploymentUuid"]}" >> servicemanager.properties
+    EOH
+end
 
+#common setup/start for a managed Server
+include_recipe 'cookbook-emcs-emsaas-weblogic::managedServer_setup'
+
+#common datasource recipe to setup data source based on properties in ../attributes/default.rb
+include_recipe 'cookbook-emcs-emsaas-weblogic::datasource_common'
+
+# common app deployment to managed server
+include_recipe 'cookbook-emcs-emsaas-weblogic::managedServer_deployApp'
--- dashboards-chef/cookbook-emcs-dashboardService-ui/recipes/default.rb
+++ dashboards-chef/cookbook-emcs-dashboardService-ui/recipes/default.rb
@@ -1,10 +1,29 @@
-
-#Cookbook Name::dataService
 #
-# This recipe calls  recipe to deploy EAR
+# Cookbook Name::dashboardService-ui
+#
+# This recipe creates the service manager properties file & then calls common recipes to ...
+#			1. perform WLS managed server setup
+#			2. perform app deploy to managed server setup in #1
+#	
+#	Refer https://confluence.oraclecorp.com/confluence/display/EMS/Cutover+to+deployment+on+WLS+Managed+Servers for details
 #
 
+#Block to create the servicemanager.properties file
+bash "create_servicemanger_properties_file"  do
+  code <<-EOH
+		echo "Creating SM properties file in #{node["apps_dir"]}/#{node["SAAS_servicename"]}/init"
+    mkdir -p #{node["apps_dir"]}/#{node["SAAS_servicename"]}/init
+    cd #{node["apps_dir"]}/#{node["SAAS_servicename"]}/init
+    echo "version=#{node["SAAS_API_version"]}" > servicemanager.properties
+    echo "serviceName=Dashboard-UI" >> servicemanager.properties
+    echo "registryUrls=$SAAS_REGISTRY_URLS" >> servicemanager.properties
+    echo "serviceUrls=$SAAS_REGISTRY_URLS" >> servicemanager.properties
+    echo "characteristics=SAAS_deploymentUuid:#{node["SAAS_deploymentUuid"]}" >> servicemanager.properties
+    EOH
+end
 
-#Recipe to create Datasource
-include_recipe 'cookbook-emcs-dashboardService-ui::dashboard_eardeploy'
+#common setup/start for a managed Server
+include_recipe 'cookbook-emcs-emsaas-weblogic::managedServer_setup'
 
+# common app deployment to managed server
+include_recipe 'cookbook-emcs-emsaas-weblogic::managedServer_deployApp'
