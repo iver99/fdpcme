a9a6a076b1161c4a92e37104b26a1496fe6419cb
Vinay Joshi <vinay.joshi@oracle.com>
2015-06-10 00:24:53 -0700

    fixed service name

--- dashboards-chef/cookbook-emcs-dashboardService-api/recipes/schema_deployment.rb
+++ dashboards-chef/cookbook-emcs-dashboardService-api/recipes/schema_deployment.rb
@@ -1,5 +1,5 @@
 include_recipe 'cookbook-emcs-lcm-rep-manager-service::db_lookup'
 
-include_recipe 'cookbook-emcs-savedsearchService::dashboard_schema'
+include_recipe 'cookbook-emcs-dashboardService-api::dashboard_schema'
 
 include_recipe 'cookbook-emcs-lcm-rep-manager-service::lcmrepmgr_framework'
