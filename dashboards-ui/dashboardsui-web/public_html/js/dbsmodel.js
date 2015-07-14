88edb9f07ed0c79d6f4cdeb0f2ecc6351b787a7b
wenjzhu <wenjie.zhu@oracle.com>
2015-07-14 14:38:53 +0800

    popup focus update

--- dashboards-ui/dashboardsui-web/public_html/js/dbsmodel.js
+++ dashboards-ui/dashboardsui-web/public_html/js/dbsmodel.js
@@ -231,6 +231,14 @@ function(dsf, oj, ko, $, dfu, pfu)
             self.selectedDashboard(data);
             if (data.element)
             {
+                if (data.dashboard.systemDashboard == true)
+                {
+                    popup.ojPopup( "option", "initialFocus", "none" );
+                }
+                else
+                {
+                    popup.ojPopup( "option", "initialFocus", "firstFocusable" );
+                }
                 popup.ojPopup('open', data.element, {'at': 'right center', 'my': 'start center'});
             }
         };
