518179c1d66ff15cd60e3ea837672239aab61cd2
Sachin Dhamdhere <sachin.dhamdhere@oracle.com>
2015-07-20 12:41:44 -0700

    Introduce changes for new micro-service. Made following changes:
    * add recipe to deploy the app
    * update deploy_application.xml
    * add deploy-list.xml
    * add json to software-definitions

---
+++ dashboards-chef/cookbook-emcs-omc-ui-framework/metadata.rb
@@ -0,0 +1,9 @@
+name             'cookbook-emcs-omc-framework-ui'
+maintainer       'ORACLE'
+maintainer_email 'sachin.dhamdhere@oracle.com'
+license          'All rights reserved'
+description      'Deploys OMC (Oracle Management Cloud) UI framework application'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.3.0'
+depends "cookbook-emcs-emsaas-weblogic"
+
