424a6a4f6513c740ac085459256b4cb8934609bd
Miao Yu <miao.yu@oracle.com>
2015-08-18 00:28:41 +0800

    JIRA:EMCPDF-831 rebuild X-USER-IDENTITY-DOMAIN-NAME & X-REMOTE-USER headers according to OAM_REMOTE_USER header

--- dashboards-web/src/main/java/oracle/sysman/emaas/platform/dashboards/ws/rest/DashboardsCORSFilter.java
+++ dashboards-web/src/main/java/oracle/sysman/emaas/platform/dashboards/ws/rest/DashboardsCORSFilter.java
@@ -19,6 +19,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.logging.log4j.LogManager;
@@ -32,6 +33,43 @@ import org.apache.logging.log4j.Logger;
  */
 public class DashboardsCORSFilter implements Filter
 {
+	private static class OAMHttpRequestWrapper extends HttpServletRequestWrapper
+	{
+		private static final String OAM_REMOTE_USER_HEADER = "OAM_REMOTE_USER";
+		private static final String X_REMOTE_USER_HEADER = "X-REMOTE-USER";
+		private static final String X_USER_IDENTITY_DOMAIN_NAME_HEADER = "X-USER-IDENTITY-DOMAIN-NAME";
+		private final String oam_remote_user = null;
+		private String tenant = null;
+
+		public OAMHttpRequestWrapper(HttpServletRequest request)
+		{
+			super(request);
+			String oamRemoteUser = request.getHeader(OAM_REMOTE_USER_HEADER);
+			//oamRemoteUser could be null in dev mode. In dev mode, there is no OHS configured
+			if (oamRemoteUser != null) {
+				int pos = oamRemoteUser.indexOf(".");
+				if (pos > 0) {
+					tenant = oamRemoteUser.substring(0, pos);
+				}
+			}
+		}
+
+		@Override
+		public String getHeader(String name)
+		{
+			HttpServletRequest request = (HttpServletRequest) getRequest();
+			if (X_REMOTE_USER_HEADER.equals(name) && oam_remote_user != null) {
+				return oam_remote_user;
+			}
+			else if (X_USER_IDENTITY_DOMAIN_NAME_HEADER.equals(name) && tenant != null) {
+				return tenant;
+			}
+			else {
+				return request.getHeader(name);
+			}
+		}
+	}
+
 	private final Logger logger = LogManager.getLogger(DashboardsCORSFilter.class);
 
 	@Override
@@ -43,11 +81,12 @@ public class DashboardsCORSFilter implements Filter
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
 			ServletException
 	{
-	        // Only add CORS headers if the developer mode is enabled to add them
-	        if (!(new java.io.File("/var/opt/ORCLemaas/DEVELOPER_MODE-ENABLE_CORS_HEADERS").exists())) {
-		    chain.doFilter(request, response);
-		    return;
-	        }
+		// Only add CORS headers if the developer mode is enabled to add them
+		if (!new java.io.File("/var/opt/ORCLemaas/DEVELOPER_MODE-ENABLE_CORS_HEADERS").exists()) {
+			chain.doFilter(request, response);
+			logger.info("developer mode is NOT enabled on server side");
+			return;
+		}
 		HttpServletResponse hRes = (HttpServletResponse) response;
 		HttpServletRequest hReq = (HttpServletRequest) request;
 		hRes.addHeader("Access-Control-Allow-Origin", "*");
@@ -59,11 +98,12 @@ public class DashboardsCORSFilter implements Filter
 			// non-specific origin, cannot support cookies
 		}
 
-		hRes.addHeader("Access-Control-Allow-Methods", "HEAD, OPTIONS, GET, POST, PUT, DELETE"); //add more methods as necessary
+		hRes.addHeader("Access-Control-Allow-Methods", "HEAD, OPTIONS, GET, POST, PUT, DELETE"); // add more methods as
+		// necessary
 		hRes.addHeader("Access-Control-Allow-Headers",
 				"Origin, X-Requested-With, Content-Type, Accept,X-USER-IDENTITY-DOMAIN-NAME,X-REMOTE-USER,Authorization,x-sso-client");
-
-		chain.doFilter(request, response);
+		HttpServletRequest oamRequest = new OAMHttpRequestWrapper(hReq);
+		chain.doFilter(oamRequest, response);
 	}
 
 	@Override
