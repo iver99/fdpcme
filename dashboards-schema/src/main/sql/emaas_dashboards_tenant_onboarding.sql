482826456635e443b758bbce556679f79a4b4e9c
Miao Yu <miao.yu@oracle.com>
2015-08-26 02:51:20 -0700

    JIRA:EMCPDF-824 1.replace ./ with absolute path 2.change 1.0.15 to 1.0.20

--- dashboards-schema/src/main/sql/emaas_dashboards_tenant_onboarding.sql
+++ dashboards-schema/src/main/sql/emaas_dashboards_tenant_onboarding.sql
@@ -45,9 +45,9 @@ BEGIN
   END IF;
 END;
 /
-@./1.0.0/emaas_dashboards_seed_data.sql &TENANT_ID
+@&EMSAAS_SQL_ROOT/1.0.0/emaas_dashboards_seed_data.sql &TENANT_ID
 
-@./1.0.15/emaas_dashboards_seed_data.sql &TENANT_ID
+@&EMSAAS_SQL_ROOT/1.0.20/emaas_dashboards_seed_data.sql &TENANT_ID
 
 COMMIT;
 /
