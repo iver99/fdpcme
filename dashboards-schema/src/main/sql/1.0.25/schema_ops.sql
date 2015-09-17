c6e0415760d6e889f919470cbce9230044e05697
Miao Yu <miao.yu@oracle.com>
2015-09-17 22:08:44 +0800

    JIRA:EMCPDF-914

--- dashboards-schema/src/main/sql/1.0.25/schema_ops.sql
+++ dashboards-schema/src/main/sql/1.0.25/schema_ops.sql
@@ -7,7 +7,7 @@ Rem ----------------------------------------------------------------
 
 SET HEADING OFF
 SET FEEDBACK OFF
-SPOOL @&EMSAAS_SQL_ROOT/1.0.25/upgrade_impl_dml_tmp.sql
+SPOOL &EMSAAS_SQL_ROOT/1.0.25/upgrade_impl_dml_tmp.sql
 SELECT DISTINCT '@&EMSAAS_SQL_ROOT/1.0.25/upgrade_impl_dml.sql ' || TENANT_ID  FROM EMS_DASHBOARD ORDER BY '@&EMSAAS_SQL_ROOT/1.0.25/upgrade_impl_dml.sql ' || TENANT_ID ;
 SPOOL OFF
 
