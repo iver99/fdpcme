108271c4f9925da3cc5c1cb91079ee89946061d8
jiangxia.nan@oracle.com <jnan@slc03nuu.us.oracle.com>
2015-11-16 23:04:53 -0800

    add ITA OOB dashboard: Performance Analytics: Middleware Datasource

--- dashboards-ui/dashboardsui-web/public_html/js/welcomeMain.js
+++ dashboards-ui/dashboardsui-web/public_html/js/welcomeMain.js
@@ -128,6 +128,7 @@ require(['ojs/ojcore',
                 self.ITA_DB_Performance = getNlsString("LANDING_HOME_ITA_DB_PERFORMANCE");
                 self.ITA_DB_Resource = getNlsString("LANDING_HOME_ITA_DB_RESOURCE");
                 self.ITA_Middleware_Performance = getNlsString("LANDING_HOME_ITA_MIDDLEWARE_PERFORMANCE");
+                self.ITA_Middleware_Datasource_Performance = getNlsString("LANDING_HOME_ITA_MIDDLEWARE_DATASOURCE_PERFORMANCE");
                 self.ITA_Middleware_Resource = getNlsString("LANDING_HOME_ITA_MIDDLEWARE_RESOURCE");
                 self.ITA_Search = getNlsString("LANDING_HOME_ITA_SEARCH");
                 self.ITA_Analyze = getNlsString("LANDING_HOME_ITA_ANALYZE");
@@ -191,6 +192,7 @@ require(['ojs/ojcore',
                     landingHomeUrls["DB_perf"] = self.getITAVerticalAppUrl("verticalApplication.db-perf");
                     landingHomeUrls["DB_resource"] = self.getITAVerticalAppUrl("verticalApplication.db-resource");
                     landingHomeUrls["mw_perf"] = self.getITAVerticalAppUrl("verticalApplication.mw-perf");
+                    landingHomeUrls["mwds_perf"] = self.getITAVerticalAppUrl("verticalApplication.mwds-perf");
                     landingHomeUrls["mw_resource"] = self.getITAVerticalAppUrl("verticalApplication.mw-resource");
                     self.landingHomeUrls = landingHomeUrls;
                 };
