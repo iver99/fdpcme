0dd1376f35be43434ddd1bf9149c5243c417fe91
Caleb Wei <caleb.wei@oracle.com>
2015-11-30 03:10:28 -0800

    add data cleanup

---
+++ dashboards-web/src/integTest/resources/cleanup.sql
@@ -0,0 +1,6 @@
+delete from ems_dashboard_tile_params;
+delete from ems_dashboard_tile where dashboard_Id>1000;
+delete from ems_dashboard_last_access where dashboard_Id>1000;
+delete from ems_dashboard where dashboard_Id>1000;
+delete from ems_preference;
+commit;
