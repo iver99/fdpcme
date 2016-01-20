56126f9bf91583153fbc39cecfb6cdc22aee1257
johnson <johnson.s.shi@oracle.com>
2016-01-20 17:34:38 +0800

    JMockitCC- Add LoggingConfigAPITest and SceenShotEntityTest

--- dashboards-web/src/integTest/java/oracle/sysman/emaas/platform/dashboards/ws/rest/ScreenShotEntityTest.java
+++ dashboards-web/src/integTest/java/oracle/sysman/emaas/platform/dashboards/ws/rest/ScreenShotEntityTest.java
@@ -1,5 +1,7 @@
 package oracle.sysman.emaas.platform.dashboards.ws.rest;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
@@ -7,15 +9,32 @@ import org.testng.annotations.Test;
  */
 public class ScreenShotEntityTest {
 
+    ScreenShotEntity screenShotEntity;
 
+    @BeforeMethod
+    public void setUp() throws Exception {
+        screenShotEntity = new ScreenShotEntity();
+    }
 
     @Test
     public void testGetHref() throws Exception {
-
-            }
+        Assert.assertNull(screenShotEntity.getHref());
+        screenShotEntity.setHref("href");
+        Assert.assertEquals(screenShotEntity.getHref(),"href");
+    }
 
     @Test
     public void testGetScreenShot() throws Exception {
-
+        Assert.assertNull(screenShotEntity.getScreenShot());
+        screenShotEntity.setScreenShot("shot");
+        Assert.assertEquals(screenShotEntity.getScreenShot(),"shot");
     }
+
+    @Test
+    public void testConstruct() throws Exception {
+        screenShotEntity = new ScreenShotEntity("anotherHref","anotherSceen");
+        Assert.assertEquals(screenShotEntity.getScreenShot(),"anotherSceen");
+        Assert.assertEquals(screenShotEntity.getHref(),"anotherHref");
+    }
+
 }
