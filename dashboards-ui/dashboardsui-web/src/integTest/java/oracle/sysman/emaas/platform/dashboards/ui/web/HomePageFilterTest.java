cfd7d28602d06dffecafd9fd4f91754287d1a723
wenjzhu <wenjie.zhu@oracle.com>
2016-01-21 10:28:10 +0800

    fix HomePageFilterTest mock interface

--- dashboards-ui/dashboardsui-web/src/integTest/java/oracle/sysman/emaas/platform/dashboards/ui/web/HomePageFilterTest.java
+++ dashboards-ui/dashboardsui-web/src/integTest/java/oracle/sysman/emaas/platform/dashboards/ui/web/HomePageFilterTest.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import mockit.Deencapsulation;
 import mockit.Expectations;
-import mockit.Mock;
-import mockit.MockUp;
 import mockit.Mocked;
 import mockit.NonStrictExpectations;
 import mockit.Verifications;
@@ -18,23 +16,19 @@ import oracle.sysman.emSDK.emaas.platform.servicemanager.registry.info.Link;
 import oracle.sysman.emSDK.emaas.platform.servicemanager.registry.lookup.LookupManager;
 import oracle.sysman.emaas.platform.dashboards.ui.webutils.util.RegistryLookupUtil;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpStatus;
-import org.apache.http.ProtocolVersion;
-import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.entity.BasicHttpEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.message.BasicStatusLine;
 import org.apache.logging.log4j.core.util.Charsets;
 import org.testng.annotations.Test;
 
-public class HomePageFilterTest {
+public class HomePageFilterTest
+{
 	@Test
 	public void testDoFilter(@Mocked final FilterChain chain, @Mocked final HttpServletRequest request,
-			@Mocked final HttpServletResponse response, @Mocked final LookupManager lookupManager, 
-			@Mocked final CloseableHttpClient httpClient, @Mocked final RegistryLookupUtil anyUtil) throws Exception {
+			@Mocked final HttpServletResponse response, @Mocked final LookupManager lookupManager,
+			@Mocked final CloseableHttpClient httpClient, @Mocked final RegistryLookupUtil anyUtil) throws Exception
+	{
 		final HomePageFilter filter = new HomePageFilter();
 		final StringBuffer homeUrl = new StringBuffer("https://xxx.us.oracle.com:4443/emsaasui/emcpdfui/home.html");
 		final String testString = "xxxvalueid=12345678";
@@ -43,55 +37,56 @@ public class HomePageFilterTest {
 		final Link link = new Link();
 		link.withHref("http://xxxx/naming/entitynaming/v1/domains");
 		final InputStream is = new ByteArrayInputStream(testString.getBytes(Charsets.UTF_8));
-		
-		final CloseableHttpResponse mockResponse = new MockUp<CloseableHttpResponse>() {
+
+		final CloseableHttpResponse mockResponse = new MockCloseableHttpResponse(is);
+		/*new MockUp<CloseableHttpResponse>() {
 			@Mock
 			HttpEntity getEntity() {
 				BasicHttpEntity entity = new BasicHttpEntity();
 				entity.setContent(is);
 				return entity;
 			}
-			
+
 			@Mock
 			StatusLine getStatusLine() {
 				ProtocolVersion protocolVersion = new ProtocolVersion("HTTP", 1, 1);
 			    StatusLine statusline = new BasicStatusLine(protocolVersion, HttpStatus.SC_BAD_REQUEST, "400");
 			    return statusline;
 			}
-		}.getMockInstance();
-		
+		}.getMockInstance();*/
+
 		new NonStrictExpectations() {
 			{
 				// authorization
 				LookupManager.getInstance().getAuthorizationToken();
 				result = "authorization".toCharArray();
-				
+
 				// getPreference
 				RegistryLookupUtil.getServiceInternalLink(anyString, anyString, anyString, anyString);
 				result = link;
-				
+
 				httpClient.execute((HttpUriRequest) any);
 				result = mockResponse;
-				
+
 				request.getRequestURL();
 				result = homeUrl;
 			}
 		};
-		
+
 		new Expectations() {
 			{
 				String referer = Deencapsulation.getField(filter, "REFERER");
 				request.getHeader(referer);
-				returns(null, 		//1
+				returns(null, //1
 						testString, //2
-						oamPath, 	//3
-						oamPath); 	//4
-				
+						oamPath, //3
+						oamPath); //4
+
 				// userTenant
 				String oamUser = Deencapsulation.getField(filter, "OAM_REMOTE_USER_HEADER");
 				request.getHeader(oamUser);
-				returns(null, 		//3
-						tenant);	//4
+				returns(null, //3
+						tenant); //4
 			}
 		};
 
@@ -102,7 +97,8 @@ public class HomePageFilterTest {
 
 		new Verifications() {
 			{
-				chain.doFilter(request, response); times = 3;
+				chain.doFilter(request, response);
+				times = 3;
 			}
 		};
 	}
---
+++ dashboards-ui/dashboardsui-web/src/integTest/java/oracle/sysman/emaas/platform/dashboards/ui/web/MockCloseableHttpResponse.java
@@ -0,0 +1,240 @@
+/*
+ * Copyright (C) 2016 Oracle
+ * All rights reserved.
+ *
+ * $$File: $$
+ * $$DateTime: $$
+ * $$Author: $$
+ * $$Revision: $$
+ */
+
+package oracle.sysman.emaas.platform.dashboards.ui.web;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+
+import org.apache.http.Header;
+import org.apache.http.HeaderIterator;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.message.BasicStatusLine;
+import org.apache.http.params.HttpParams;
+
+/**
+ * @author wenjzhu
+ */
+@SuppressWarnings("deprecation")
+public class MockCloseableHttpResponse implements CloseableHttpResponse
+{
+
+	private final InputStream is;
+
+	public MockCloseableHttpResponse(InputStream is)
+	{
+		this.is = is;
+	}
+
+	@Override
+	public void addHeader(Header arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addHeader(String arg0, String arg1)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void close() throws IOException
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean containsHeader(String arg0)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Header[] getAllHeaders()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HttpEntity getEntity()
+	{
+		BasicHttpEntity entity = new BasicHttpEntity();
+		entity.setContent(is);
+		return entity;
+	}
+
+	@Override
+	public Header getFirstHeader(String arg0)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Header[] getHeaders(String arg0)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Header getLastHeader(String arg0)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Locale getLocale()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HttpParams getParams()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProtocolVersion getProtocolVersion()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StatusLine getStatusLine()
+	{
+		ProtocolVersion protocolVersion = new ProtocolVersion("HTTP", 1, 1);
+		StatusLine statusline = new BasicStatusLine(protocolVersion, HttpStatus.SC_BAD_REQUEST, "400");
+		return statusline;
+	}
+
+	@Override
+	public HeaderIterator headerIterator()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HeaderIterator headerIterator(String arg0)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeHeader(Header arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeHeaders(String arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setEntity(HttpEntity arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setHeader(Header arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setHeader(String arg0, String arg1)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setHeaders(Header[] arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setLocale(Locale arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setParams(HttpParams arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setReasonPhrase(String arg0) throws IllegalStateException
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setStatusCode(int arg0) throws IllegalStateException
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setStatusLine(ProtocolVersion arg0, int arg1)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setStatusLine(ProtocolVersion arg0, int arg1, String arg2)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setStatusLine(StatusLine arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
