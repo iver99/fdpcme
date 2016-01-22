e2334acf4280e1aec9180c9bce021287d7b3fc96
danfjian <daniel.jiang@oracle.com>
2016-01-22 16:31:11 +0800

    Add inner class tests and MockHttpServletRequest in rest API package.

--- dashboards-web/src/integTest/java/oracle/sysman/emaas/platform/dashboards/ws/rest/DashboardsCORSFilterTest.java
+++ dashboards-web/src/integTest/java/oracle/sysman/emaas/platform/dashboards/ws/rest/DashboardsCORSFilterTest.java
@@ -1,15 +1,28 @@
 package oracle.sysman.emaas.platform.dashboards.ws.rest;
 
+import mockit.Deencapsulation;
 import mockit.Expectations;
 import mockit.Mocked;
+import oracle.sysman.emaas.platform.dashboards.ws.MockHttpServletRequest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * Created by jishshi on 1/21/2016.
@@ -38,6 +51,16 @@ public class DashboardsCORSFilterTest {
     public void testDoFilter(@Mocked final HttpServletRequest httpServletRequest, @Mocked final HttpServletResponse httpServletResponse, @Mocked final FilterChain filterChain) throws Exception {
         //do nothing return
         dashboardsCORSFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
+    }
+
+
+    @Test
+    public void testDoFilterInnerClass(@Mocked final HttpServletRequest httpServletRequest, @Mocked final HttpServletResponse httpServletResponse, @Mocked final FilterChain filterChain) throws Exception {
+        MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.setHeader("OAM_REMOTE_USER", "tenant01.emcsadmin");
+        Object oamHttpRequestWrapperInst = Deencapsulation.newInstance(
+                "oracle.sysman.emaas.platform.dashboards.ws.rest.DashboardsCORSFilter$OAMHttpRequestWrapper",
+                mockHttpServletRequest);
     }
 
     @Test
