98bdf2346fea9b80667b9366824c22ab3460b2fd
Miao Yu <miao.yu@oracle.com>
2016-02-19 14:43:56 +0800

    update service version from 0.1 to 1.0 for ita

--- omc-ui-framework/omc-ui-framework-web/public_html/@version@/js/widgets/widgetselector/widget-selector-impl.js
+++ omc-ui-framework/omc-ui-framework-web/public_html/@version@/js/widgets/widgetselector/widget-selector-impl.js
@@ -496,7 +496,7 @@ define([
                             if ((!widgetProviderName && !widgetProviderVersion) || 
                                     widgetProviderName === pname && widgetProviderVersion === pversion) {
                                 //Enable ITA widget group since ITA widgets are enabled now.
-//                                if (!(pname === 'emcitas-ui-apps' && pversion === '0.1' && data[i].WIDGET_GROUP_ID === 3)) {
+//                                if (!(pname === 'emcitas-ui-apps' && pversion === '1.0' && data[i].WIDGET_GROUP_ID === 3)) {
                                     var widgetGroup = {value:pname+'|'+pversion+'|'+gname, label:gname};
                                     targetWidgetGroupArray.push(widgetGroup);
                                     availableWidgetGroups.push(data[i]);
