9798f4cfef3e60214137164bd321a6ea9f7091c3
johnson <johnson.s.shi@oracle.com>
2016-03-16 17:32:44 +0800

    remove dashboard sets record if the dashboard is deleted

--- dashboards-core/src/main/java/oracle/sysman/emaas/platform/dashboards/core/DashboardManager.java
+++ dashboards-core/src/main/java/oracle/sysman/emaas/platform/dashboards/core/DashboardManager.java
@@ -191,6 +191,7 @@ public class DashboardManager
 		if (!permanent) {
 			ed.setDeleted(dashboardId);
 			dsf.mergeEmsDashboard(ed);
+			dsf.removeEmsSubDashboardById(dashboardId);
 		}
 		else {
 			EmsDashboardLastAccess edla = getLastAccess(dashboardId, tenantId);
