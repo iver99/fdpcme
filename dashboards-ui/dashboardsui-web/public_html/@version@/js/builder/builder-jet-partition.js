77a9de8c89d15f2907a7c56ceb860461927b155b
Guobao Chen <guobao.chen@oracle.com>
2016-03-21 11:34:40 +0800

    temp: push combine js code but issue exists

---
+++ dashboards-ui/dashboardsui-web/public_html/@version@/js/builder/builder-jet-partition.js
@@ -0,0 +1,32 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+define([
+    'ojs/ojknockout',
+    'ojs/ojmenu',
+    'ojs/ojtree',
+    'ojs/ojvalidation',
+    'ojs/ojknockout-validation',
+    'ojs/ojbutton',
+    'ojs/ojselectcombobox',
+    'ojs/ojpopup',
+    'ojs/ojchart',
+    'ojs/ojcomponents',   
+    'ojs/ojdatetimepicker',
+    'ojs/ojmodel',
+    'ojs/ojknockout-model',
+    'ojs/ojtoolbar',
+    'ojs/ojpagingcontrol',
+    'ojs/ojeditablevalue',
+    'ojs/internal-deps/dvt/DvtChart',
+    'ojs/ojdvt-base',
+    'ojs/ojcheckboxset',
+    'ojs/ojpopup',
+    'ojs/ojgauge'
+], function (oj)
+{
+    return {
+    };
+});
