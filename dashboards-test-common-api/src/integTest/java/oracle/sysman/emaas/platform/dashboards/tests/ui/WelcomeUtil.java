ef95a0f0bf633e09563b9dc2b062b9762f27ab03
jiangxia.nan@oracle.com <jnan@slc09wrc.us.oracle.com>
2016-03-31 00:53:02 -0700

    common api for welcome page

--- dashboards-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/WelcomeUtil.java
+++ dashboards-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/WelcomeUtil.java
@@ -1,68 +1,107 @@
 package oracle.sysman.emaas.platform.dashboards.tests.ui;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import oracle.sysman.emsaas.login.LoginUtils;
-import oracle.sysman.emsaas.login.PageUtils;
+import oracle.sysman.emaas.platform.dashboards.tests.ui.util.DashBoardPageId;
 import oracle.sysman.qatool.uifwk.webdriver.WebDriver;
-import oracle.sysman.qatool.uifwk.webdriver.WebDriverUtils;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.Keys;
 
-import org.testng.Assert;
-
-public class WelcomeUtil {
+public class WelcomeUtil
+{
 
 	private static WebDriver driver;
 
-    public static void loadWebDriverOnly(WebDriver webDriver) throws Exception
+	public static void dataExplorers(WebDriver webd, String selection) throws Exception
+	{
+		webd.getLogger().info("Visiting Data Explorer-" + selection + " from Welcome Page...");
+		webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_DataExp_SelectID)).click();
+		switch (selection) {
+			case "log":
+				webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_DataExp_Log)).click();
+				break;
+			case "analyze":
+				webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_DataExp_Analyze)).click();
+				break;
+			case "search":
+				webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_DataExp_Search)).click();
+				break;
+		}
+		webd.takeScreenShot();
+	}
+
+	public static void learnMoreHow(WebDriver webd) throws Exception
+	{
+		webd.getLogger().info("Visiting 'Learn More-How to get started' from Welcome Page...");
+		webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_LearnMore_getStarted)).click();
+		webd.takeScreenShot();
+	}
+
+	public static void learnMoreServiceOffering(WebDriver webd) throws Exception
+	{
+		webd.getLogger().info("Visiting 'Learn More-Service Offerings' from Welcome Page...");
+		webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_LearnMore_ServiceOffering)).click();
+		webd.takeScreenShot();
+	}
+
+	public static void learnMoreVideo(WebDriver webd) throws Exception
+	{
+		webd.getLogger().info("Visiting 'Learn More-Videos' from Welcome Page...");
+		webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_LearnMore_Videos)).click();
+		webd.takeScreenShot();
+	}
+
+	public static void loadWebDriverOnly(WebDriver webDriver) throws Exception
 	{
 		driver = webDriver;
 	}
-    
-    public static void visitAPM() throws Exception
-    {
-    	
-    }
-    public static void visitLA() throws Exception
-    {
-    	
-    }
-    public static void visitITA(String selection ) throws Exception
-    {
-    	
-    }
-    public static void visitDashboards() throws Exception
-    {
-    	
-    }
-    public static void dataExplorers(String selection) throws Exception
-    {
-    	
-    }
-    public static void learnMoreHow() throws Exception
-    {
-    	
-    }
-    public static void learnMoreVideo() throws Exception
-    {
-    	
-    }
-    public static void learnMoreServiceOffering() throws Exception
-    {
-    	
-    }
-    
-    
-    
-	
+
+	public static void visitAPM(WebDriver webd) throws Exception
+	{
+		webd.getLogger().info("Visit APM from Welcome Page...");
+		webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_APMLinkCSS)).click();
+		webd.takeScreenShot();
+	}
+
+	public static void visitDashboards(WebDriver webd) throws Exception
+	{
+		webd.getLogger().info("Visit Dashboards from Welcome Page...");
+		webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_DashboardsLinkID)).click();
+		webd.takeScreenShot();
+	}
+
+	public static void visitITA(WebDriver webd, String selection) throws Exception
+	{
+		webd.getLogger().info("Visiting ITA-" + selection + " from Welcome Page...");
+		webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_ITA_SelectID)).click();
+		switch (selection) {
+			case "default":
+				webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_ITALinkID)).click();
+				break;
+			case "performanceAnalyticsDatabase":
+				webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_ITA_PADatabase)).click();
+				break;
+			case "performanceAnalyticsMiddleware":
+				webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_ITA_PAMiddleware)).click();
+				break;
+			case "resourceAnalyticsDatabase":
+				webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_ITA_RADatabase)).click();
+				break;
+			case "resourceAnalyticsMiddleware":
+				webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_ITA_RAMiddleware)).click();
+				break;
+			case "dataExplorerAnalyze":
+				webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_ITA_DEAnalyze)).click();
+				break;
+			case "dataExplorer":
+				webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_ITA_DE)).click();
+				break;
+		}
+		webd.takeScreenShot();
+	}
+
+	public static void visitLA(WebDriver webd) throws Exception
+	{
+		webd.getLogger().info("Visiting LA from Welcome Page...");
+		webd.getWebDriver().findElement(By.cssSelector(DashBoardPageId.Welcome_LALinkCSS)).click();
+		webd.takeScreenShot();
+	}
 }
