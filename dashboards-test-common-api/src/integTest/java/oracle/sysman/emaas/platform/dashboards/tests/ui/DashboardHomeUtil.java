d1da13e20dd71530c7e8a26e8364c7ffa3cdb0c0
wenjzhu <wenjie.zhu@oracle.com>
2016-03-31 19:22:17 +0800

    reorder api

--- dashboards-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/DashboardHomeUtil.java
+++ dashboards-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/DashboardHomeUtil.java
@@ -1,74 +1,68 @@
 package oracle.sysman.emaas.platform.dashboards.tests.ui;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import oracle.sysman.emsaas.login.LoginUtils;
-import oracle.sysman.emsaas.login.PageUtils;
 import oracle.sysman.qatool.uifwk.webdriver.WebDriver;
-import oracle.sysman.qatool.uifwk.webdriver.WebDriverUtils;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.Keys;
-
-import org.testng.Assert;
-
-public class DashboardHomeUtil {
+public class DashboardHomeUtil
+{
 
 	private static WebDriver driver;
 
-    public static void loadWebDriverOnly(WebDriver webDriver) throws Exception
+	public static void createDashboard(String name, String descriptions, Boolean displayDesc, Boolean selectorRefreshcontrol)
+			throws Exception
+	{
+
+	}
+
+	public static void createDashboardSet(String name, String descriptions, Boolean displayDesc, Boolean selectorRefreshcontrol)
+			throws Exception
+	{
+
+	}
+
+	public static void delete(String dashboardName, String view) throws Exception
+	{
+
+	}
+
+	public static void exploreData(String option) throws Exception
+	{
+
+	}
+
+	public static void filterOptions(String cloudServices, String createdBy, String Navigation, Boolean Favorites)
+			throws Exception
+	{
+
+	}
+
+	public static void gridView() throws Exception
+	{
+
+	}
+
+	public static void listView() throws Exception
+	{
+
+	}
+
+	public static void loadWebDriverOnly(WebDriver webDriver) throws Exception
 	{
 		driver = webDriver;
 	}
-    public static void filterOptions(String cloudServices, String createdBy, String Navigation, Boolean Favorites) throws Exception
-    {
-    	
-    }
-    public static void search(String searchString) throws Exception
-    {
-    	
-    }
-    public static void selectOOB(String dashboardName) throws Exception
-    {
-    	
-    }
-    public static void sortBy(String option) throws Exception
-    {
-    	
-    }
-    public static void exploreData(String option) throws Exception
-    {
-    	
-    }
-    public static void listView() throws Exception
-    {
-    	
-    }
-    public static void gridView() throws Exception
-    {
-    	
-    }
-    public static void delete(String dashboardName, String view) throws Exception
-    {
-    	
-    }
-    public static void createDashboard(String name, String descriptions, Boolean displayDesc, Boolean selectorRefreshcontrol) throws Exception
-    {
-    	
-    }
-    public static void createDashboardSet(String name, String descriptions, Boolean displayDesc, Boolean selectorRefreshcontrol) throws Exception
-    {
-    	
-    }
-    
-    
-	
+
+	public static void search(String searchString) throws Exception
+	{
+
+	}
+
+	public static void selectOOB(String dashboardName) throws Exception
+	{
+
+	}
+
+	public static void sortBy(String option) throws Exception
+	{
+
+	}
+
 }
