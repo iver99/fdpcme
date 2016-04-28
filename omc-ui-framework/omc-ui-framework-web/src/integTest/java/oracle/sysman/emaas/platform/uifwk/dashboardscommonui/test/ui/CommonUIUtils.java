bd05cf4d70e1d52b47a277c2a34ad00449e28c07
Shangping Wang <shangping.wang@oracle.com>
2016-04-28 02:18:20 -0700

    add wait before verify appname

--- omc-ui-framework/omc-ui-framework-web/src/integTest/java/oracle/sysman/emaas/platform/uifwk/dashboardscommonui/test/ui/CommonUIUtils.java
+++ omc-ui-framework/omc-ui-framework-web/src/integTest/java/oracle/sysman/emaas/platform/uifwk/dashboardscommonui/test/ui/CommonUIUtils.java
@@ -207,7 +207,7 @@ public class CommonUIUtils
 				sPropertyValue = emaasProp.getProperty("SSO_USERNAME");
 				if (sPropertyValue == null) {
 					CommonUIUtils
-					.commonUITestLog("The SSO_USERNAME property value is null ... set it to a different value -- 'emcsadmin'.");
+							.commonUITestLog("The SSO_USERNAME property value is null ... set it to a different value -- 'emcsadmin'.");
 					sPropertyValue = "emcsadmin";
 				}
 			}
@@ -215,7 +215,7 @@ public class CommonUIUtils
 				sPropertyValue = emaasProp.getProperty("SSO_PASSWORD");
 				if (sPropertyValue == null) {
 					CommonUIUtils
-					.commonUITestLog("The SSO_PASSWORD property value is null ... set it to a different value -- 'Welcome1!'.");
+							.commonUITestLog("The SSO_PASSWORD property value is null ... set it to a different value -- 'Welcome1!'.");
 					sPropertyValue = "Welcome1!";
 				}
 			}
@@ -223,7 +223,7 @@ public class CommonUIUtils
 				sPropertyValue = emaasProp.getProperty("COMMON_UI_URL_SUFFIX");
 				if (sPropertyValue == null) {
 					CommonUIUtils
-					.commonUITestLog("The COMMON_UI_URL_SUFFIX property value is null ... set it to a different value -- '/emsaasui/uifwk/test.html'.");
+							.commonUITestLog("The COMMON_UI_URL_SUFFIX property value is null ... set it to a different value -- '/emsaasui/uifwk/test.html'.");
 					sPropertyValue = "/emsaasui/uifwk/test.html";
 				}
 			}
@@ -231,7 +231,7 @@ public class CommonUIUtils
 				sPropertyValue = emaasProp.getProperty("SAAS_AUTH_TOKEN");
 				if (sPropertyValue == null) {
 					CommonUIUtils
-							.commonUITestLog("The DASHBOARD_API_ENDPOINT property value is null ... set it to a different value -- 'welcome1'.");
+					.commonUITestLog("The DASHBOARD_API_ENDPOINT property value is null ... set it to a different value -- 'welcome1'.");
 					sPropertyValue = "Basic d2VibG9naWM6d2VsY29tZTE=";
 
 				}
@@ -239,7 +239,7 @@ public class CommonUIUtils
 					sPropertyValue = emaasProp.getProperty("DASHBOARD_API_ENDPOINT");
 					if (sPropertyValue == null) {
 						CommonUIUtils
-								.commonUITestLog("The SAAS_AUTH_TOKEN property value is null ... set it to a different value .");
+						.commonUITestLog("The SAAS_AUTH_TOKEN property value is null ... set it to a different value .");
 						sPropertyValue = sOhsUrl + "/emcpdf/api/v1/";
 					}
 				}
@@ -248,7 +248,7 @@ public class CommonUIUtils
 				sPropertyValue = emaasProp.getProperty("TARGETMODEL_SERVICE_SHARD_ENDPOINT");
 				if (sPropertyValue == null) {
 					CommonUIUtils
-					.commonUITestLog("The TARGETMODEL_SERVICE_SHARD_ENDPOINT property value is null ... set it to a different value.");
+							.commonUITestLog("The TARGETMODEL_SERVICE_SHARD_ENDPOINT property value is null ... set it to a different value.");
 					sRolesUrl = CommonUIUtils.getEmaasPropertyValue("EMCS_NODE2_HOSTNAME");
 					if (sRolesUrl == null) {
 						sPropertyValue = null;
@@ -504,6 +504,8 @@ public class CommonUIUtils
 		driver.getLogger().info("The Product is:  " + driver.getText(UIControls.sProductText));
 		Assert.assertEquals(driver.getText(UIControls.sProductText), "Management Cloud");
 		Assert.assertTrue(driver.isElementPresent(UIControls.sAppText));
+		WebDriverWait wait = new WebDriverWait(driver.getWebDriver(), 900L);
+		wait.until(ExpectedConditions.textToBePresentInElement(By.xpath(UIControls.sAppText), sAppName));
 		driver.getLogger().info("The App is:  " + sAppName);
 		Assert.assertEquals(driver.getText(UIControls.sAppText), sAppName);
 		Assert.assertTrue(driver.isElementPresent(UIControls.sPageText));
