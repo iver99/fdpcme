63768f33b00996245ddbd1aef64c6e305a9bb93c
Miao Yu <miao.yu@oracle.com>
2016-05-14 22:14:08 +0800

    remove tests related to ITA FA which is retired in 1.7

--- dashboards-uitest/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/ui/TestHomePage.java
+++ dashboards-uitest/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/ui/TestHomePage.java
@@ -51,19 +51,6 @@ public class TestHomePage extends LoginAndLogout
 
 	}
 
-	@Test
-	public void testExploreData_AnalyzeLink() throws Exception
-	{
-		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
-		webd.getLogger().info("start to test in testExploreData_AnalyzeLink");
-
-		DashboardHomeUtil.gotoDataExplorer(webd, DashboardHomeUtil.EXPLOREDATA_MENU_ANALYZE);
-		String url = webd.getWebDriver().getCurrentUrl();
-		webd.getLogger().info("url = " + url);
-		Assert.assertEquals(url.substring(url.indexOf("emsaasui") + 9),
-				"emcitas/flex-analyzer/html/displaying/new-chart-config.html");
-
-	}
 
 	@Test
 	public void testExploreData_LALink() throws Exception
