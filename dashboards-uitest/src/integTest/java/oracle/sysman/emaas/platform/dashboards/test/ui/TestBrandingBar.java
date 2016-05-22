ca55e78e5cf38685806cdd3c0c7f0767e233fd55
alvin.duan <alvin.duan@oracle.com>
2016-05-22 07:34:37 -0700

    Improve test cases for TestBrandingBar

--- dashboards-uitest/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/ui/TestBrandingBar.java
+++ dashboards-uitest/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/ui/TestBrandingBar.java
@@ -146,6 +146,8 @@ public class TestBrandingBar extends LoginAndLogout {
 				Thread.currentThread().getStackTrace()[1].getMethodName(),
 				"emaastesttenant1_la_admin1");
 		webd.getLogger().info("start to test in testAdminLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
 		BrandingBarUtil.visitApplicationCloudService(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_CS_LA);
 		WaitUtil.waitForPageFullyLoaded(webd);
@@ -229,6 +231,8 @@ public class TestBrandingBar extends LoginAndLogout {
 				Thread.currentThread().getStackTrace()[1].getMethodName(),
 				"emaastesttenant1_apm_admin1");
 		webd.getLogger().info("start to test in testAdminLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
 		// navigate to APM page
 		BrandingBarUtil.visitApplicationCloudService(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_CS_APM);
@@ -250,6 +254,7 @@ public class TestBrandingBar extends LoginAndLogout {
 		// Agents link
 		BrandingBarUtil.visitApplicationAdministration(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_ADMIN_AGENT);
+		WaitUtil.waitForPageFullyLoaded(webd);
 		url = webd.getWebDriver().getCurrentUrl();
 		webd.getLogger().info("url = " + url);
 		Assert.assertEquals(url.substring(url.indexOf("emsaasui") + 9),
@@ -261,8 +266,6 @@ public class TestBrandingBar extends LoginAndLogout {
 				BrandingBarUtil.NAV_LINK_TEXT_ADMIN_AGENT));
 		Assert.assertTrue(BrandingBarUtil.isAdminLinkExisted(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_ADMIN_ALERT));
-		BrandingBarUtil.visitApplicationAdministration(webd,
-				BrandingBarUtil.NAV_LINK_TEXT_ADMIN_ALERT);
 
 		BrandingBarUtil.visitApplicationAdministration(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_ADMIN_ALERT);
@@ -281,6 +284,7 @@ public class TestBrandingBar extends LoginAndLogout {
 				BrandingBarUtil.NAV_LINK_TEXT_ADMIN_ALERT));
 		BrandingBarUtil.visitApplicationAdministration(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_ADMIN_ALERT);
+		WaitUtil.waitForPageFullyLoaded(webd);
 
 		// Test From Home Page
 		BrandingBarUtil.visitDashboardHome(webd);
@@ -303,6 +307,8 @@ public class TestBrandingBar extends LoginAndLogout {
 				Thread.currentThread().getStackTrace()[1].getMethodName(),
 				"emaastesttenant1_apm_user1");
 		webd.getLogger().info("start to test in testAdminLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
 		BrandingBarUtil.visitApplicationCloudService(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_CS_APM);
 		WaitUtil.waitForPageFullyLoaded(webd);
@@ -321,10 +327,12 @@ public class TestBrandingBar extends LoginAndLogout {
 	public void testAdminLink() throws Exception {
 		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
 		webd.getLogger().info("start to test in testAdminLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
 
 		// IT Analytics Administration link
 		BrandingBarUtil.visitApplicationAdministration(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_ADMIN_ITA);
+		WaitUtil.waitForPageFullyLoaded(webd);
 		String url = webd.getWebDriver().getCurrentUrl();
 		webd.getLogger().info("url = " + url);
 		Assert.assertEquals(url.substring(url.indexOf("emsaasui") + 9),
@@ -335,10 +343,12 @@ public class TestBrandingBar extends LoginAndLogout {
 	public void testAgentsLink() throws Exception {
 		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
 		webd.getLogger().info("start to test in testAgentsLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
 
 		// Agents link
 		BrandingBarUtil.visitApplicationAdministration(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_ADMIN_AGENT);
+		WaitUtil.waitForPageFullyLoaded(webd);
 		String url = webd.getWebDriver().getCurrentUrl();
 		webd.getLogger().info("url = " + url);
 		Assert.assertEquals(url.substring(url.indexOf("emsaasui") + 9),
@@ -350,10 +360,12 @@ public class TestBrandingBar extends LoginAndLogout {
 	public void testAPMLink() throws Exception {
 		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
 		webd.getLogger().info("start to test in testAPMLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
 
 		// APM link
 		BrandingBarUtil.visitApplicationCloudService(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_CS_APM);
+		WaitUtil.waitForPageFullyLoaded(webd);
 		String url = webd.getWebDriver().getCurrentUrl();
 		webd.getLogger().info("url = " + url);
 		Assert.assertEquals(url.substring(url.indexOf("emsaasui") + 9),
@@ -364,9 +376,11 @@ public class TestBrandingBar extends LoginAndLogout {
 	public void testDashBoardHomeLink() throws Exception {
 		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
 		webd.getLogger().info("start to test in testDashBoardHomeLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
 
 		// dashboardHome link
 		BrandingBarUtil.visitDashboardHome(webd);
+		WaitUtil.waitForPageFullyLoaded(webd);
 		String url = webd.getWebDriver().getCurrentUrl();
 		webd.getLogger().info("url = " + url);
 		Assert.assertEquals(url.substring(url.indexOf("emsaasui") + 9),
@@ -377,9 +391,11 @@ public class TestBrandingBar extends LoginAndLogout {
 	public void testHomeLink() throws Exception {
 		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
 		webd.getLogger().info("start to test in testHomeLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
 
 		// Home link
 		BrandingBarUtil.visitMyHome(webd);
+		WaitUtil.waitForPageFullyLoaded(webd);
 		String url = webd.getWebDriver().getCurrentUrl();
 		webd.getLogger().info("url = " + url);
 		Assert.assertEquals(url.substring(url.indexOf("emsaasui") + 9),
@@ -390,10 +406,12 @@ public class TestBrandingBar extends LoginAndLogout {
 	public void testITALink() throws Exception {
 		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
 		webd.getLogger().info("start to test in testITALink");
+		WaitUtil.waitForPageFullyLoaded(webd);
 
 		// IT Analytics link,check checkbox
 		BrandingBarUtil.visitApplicationCloudService(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_CS_ITA);
+		WaitUtil.waitForPageFullyLoaded(webd);
 		String url = webd.getWebDriver().getCurrentUrl();
 		webd.getLogger().info("url = " + url);
 		Assert.assertEquals(url.substring(url.indexOf("emsaasui") + 9),
@@ -404,10 +422,12 @@ public class TestBrandingBar extends LoginAndLogout {
 	public void testLALink() throws Exception {
 		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
 		webd.getLogger().info("start to test in testLALink");
+		WaitUtil.waitForPageFullyLoaded(webd);
 
 		// Log Analytics link
 		BrandingBarUtil.visitApplicationCloudService(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_CS_LA);
+		WaitUtil.waitForPageFullyLoaded(webd);
 		String url = webd.getWebDriver().getCurrentUrl();
 		webd.getLogger().info("url = " + url);
 		Assert.assertEquals(url.substring(url.indexOf("emsaasui") + 9),
@@ -418,10 +438,12 @@ public class TestBrandingBar extends LoginAndLogout {
 	public void testLogLink() throws Exception {
 		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
 		webd.getLogger().info("start to test in testLogLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
 
 		// Log link
 		BrandingBarUtil.visitApplicationVisualAnalyzer(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_VA_LA);
+		WaitUtil.waitForPageFullyLoaded(webd);
 		String url = webd.getWebDriver().getCurrentUrl();
 		webd.getLogger().info("url = " + url);
 		Assert.assertEquals(url.substring(url.indexOf("emsaasui") + 9),
@@ -432,10 +454,12 @@ public class TestBrandingBar extends LoginAndLogout {
 	public void testSearchLink() throws Exception {
 		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
 		webd.getLogger().info("start to test in testSearchLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
 
 		// Target link
 		BrandingBarUtil.visitApplicationVisualAnalyzer(webd,
 				BrandingBarUtil.NAV_LINK_TEXT_VA_TA);
+		WaitUtil.waitForPageFullyLoaded(webd);
 		String url = webd.getWebDriver().getCurrentUrl();
 		webd.getLogger().info("url = " + url);
 		String sub_str = url.substring(url.indexOf("emsaasui") + 9);
