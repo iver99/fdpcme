53831d2794c25fe03b9437b8eafe867eb95b64f3
Caleb Wei <caleb.wei@oracle.com>
2016-06-28 23:36:04 -0700

    comment out shutdown method

--- dashboards-uitest/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/ui/util/LoginAndLogout.java
+++ dashboards-uitest/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/ui/util/LoginAndLogout.java
@@ -8,42 +8,61 @@ import oracle.sysman.qatool.uifwk.webdriver.WebDriverUtils;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 
-public class LoginAndLogout {
+public class LoginAndLogout
+{
 	public static WebDriver webd = null;
 
 	@AfterTest
-	public static void logout() {
+	public static void logout()
+	{
 		if (webd != null) {
-			// LoginUtils.doLogout(webd);
-			webd.shutdownBrowser(true);
+			LoginUtils.doLogout(webd);
+			//webd.shutdownBrowser(true);
 		}
 	}
 
 	@AfterMethod
-	public static void logout_method() {
+	public static void logout_method()
+	{
 		if (webd != null) {
 			LoginUtils.doLogout(webd);
 		}
 	}
 
-	public void login(String testName) {
+	public void customlogin(String testName, String customUser)
+	{
+		String tenantID = null, username = null;
+		try {
+			tenantID = oracle.sysman.emsaas.login.utils.Utils.getProperty("TENANT_ID");
+		}
+		catch (Exception e) {
+			tenantID = "emaastesttenant1";// site59tenant1";//";//site46tenant1";"emaastesttenant1";id0816b";//
+		}
+		username = customUser;
+		login(testName, username, "Welcome1!", tenantID, "home", "Dashboard-UI");
+
+	}
+
+	public void login(String testName)
+	{
 		login(testName, "home");
 	}
 
-	public void login(String testName, String rel) {
+	public void login(String testName, String rel)
+	{
 		String tenantID = null, username = null;
 		try {
-			tenantID = oracle.sysman.emsaas.login.utils.Utils
-					.getProperty("TENANT_ID");
-		} catch (Exception e) {
+			tenantID = oracle.sysman.emsaas.login.utils.Utils.getProperty("TENANT_ID");
+		}
+		catch (Exception e) {
 			tenantID = "emaastesttenant1";// site59tenant1";//";//site46tenant1";"emaastesttenant1";id0816b";//
 		}
 
 		try {
-			username = oracle.sysman.emsaas.login.utils.Utils
-					.getProperty("SSO_USERNAME");
+			username = oracle.sysman.emsaas.login.utils.Utils.getProperty("SSO_USERNAME");
 
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			username = "emcsadmin";// juan.zhang@oracle.com";//
 		}
 
@@ -52,27 +71,15 @@ public class LoginAndLogout {
 		// "Dashboard-UI");
 	}
 
-	public void customlogin(String testName, String customUser) {
-		String tenantID = null, username = null;
-		try {
-			tenantID = oracle.sysman.emsaas.login.utils.Utils
-					.getProperty("TENANT_ID");
-		} catch (Exception e) {
-			tenantID = "emaastesttenant1";// site59tenant1";//";//site46tenant1";"emaastesttenant1";id0816b";//
-		}
-		username = customUser;
-		login(testName, username, "Welcome1!", tenantID, "home", "Dashboard-UI");
-		
-	}
-
-	public void login(String testName, String username, String password,
-			String tenantId, String rel, String servicename) {
+	public void login(String testName, String username, String password, String tenantId, String rel, String servicename)
+	{
 		webd = WebDriverUtils.initWebDriver(testName);
 		String url = null;
 		webd.getLogger().info("before::start to test in LoginAndOut");
 		try {
 			url = PageUtils.getServiceLink(tenantId, rel, servicename);
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			// url =
 			// "https://slc04lec.us.oracle.com:4443/emsaasui/emcpdfui/home.html";
 			// url =
@@ -89,9 +96,7 @@ public class LoginAndLogout {
 		}
 
 		String testPropertiesFile = System.getenv("EMAAS_PROPERTIES_FILE");
-		webd.getLogger()
-				.info("url is " + url + "   properties file is "
-						+ testPropertiesFile);
+		webd.getLogger().info("url is " + url + "   properties file is " + testPropertiesFile);
 		// String url =
 		// "https://slc07hcn.us.oracle.com:4443/emsaasui/emcpdfui/home.html";
 		// String url =
@@ -103,7 +108,8 @@ public class LoginAndLogout {
 		if (!webd.getWebDriver().getCurrentUrl().equals(url)) {
 			try {
 				LoginUtils.doLogin(webd, username, password, tenantId, url);
-			} catch (Exception e) {
+			}
+			catch (Exception e) {
 				webd.getLogger().info("LogUtils is null");
 			}
 		}
