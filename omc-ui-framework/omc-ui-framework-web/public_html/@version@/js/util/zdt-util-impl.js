fff08a6ed0dba7b4403d1207cebd2cb477488457
alvin.duan <alvin.duan@oracle.com>
2016-07-19 14:27:42 +0800

    JIRA:EMCPDF-1860 ZDT support for planned downtime detecting.

---
+++ omc-ui-framework/omc-ui-framework-web/public_html/@version@/js/util/zdt-util-impl.js
@@ -0,0 +1,89 @@
+define([
+    'jquery', 
+    'knockout',
+    'ojs/ojcore',
+    'uifwk/js/util/df-util',
+    'uifwk/js/util/ajax-util'
+],
+    function($, ko, oj, dfuModel, ajaxUtilModel)
+    {
+        function DashboardFrameworkZdtUtil() {
+            var self = this;
+            var downtimeDetectUrl = "/sso.static/dashboards.omcstatus";
+            var dfu = new dfuModel();
+            var ajaxUtil = new ajaxUtilModel();
+            
+            /**
+             * Check if OMC is under planned downtime or not.
+             * 
+             * This is a sync call to check whether OMC is under planned downtime or not, 
+             * if yes return true, otherwise return false. 
+             * 
+             * @returns {boolean}
+             */ 
+            self.isUnderPlannedDowntime = function() {
+                var underPlannedDowntime = false;
+                if (dfu.isDevMode()){
+                    downtimeDetectUrl = dfu.buildFullUrl(dfu.getDevData().dfRestApiEndPoint,"omcstatus");
+                }
+                ajaxUtil.ajaxWithRetry({
+                    type: "GET",
+                    url: downtimeDetectUrl,
+                    async: false,
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8"
+                })
+                .fail(function(jqXHR, textStatus, errorThrown) {
+                    var apigwHeaders = ajaxUtil.getAPIGWHeaderValues(jqXHR, 'X-ORCL-OMC-APIGW-RETRYAFTER');
+                    if (jqXHR.status === 503 && apigwHeaders && apigwHeaders['msg'] === '"planned downtime"') {
+                        underPlannedDowntime = true;
+                    }
+                });
+                
+                return underPlannedDowntime;
+            };
+            
+            /**
+             * Detect whether OMC is under planned downtime or not asynchronously.
+             * 
+             * This is an async call to check whether OMC is under planned downtime or not, 
+             * where callback is a function with a input paramter of type boolean, true: downtime, false: not downtime.
+             * 
+             * @param {Function} callback
+             * @returns 
+             */ 
+            self.detectPlannedDowntime = function(callback) {
+                if (typeof(callback) !== 'function') {
+                    oj.Logger.error("Error: Failed to detect OMC's planned downtime. callback should be defined as a function.");
+                    return;
+                }
+                if (dfu.isDevMode()){
+                    downtimeDetectUrl = dfu.buildFullUrl(dfu.getDevData().dfRestApiEndPoint,"omcstatus");
+                }
+                ajaxUtil.ajaxWithRetry({
+                    type: "GET",
+                    url: downtimeDetectUrl,
+                    async: true,
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8"
+                })
+                .done(function() {
+                    callback(false);
+                })
+                .fail(function(jqXHR, textStatus, errorThrown) {
+                    var apigwHeaders = ajaxUtil.getAPIGWHeaderValues(jqXHR, 'X-ORCL-OMC-APIGW-RETRYAFTER');
+                    if (jqXHR.status === 503 && apigwHeaders && apigwHeaders['msg'] === '"planned downtime"') {
+                        callback(true);
+                    }
+                    else {
+                        callback(false);
+                    }
+                });
+            };
+            
+        }
+        
+        return DashboardFrameworkZdtUtil;
+    }
+);
+
