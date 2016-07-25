2aa836687ed62b91ef0ad175f387c6758b36acc4
Miao Yu <miao.yu@oracle.com>
2016-07-25 20:19:37 +0800

    JIRA:EMCPDF-1956 improve sql

--- dashboards-schema/src/main/sql/1.9.0/emaas_dashboards_remove_brownfield.sql
+++ dashboards-schema/src/main/sql/1.9.0/emaas_dashboards_remove_brownfield.sql
@@ -2,10 +2,12 @@ DEFINE TENANT_ID = '&1'
 SET FEEDBACK ON
 SET SERVEROUTPUT ON
 
-
+DECLARE
+BEGIN
 DELETE FROM EMS_DASHBOARD_TILE WHERE TENANT_ID = '&TENANT_ID'
   AND DASHBOARD_ID IN
-  (SELECT DASHBOARD_ID FROM EMS_DASHBOARD WHERE NAME IN ('Database Configuration and Storage by Version'
+  (SELECT DASHBOARD_ID FROM EMS_DASHBOARD WHERE TENANT_ID = '&TENANT_ID' AND DASHBOARD_ID<=1000 and NAME IN 
+('Database Configuration and Storage by Version'
 ,'Enterprise Overview'
 ,'Host Inventory by Platform'
 ,'Top 25 Databases by Resource Consumption'
@@ -32,7 +34,8 @@ DELETE FROM EMS_DASHBOARD_TILE WHERE TENANT_ID = '&TENANT_ID'
 
 DELETE FROM EMS_DASHBOARD_LAST_ACCESS WHERE TENANT_ID = '&TENANT_ID'
   AND DASHBOARD_ID IN
-  (SELECT DASHBOARD_ID FROM EMS_DASHBOARD WHERE NAME IN ('Database Configuration and Storage by Version'
+  (SELECT DASHBOARD_ID FROM EMS_DASHBOARD WHERE TENANT_ID = '&TENANT_ID' AND DASHBOARD_ID<=1000 and NAME IN 
+('Database Configuration and Storage by Version'
 ,'Enterprise Overview'
 ,'Host Inventory by Platform'
 ,'Top 25 Databases by Resource Consumption'
@@ -59,7 +62,8 @@ DELETE FROM EMS_DASHBOARD_LAST_ACCESS WHERE TENANT_ID = '&TENANT_ID'
 
 DELETE FROM EMS_DASHBOARD_USER_OPTIONS WHERE TENANT_ID = '&TENANT_ID'
   AND DASHBOARD_ID IN
-  (SELECT DASHBOARD_ID FROM EMS_DASHBOARD WHERE NAME IN ('Database Configuration and Storage by Version'
+  (SELECT DASHBOARD_ID FROM EMS_DASHBOARD WHERE TENANT_ID = '&TENANT_ID' AND DASHBOARD_ID<=1000 and NAME IN 
+('Database Configuration and Storage by Version'
 ,'Enterprise Overview'
 ,'Host Inventory by Platform'
 ,'Top 25 Databases by Resource Consumption'
@@ -108,4 +112,11 @@ DELETE FROM EMS_DASHBOARD WHERE NAME IN ('Database Configuration and Storage by 
 ,'Top 5 weblogic servers'
 ,'Top 5 application deployments'
 ,'Target status');
+  DBMS_OUTPUT.PUT_LINE('Remove out of box dashboards for BF successfully');
+EXCEPTION
+  WHEN OTHERS THEN
+    ROLLBACK;
+    DBMS_OUTPUT.PUT_LINE('Failed to remove out of box dashboards for BF due to error '||SQLERRM);
+    RAISE;
+END;
 /
