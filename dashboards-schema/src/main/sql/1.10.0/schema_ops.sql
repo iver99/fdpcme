9119b2a1fcaa14b031470fcf86e2acc0faaae9aa
Miao Yu <miao.yu@oracle.com>
2016-08-14 09:32:10 +0800

    bump version from 1.9.0 to 1.10.0

---
+++ dashboards-schema/src/main/sql/1.10.0/schema_ops.sql
@@ -0,0 +1,21 @@
+Rem ----------------------------------------------------------------
+
+Rem 07/11/2016	aduan  	Created file
+
+Rem Extract unique teant IDs from tables EMS_DASHBOARD & append that ID next to upgrade implementation file & run that file
+Rem ----------------------------------------------------------------
+
+@&EMSAAS_SQL_ROOT/1.10.0/upgrade_impl_ddl.sql
+
+SET HEADING OFF
+SET FEEDBACK OFF
+SET LINESIZE 2000
+SPOOL &EMSAAS_SQL_ROOT/1.10.0/upgrade_impl_dml_tmp.sql
+SELECT DISTINCT '@&EMSAAS_SQL_ROOT/1.10.0/upgrade_impl_dml.sql ' || TENANT_ID  FROM EMS_DASHBOARD ORDER BY '@&EMSAAS_SQL_ROOT/1.10.0/upgrade_impl_dml.sql ' || TENANT_ID ;
+SPOOL OFF
+SPOOL ON
+SET HEADING ON
+SET FEEDBACK ON
+WHENEVER SQLERROR EXIT ROLLBACK
+
+@&EMSAAS_SQL_ROOT/1.10.0/upgrade_impl_dml_tmp.sql
--- dashboards-schema/src/main/sql/1.9.0/emaas_dashboards_seed_data.sql
+++ dashboards-schema/src/main/sql/1.9.0/emaas_dashboards_seed_data.sql
@@ -21,27 +21,6 @@ DEFINE TENANT_ID = '&1'
 SET FEEDBACK ON
 SET SERVEROUTPUT ON
 
-DECLARE
-  V_COUNT number;
-BEGIN
-
-SELECT COUNT(1) INTO V_COUNT FROM EMS_DASHBOARD_TILE WHERE TENANT_ID='&TENANT_ID' AND PROVIDER_NAME='LoganService';
-IF (V_COUNT>0) THEN
-  UPDATE EMS_DASHBOARD_TILE SET PROVIDER_NAME='LogAnalyticsUI' WHERE TENANT_ID='&TENANT_ID' AND PROVIDER_NAME='LoganService';
-  COMMIT;
-  DBMS_OUTPUT.PUT_LINE('Provider name of LogAnalytics has been upgraded from [LoganService] to [LogAnalyticsUI] successfully for tenant: &TENANT_ID ! Upgraded records: '||V_COUNT);
-ELSE
-  DBMS_OUTPUT.PUT_LINE('Provider name of LogAnalytics has been upgraded from [LoganService] to [LogAnalyticsUI] for tenant: &TENANT_ID before, no need to upgrade again');
-END IF;
-
-EXCEPTION
-  WHEN OTHERS THEN
-    ROLLBACK;
-    DBMS_OUTPUT.PUT_LINE('Failed to upgrade provider name of LogAnalytics from [LoganService] to [LogAnalyticsUI] for tenant: &TENANT_ID due to '||SQLERRM);
-    RAISE;
-END;
-/
-
 --REM Add one ITA OOB Resource Analytics - Host
 DECLARE
   V_COUNT NUMBER;
