8ac8adb196466484a87933a7d90643885cf7dc06
ping.w.wu@oracle.com <pingwu@slc09wob.us.oracle.com>
2016-08-15 05:05:21 -0700

    move the logic of deleting tables to 1.10.0

---
+++ dashboards-schema/src/main/sql/1.10.0/emaas_create_tables.sql
@@ -0,0 +1,54 @@
+Rem
+Rem
+Rem emaas_create_tables.sql
+Rem
+Rem
+Rem    NAME
+Rem      emaas_create_tables.sql
+Rem
+Rem    DESCRIPTION
+Rem      create the two tables ems_dashboards_favorite and ems_dashboards_last_access at the beginning of onboarding 
+Rem		as the old versions need them;
+Rem
+Rem    NOTES
+Rem      None
+Rem
+Rem    MODIFIED   (MM/DD/YY)
+Rem    pamela   8/15/1016  Created
+
+SET FEEDBACK ON
+SET SERVEROUTPUT ON
+
+DECLARE
+  v_count       NUMBER;
+BEGIN
+	--create EMS_DASHBOARD_FAVORITE 
+  SELECT COUNT(*) INTO v_count FROM user_tab_columns WHERE table_name='EMS_DASHBOARD_FAVORITE';
+  IF v_count = 0 THEN
+    EXECUTE IMMEDIATE 'CREATE TABLE EMS_DASHBOARD_FAVORITE (
+    USER_NAME          VARCHAR2(128) NOT NULL,
+    DASHBOARD_ID       NUMBER(*,0) NOT NULL,
+    CREATION_DATE      TIMESTAMP NOT NULL,
+    TENANT_ID          NUMBER(*,0) NOT NULL,
+    CONSTRAINT EMS_DASHBOARD_FAVORITE_PK PRIMARY KEY (USER_NAME, DASHBOARD_ID, TENANT_ID) USING INDEX,
+    CONSTRAINT EMS_DASHBOARD_FAVORITE_FK1 FOREIGN KEY (DASHBOARD_ID, TENANT_ID) REFERENCES EMS_DASHBOARD (DASHBOARD_ID, TENANT_ID)
+  )';
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('Schema object: EMS_DASHBOARD_FAVORITE exists');      
+  END IF;
+  
+  --create EMS_DASHBOARD_LAST_ACCESS
+  SELECT COUNT(*) INTO v_count FROM user_tab_columns WHERE table_name='EMS_DASHBOARD_LAST_ACCESS';
+  IF v_count = 0 THEN
+    EXECUTE IMMEDIATE 'CREATE TABLE EMS_DASHBOARD_LAST_ACCESS (
+    DASHBOARD_ID   NUMBER(*,0) NOT NULL,
+    ACCESSED_BY VARCHAR2(128) NOT NULL, 
+    ACCESS_DATE TIMESTAMP NOT NULL,
+    TENANT_ID     NUMBER(*,0) NOT NULL,
+    CONSTRAINT EMS_DASHBOARD_LAST_ACCESS_PK PRIMARY KEY (DASHBOARD_ID, ACCESSED_BY,TENANT_ID) USING INDEX
+   )';
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('Schema object: EMS_DASHBOARD_LAST_ACCESS exists');      
+  END IF;  
+END;
+/
--- dashboards-schema/src/main/sql/emaas_dashboards_tenant_onboarding.sql
+++ dashboards-schema/src/main/sql/emaas_dashboards_tenant_onboarding.sql
@@ -27,7 +27,6 @@ DEFINE EMSAAS_SQL_ROOT = '&2'
 DECLARE
   oob_dsb_count NUMBER;
   Valid_Input   NUMBER;
-  v_count       NUMBER;
 BEGIN
   -- If the count of OOB dashboards is ZERO then insert the OOB serches , otherwise do nothing
   -- for given tenant.
@@ -45,41 +44,11 @@ BEGIN
   IF oob_dsb_count    >0 THEN
     DBMS_OUTPUT.PUT_LINE('OOB dashboards for &TENANT_ID are already present');
     RAISE_APPLICATION_ERROR(-20000, ' OOB dashboards for &TENANT_ID are already present');
-  END IF;
-  
-  BEGIN
-	--create EMS_DASHBOARD_FAVORITE 
-  SELECT COUNT(*) INTO v_count FROM user_tab_columns WHERE table_name='EMS_DASHBOARD_FAVORITE';
-  IF v_count = 0 THEN
-    EXECUTE IMMEDIATE 'CREATE TABLE EMS_DASHBOARD_FAVORITE (
-    USER_NAME          VARCHAR2(128) NOT NULL,
-    DASHBOARD_ID       NUMBER(*,0) NOT NULL,
-    CREATION_DATE      TIMESTAMP NOT NULL,
-    TENANT_ID          NUMBER(*,0) NOT NULL,
-    CONSTRAINT EMS_DASHBOARD_FAVORITE_PK PRIMARY KEY (USER_NAME, DASHBOARD_ID, TENANT_ID) USING INDEX,
-    CONSTRAINT EMS_DASHBOARD_FAVORITE_FK1 FOREIGN KEY (DASHBOARD_ID, TENANT_ID) REFERENCES EMS_DASHBOARD (DASHBOARD_ID, TENANT_ID)
-  )';
-  ELSE
-    DBMS_OUTPUT.PUT_LINE('Schema object: EMS_DASHBOARD_FAVORITE exists');      
-  END IF;
-  
-  --create EMS_DASHBOARD_LAST_ACCESS
-  SELECT COUNT(*) INTO v_count FROM user_tab_columns WHERE table_name='EMS_DASHBOARD_LAST_ACCESS';
-  IF v_count = 0 THEN
-    EXECUTE IMMEDIATE 'CREATE TABLE EMS_DASHBOARD_LAST_ACCESS (
-    DASHBOARD_ID   NUMBER(*,0) NOT NULL,
-    ACCESSED_BY VARCHAR2(128) NOT NULL, 
-    ACCESS_DATE TIMESTAMP NOT NULL,
-    TENANT_ID     NUMBER(*,0) NOT NULL,
-    CONSTRAINT EMS_DASHBOARD_LAST_ACCESS_PK PRIMARY KEY (DASHBOARD_ID, ACCESSED_BY,TENANT_ID) USING INDEX
-   )';
-  ELSE
-    DBMS_OUTPUT.PUT_LINE('Schema object: EMS_DASHBOARD_LAST_ACCESS exists');      
-  END IF;  
-  END;
-  
+  END IF; 
 END;
 /
+@&EMSAAS_SQL_ROOT/1.10.0/emaas_create_tables.sql
+
 @&EMSAAS_SQL_ROOT/1.0.0/emaas_dashboards_seed_data.sql &TENANT_ID
 
 @&EMSAAS_SQL_ROOT/1.0.25/emaas_dashboards_seed_data.sql &TENANT_ID
@@ -109,32 +78,11 @@ END;
 @&EMSAAS_SQL_ROOT/1.9.0/emaas_dashboards_seed_data_uigallery.sql &TENANT_ID
 
 @&EMSAAS_SQL_ROOT/1.10.0/emaas_dashboards_seed_data_ta.sql &TENANT_ID
+
+@&EMSAAS_SQL_ROOT/1.10.0/emaas_drop_tables.sql
 COMMIT;
 /
 BEGIN
   DBMS_OUTPUT.PUT_LINE('Inserting OOB dashboards for &TENANT_ID is completed');
 END;
 /
-
-DECLARE
-  v_count       NUMBER;
-BEGIN
-  --remove EMS_DASHBOARD_FAVORITE 
-  SELECT COUNT(*) INTO v_count FROM user_tab_columns WHERE table_name='EMS_DASHBOARD_FAVORITE';
-  IF v_count > 0 THEN
-    EXECUTE IMMEDIATE 'DROP TABLE EMS_DASHBOARD_FAVORITE PURGE';
-  ELSE
-    DBMS_OUTPUT.PUT_LINE('Schema object: EMS_DASHBOARD_FAVORITE does not exist, no change is needed');      
-  END IF;
-  
-  --remove EMS_DASHBOARD_LAST_ACCESS
-  SELECT COUNT(*) INTO v_count FROM user_tab_columns WHERE table_name='EMS_DASHBOARD_LAST_ACCESS';
-  IF v_count > 0 THEN
-    EXECUTE IMMEDIATE 'DROP TABLE EMS_DASHBOARD_LAST_ACCESS PURGE';
-  ELSE
-    DBMS_OUTPUT.PUT_LINE('Schema object: EMS_DASHBOARD_LAST_ACCESS does not exist, no change is needed');      
-  END IF;
-  
-END;
-/
-
