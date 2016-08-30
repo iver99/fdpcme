aab3e3fa3bbed7d112c89803f3604b535c9101cd
Shangping Wang <shangping.wang@oracle.com>
2016-08-30 00:39:10 -0700

    fix compile error

--- omc-ui-framework/omc-ui-framework-web/src/integTest/java/oracle/sysman/emaas/platform/uifwk/dashboardscommonui/test/ui/TestDashboardPage.java
+++ omc-ui-framework/omc-ui-framework-web/src/integTest/java/oracle/sysman/emaas/platform/uifwk/dashboardscommonui/test/ui/TestDashboardPage.java
@@ -16,6 +16,8 @@ import oracle.sysman.emaas.platform.dashboards.tests.ui.util.WaitUtil;
 import oracle.sysman.qatool.uifwk.webdriver.WebDriver;
 import oracle.sysman.qatool.uifwk.webdriver.WebDriverUtils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
