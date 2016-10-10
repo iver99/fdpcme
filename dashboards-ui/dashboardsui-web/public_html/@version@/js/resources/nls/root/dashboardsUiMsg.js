7515376d600a6a0dd30776bd71b51b38a02645f8
xia_dai <xiaoyuan.d.dai@oracle.com>
2016-10-10 11:54:11 +0800

    EMCPDF-2324

--- dashboards-ui/dashboardsui-web/public_html/@version@/js/resources/nls/root/dashboardsUiMsg.js
+++ dashboards-ui/dashboardsui-web/public_html/@version@/js/resources/nls/root/dashboardsUiMsg.js
@@ -231,7 +231,7 @@ define({
       "DBS_BUILDER_MSG_ERROR_NAME_DUPLICATED_DETAIL": "Provide a unique name.",
       "DBS_BUILDER_MSG_AUTO_REFRESH_ON": "Auto-refresh enabled.",
       "DBS_BUILDER_MSG_AUTO_REFRESH_OFF": "Auto-refresh disabled.",
-
+      "DBS_BUILDER_MSG_AUTO_OPEN_IN":"Open in ",
       // DO NOT DELETE since error page will use it as a parameter in URL
       // -----start
       "DBS_ERROR_PAGE_TITLE":"Error",
