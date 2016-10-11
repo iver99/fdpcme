d1719beef172424667c8115d7b0ef4d149956f76
ping.w.wu@oracle.com <pingwu@slc09wob.us.oracle.com>
2016-10-11 02:03:41 -0700

    update a dml file

--- dashboards-schema/src/main/sql/1.12.0/upgrade_impl_dml.sql
+++ dashboards-schema/src/main/sql/1.12.0/upgrade_impl_dml.sql
@@ -1,2 +1,17 @@
-REM --
+Rem --DML change during upgrade
+Rem
+Rem upgrade_impl_dml.sql
+Rem
+Rem Copyright (c) 2013, 2014, 2015, 2016 Oracle and/or its affiliates.
+Rem All rights reserved.
+Rem
+Rem    NAME
+Rem      upgrade_impl_dml.sql 
+Rem
+Rem    DESCRIPTION
+Rem      DML change during upgrade
+Rem
+Rem    NOTES
+Rem      None
+Rem
 
