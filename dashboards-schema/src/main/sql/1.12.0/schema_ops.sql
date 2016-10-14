85cd50d01ff71a8e1ef96103e58efda418e51764
ping.w.wu@oracle.com <pingwu@slc09wob.us.oracle.com>
2016-10-14 04:19:19 -0700

    a typo

--- dashboards-schema/src/main/sql/1.12.0/schema_ops.sql
+++ dashboards-schema/src/main/sql/1.12.0/schema_ops.sql
@@ -5,10 +5,10 @@ Rem 10/11/2016	pingwu  	Created file
 Rem -------------------------------------------
 Rem ----------------------------------------------------------------
 
-@&EMSAAS_SQL_ROOT/1.11.0/upgrade_impl_ddl.sql
+@&EMSAAS_SQL_ROOT/1.12.0/upgrade_impl_ddl.sql
 
 SET HEADING OFF
 SET FEEDBACK OFF
 SET LINESIZE 2000
 
-@&EMSAAS_SQL_ROOT/1.11.0/upgrade_impl_dml.sql
+@&EMSAAS_SQL_ROOT/1.12.0/upgrade_impl_dml.sql
