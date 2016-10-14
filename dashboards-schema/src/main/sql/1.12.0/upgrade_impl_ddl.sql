dc6568500c35ee54073fa39451a63c2711db6f73
ping.w.wu@oracle.com <pingwu@slc09wob.us.oracle.com>
2016-10-14 04:49:50 -0700

    move Rex's schema change to 1.12.0

--- dashboards-schema/src/main/sql/1.12.0/upgrade_impl_ddl.sql
+++ dashboards-schema/src/main/sql/1.12.0/upgrade_impl_ddl.sql
@@ -1,25 +1,40 @@
-Rem --DDL change during upgrade
+Rem    DDL change during upgrade
+Rem    add column CREATION_DATE & LAST_MODIFICATION_DATE to all tables
+Rem    MODIFIED   (MM/DD/YY)
+Rem    Rex Liang    06/29/2016 - created
 Rem
-Rem upgrade_impl_ddl.sql
 Rem
-Rem Copyright (c) 2013, 2014, 2015, 2016 Oracle and/or its affiliates.
-Rem All rights reserved.
-Rem
-Rem    NAME
-Rem      upgrade_impl_ddl.sql 
-Rem
-Rem    DESCRIPTION
-Rem      DDL change during upgrade
-Rem
-Rem    NOTES
-Rem      None
-Rem
-
 
 DECLARE
   v_count     INTEGER;
 BEGIN
-
+  --alter column EMS_DASHBOARD_TILE.TILE_ID
+  SELECT COUNT(*) INTO v_count FROM user_tab_columns WHERE table_name='EMS_DASHBOARD_TILE' AND column_name='TILE_ID';
+  IF v_count>0 THEN
+    EXECUTE IMMEDIATE 'alter table EMS_DASHBOARD_TILE drop constraint EMS_DASHBOARD_TILE_PK cascade drop index';
+    EXECUTE IMMEDIATE 'alter table EMS_DASHBOARD_TILE rename column TILE_ID to TEMP_ID';
+    EXECUTE IMMEDIATE 'alter table EMS_DASHBOARD_TILE add TILE_ID varchar2(63)';
+    EXECUTE IMMEDIATE 'update EMS_DASHBOARD_TILE set TILE_ID = to_char(TEMP_ID)';
+    EXECUTE IMMEDIATE 'alter table EMS_DASHBOARD_TILE drop column TEMP_ID';
+    EXECUTE IMMEDIATE 'alter table EMS_DASHBOARD_TILE add constraint EMS_DASHBOARD_TILE_PK primary key(TILE_ID, TENANT_ID) USING INDEX';
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('Schema object: EMS_DASHBOARD_TILE.TILE_ID does not exist, no change is needed');
+  END IF;
+  
+  --alter column EMS_DASHBOARD_TILE_PARAMS.TILE_ID
+  SELECT COUNT(*) INTO v_count FROM user_tab_columns WHERE table_name='EMS_DASHBOARD_TILE_PARAMS' AND column_name='TILE_ID';
+  IF v_count>0 THEN
+    EXECUTE IMMEDIATE 'alter table EMS_DASHBOARD_TILE_PARAMS drop constraint EMS_DASHBOARD_TILE_PARAMS_PK cascade drop index';
+    EXECUTE IMMEDIATE 'alter table EMS_DASHBOARD_TILE_PARAMS rename column TILE_ID to TEMP_ID';
+    EXECUTE IMMEDIATE 'alter table EMS_DASHBOARD_TILE_PARAMS add TILE_ID varchar2(63)';
+    EXECUTE IMMEDIATE 'update EMS_DASHBOARD_TILE_PARAMS set TILE_ID = to_char(TEMP_ID)';
+    EXECUTE IMMEDIATE 'alter table EMS_DASHBOARD_TILE_PARAMS drop column TEMP_ID';
+    EXECUTE IMMEDIATE 'alter table EMS_DASHBOARD_TILE_PARAMS add constraint EMS_DASHBOARD_TILE_PARAMS_PK primary key(TILE_ID, TENANT_ID, PARAM_NAME) USING INDEX';
+    EXECUTE IMMEDIATE 'alter table EMS_DASHBOARD_TILE_PARAMS add constraint EMS_DASHBOARD_TILE_PARAMS_FK1 FOREIGN KEY (TILE_ID, TENANT_ID) REFERENCES EMS_DASHBOARD_TILE (TILE_ID, TENANT_ID)';
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('Schema object: EMS_DASHBOARD_TILE_PARAMS.TILE_ID does not exist, no change is needed');
+  END IF;
+  
   --add new column 'EMS_DASHBOARD_SET.CREATION_DATE'
   SELECT COUNT(*) INTO v_count FROM user_tab_columns WHERE table_name='EMS_DASHBOARD_SET' AND column_name='CREATION_DATE';
   IF v_count=0 THEN
@@ -83,7 +98,8 @@ BEGIN
   ELSE
     DBMS_OUTPUT.PUT_LINE('Schema object: EMS_PREFERENCE.LAST_MODIFICATION_DATE exists already, no change is needed');
   END IF;
-
+  
+  
  --add new column 'DELETED' for EMS_DASHBOARD_SET
   SELECT count(*) into v_count from user_tab_columns WHERE table_name='EMS_DASHBOARD_SET' AND column_name='DELETED';
   IF v_count=0 THEN
@@ -125,7 +141,6 @@ BEGIN
   END IF;
 END;
 /
-
 
 BEGIN
   EXECUTE IMMEDIATE 'ALTER TRIGGER EMS_DASHBOARD_TR DISABLE';
