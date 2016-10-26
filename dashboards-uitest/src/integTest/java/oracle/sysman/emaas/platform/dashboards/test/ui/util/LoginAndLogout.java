f6e8cf4105e4f95b6cef6b13dee5d433cf40ec1b
Jiangxia Nan <jiangxia.nan@oracle.com>
2016-10-26 19:48:02 +0800

    add param for login and logout

--- dashboards-uitest/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/ui/util/LoginAndLogout.java
+++ dashboards-uitest/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/ui/util/LoginAndLogout.java
@@ -94,8 +94,14 @@ public class LoginAndLogout
 		webd.getLogger().info("url is " + url + "   properties file is " + testPropertiesFile);
 		webd.getLogger().info("after::start to test in LoginAndOut");
 		// if the ui have been login, do not login ,again
-		if (!webd.getWebDriver().getCurrentUrl().equals(url)) {
-
+//		if (!webd.getWebDriver().getCurrentUrl().equals(url)) {
+		if (!webd.getWebDriver().getCurrentUrl().equals(url)
+				&& !webd.getWebDriver().getCurrentUrl().contains("omcCtx=")) {
+			//Append omc context into login url
+			if (!url.contains("omcCtx=")) {
+				url = url + (url.indexOf("?") > 0 ? "&" : "?") + "omcCtx=timePeriod%3DLast%201%20day&compositeName%3DRideshare%20Application&compositeMEID%3DEE380645A9711FFF881A146A00C98324";
+				webd.getLogger().info("New url with OMC global context appended is: " + url);
+			}
 			LoginUtils.doLogin(webd, username, password, tenantId, url);
 		}
 	}
@@ -142,8 +148,14 @@ public class LoginAndLogout
 		//		webd.getLogger().info("url is " + url + "   properties file is " + testPropertiesFile);
 		webd.getLogger().info("after::start to test in LoginAndOut");
 		// if the ui have been login, do not login ,again
-		if (!webd.getWebDriver().getCurrentUrl().equals(url)) {
-
+//		if (!webd.getWebDriver().getCurrentUrl().equals(url)) {
+		if (!webd.getWebDriver().getCurrentUrl().equals(url)
+				&& !webd.getWebDriver().getCurrentUrl().contains("omcCtx=")) {
+			//Append omc context into login url
+			if (!url.contains("omcCtx=")) {
+				url = url + (url.indexOf("?") > 0 ? "&" : "?") + "omcCtx=timePeriod%3DLast%201%20day&compositeName%3DRideshare%20Application&compositeMEID%3DEE380645A9711FFF881A146A00C98324";
+				webd.getLogger().info("New url with OMC global context appended is: " + url);
+			}
 			LoginUtils.doLogin(webd, username, password, tenantID, url);
 		}
 
