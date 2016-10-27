0bb691aab1b193931f6cd21ce2f6a9cb81d06d53
rex.liang <rex.liang@oracle.com>
2016-10-27 00:12:22 -0700

    EMCPDF-2505: set html lang by using browser's settings

--- dashboards-ui/dashboardsui-web/public_html/@version@/js/homeMain.js
+++ dashboards-ui/dashboardsui-web/public_html/@version@/js/homeMain.js
@@ -128,7 +128,6 @@ require(['dashboards/dbsmodel',
                 // TODO: Will need to change this to warning, once we figure out the level of our current log calls.
                 // If you comment the line below, our current log calls will not be output!
                 logger.setLogLevel(oj.Logger.LEVEL_WARN);
-        
             window.onerror = function (msg, url, lineNo, columnNo, error)
             {
                 var msg = "Accessing " + url + " failed. " + "Error message: " + msg + ". Line: " + lineNo + ". Column: " + columnNo;
@@ -162,6 +161,7 @@ require(['dashboards/dbsmodel',
             ko.virtualElements.allowedBindings.stopBinding = true;
 
             var dfu_model = new dfumodel(dfu.getUserName(), dfu.getTenantName());
+            dfu_model.setHtmlLang();
 
             function HeaderViewModel() {
                 var self = this;
