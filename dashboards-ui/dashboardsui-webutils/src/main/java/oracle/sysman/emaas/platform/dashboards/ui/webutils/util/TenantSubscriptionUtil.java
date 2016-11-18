4af85e50ebcce6ccbf90e8bb867cbd56dd8415ed
Guobao Chen <guobao.chen@oracle.com>
2016-11-18 15:03:22 +0800

    sync with nov21, introduce cache for ui side

--- dashboards-ui/dashboardsui-webutils/src/main/java/oracle/sysman/emaas/platform/dashboards/ui/webutils/util/TenantSubscriptionUtil.java
+++ dashboards-ui/dashboardsui-webutils/src/main/java/oracle/sysman/emaas/platform/dashboards/ui/webutils/util/TenantSubscriptionUtil.java
@@ -28,6 +28,8 @@ import oracle.sysman.emaas.platform.dashboards.ui.webutils.json.DomainEntity;
 import oracle.sysman.emaas.platform.dashboards.ui.webutils.json.DomainsEntity;
 import oracle.sysman.emaas.platform.dashboards.ui.webutils.util.LogUtil.InteractionLogDirection;
 
+import oracle.sysman.emaas.platform.dashboards.ui.webutils.util.subscription.SubscribedAppCacheUtil;
+import oracle.sysman.emaas.platform.dashboards.ui.webutils.util.subscription.SubscribedApps;
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -78,87 +80,43 @@ public class TenantSubscriptionUtil
 	private static Logger LOGGER = LogManager.getLogger(TenantSubscriptionUtil.class);
 	private static Logger itrLogger = LogUtil.getInteractionLogger();
 
-	public static List<String> getTenantSubscribedServices(String tenant)
-	{
+	public static List<String> getTenantSubscribedServices(String tenant) {
 		if (tenant == null) {
 			return Collections.emptyList();
 		}
-		Link domainLink = RegistryLookupUtil.getServiceInternalLink("EntityNaming", "1.0+", "collection/domains", tenant);
-		if (domainLink == null || StringUtils.isEmpty(domainLink.getHref())) {
+
+		long startTime = System.currentTimeMillis();
+		SubscribedAppCacheUtil cache = SubscribedAppCacheUtil.getInstance();
+		List<String> apps = cache.get(tenant);
+		if (apps != null) {
+			LOGGER.info("Retrieved subscribed app information from cache");
+			return apps;
+		}
+
+		// instead of retrieving subscribed apps from entitynaming, we get that from dashboard api
+		Link subAppLink = RegistryLookupUtil.getServiceInternalLink("Dashboard-API", "1.0+", "static/dashboards.subscribedapps", null);
+		if (subAppLink == null || StringUtils.isEmpty(subAppLink.getHref())) {
 			LOGGER.warn("Checking tenant (" + tenant
-					+ ") subscriptions: null/empty entity naming service collection/domains is retrieved.");
+					+ ") subscriptions: null/empty subscribedapp link retrieved from dashboard-api.");
 			return Collections.emptyList();
 		}
-		LOGGER.info("Checking tenant (" + tenant + ") subscriptions. The entity naming href is " + domainLink.getHref());
-		String domainHref = domainLink.getHref();
+		LOGGER.info("Checking tenant (" + tenant + ") subscriptions. Subscribedapp link retrieved from dashboard-api href is " + subAppLink.getHref());
+		String subAppHref = subAppLink.getTypesStr();
 		RestClient rc = new RestClient();
-		String domainsResponse = rc.get(domainHref, tenant);
-		LOGGER.info("Checking tenant (" + tenant + ") subscriptions. Domains list response is " + domainsResponse);
+		String subAppResponse = rc.get(subAppHref, tenant);
+		LOGGER.info("Checking tenant (" + tenant + ") subscriptions. Dashboard-API subscribed app response is " + subAppResponse);
 		JsonUtil ju = JsonUtil.buildNormalMapper();
 		try {
-			DomainsEntity de = ju.fromJson(domainsResponse, DomainsEntity.class);
-			if (de == null || de.getItems() == null || de.getItems().isEmpty()) {
-				LOGGER.warn("Checking tenant (" + tenant
-						+ ") subscriptions: null/empty domains entity or domains item retrieved.");
+			SubscribedApps sa = ju.fromJson(subAppResponse, SubscribedApps.class);
+			if (sa == null || sa.getApplications() == null || sa.getApplications().length <= 0) {
+				LOGGER.info("Checking tenant (" + tenant + ") subscriptions. Dashboard-API subscribed app application is null or empty");
+				cache.remove(tenant);
 				return Collections.emptyList();
 			}
-			String tenantAppUrl = null;
-			for (DomainEntity domain : de.getItems()) {
-				if ("TenantApplicationMapping".equals(domain.getDomainName())) {
-					tenantAppUrl = domain.getCanonicalUrl();
-					break;
-				}
-			}
-			if (tenantAppUrl == null || "".equals(tenantAppUrl)) {
-				LOGGER.warn("Checking tenant (" + tenant + ") subscriptions. 'TenantApplicationMapping' not found");
-				return Collections.emptyList();
-			}
-			String appMappingUrl = tenantAppUrl + "/lookups?opcTenantId=" + tenant;
-			LOGGER.info("Checking tenant (" + tenant + ") subscriptions. tenant application mapping lookup URL is "
-					+ appMappingUrl);
-			String appMappingJson = rc.get(appMappingUrl, tenant);
-			LOGGER.info("Checking tenant (" + tenant + ") subscriptions. application lookup response json is " + appMappingJson);
-			if (appMappingJson == null || "".equals(appMappingJson)) {
-				return Collections.emptyList();
-			}
-			AppMappingCollection amec = ju.fromJson(appMappingJson, AppMappingCollection.class);
-			if (amec == null || amec.getItems() == null || amec.getItems().isEmpty()) {
-				LOGGER.error("Checking tenant (" + tenant + ") subscriptions. Empty application mapping items are retrieved");
-				return Collections.emptyList();
-			}
-			AppMappingEntity ame = null;
-			for (AppMappingEntity entity : amec.getItems()) {
-				if (entity.getValues() == null) {
-					continue;
-				}
-				for (AppMappingEntity.AppMappingValue amv : entity.getValues()) {
-					if (tenant.equals(amv.getOpcTenantId())) {
-						ame = entity;
-						break;
-					}
-
-				}
-			}
-			if (ame == null || ame.getValues() == null || ame.getValues().isEmpty()) {
-				LOGGER.error("Checking tenant (" + tenant
-						+ ") subscriptions. Failed to get an application mapping for the specified tenant");
-				return Collections.emptyList();
-			}
-			String apps = null;
-			for (AppMappingEntity.AppMappingValue amv : ame.getValues()) {
-				if (tenant.equals(amv.getOpcTenantId())) {
-					apps = amv.getApplicationNames();
-					break;
-				}
-			}
-			LOGGER.info("Checking tenant (" + tenant + ") subscriptions. applications for the tenant are " + apps);
-			if (apps == null || "".equals(apps)) {
-				return Collections.emptyList();
-			}
-			List<String> origAppsList = Arrays.asList(apps
-					.split(ApplicationEditionConverter.APPLICATION_EDITION_ELEMENT_DELIMINATOR));
-			return origAppsList;
-
+			apps = Arrays.asList(sa.getApplications());
+			cache.put(tenant, apps);
+			LOGGER.info("Getting subscribed app from dashboard-api: {} ms", System.currentTimeMillis() - startTime);
+			return apps;
 		}
 		catch (IOException e) {
 			LOGGER.error(e);
---
+++ dashboards-ui/dashboardsui-webutils/src/main/java/oracle/sysman/emaas/platform/dashboards/ui/webutils/util/subscription/SubscribedAppCacheUtil.java
@@ -0,0 +1,119 @@
+package oracle.sysman.emaas.platform.dashboards.ui.webutils.util.subscription;
+
+import oracle.sysman.emaas.platform.dashboards.ui.webutils.util.StringUtil;
+import oracle.sysman.emaas.platform.dashboards.ui.webutils.util.TenantSubscriptionUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+/**
+ * Created by guochen on 11/18/16.
+ */
+public class SubscribedAppCacheUtil {
+    private static Logger LOGGER = LogManager.getLogger(TenantSubscriptionUtil.class);
+
+    private static final SubscribedAppCacheUtil INSTANCE = new SubscribedAppCacheUtil();
+
+    // each subscribed app will live untime expiry time is reached
+    private static final long EXPIRY_TIME = 60000L;
+
+    // let's set the capacity for the cache. Not that an known issue is, expired cached object may be inside cache still
+    private static final int CACHE_CAPACITY = 800;
+
+    private static class CachedObj {
+        private long timeStamp;
+        private List<String> obj;
+
+        public CachedObj(List<String> obj) {
+            timeStamp = System.currentTimeMillis();
+            this.obj = obj;
+        }
+
+        public List<String> getObject() {
+            return obj;
+        }
+
+        public void setObject(List<String> obj) {
+            this.obj = obj;
+        }
+
+        public long getTimestamp() {
+            return timeStamp;
+        }
+
+        public void setTimestamp(long timestamp) {
+            this.timeStamp = timestamp;
+        }
+    }
+
+    private LinkedHashMap<String,CachedObj> container = new LinkedHashMap<String,CachedObj>(CACHE_CAPACITY);
+
+    public static final SubscribedAppCacheUtil getInstance() {
+        return INSTANCE;
+    }
+
+    private SubscribedAppCacheUtil() {
+
+    }
+
+    public List<String> put(String tenant, List<String> value) {
+        if (StringUtil.isEmpty(tenant)) {
+            LOGGER.error("Failed to put value {} into cache, as specified tenant is empty", value == null? null: value.toString());
+            return value;
+        }
+        if (value == null || value.isEmpty()) {
+            LOGGER.warn("Did not put value into cache for tenant {}, as specified value is empty", tenant);
+            return value;
+        }
+        CachedObj co = new CachedObj(value);
+        container.put(tenant, co);
+        return value;
+    }
+
+    public List<String> get(String tenant) {
+        if (StringUtil.isEmpty(tenant)) {
+            LOGGER.error("Failed to get value for tenant as it is null or empty: {}", tenant);
+            return null;
+        }
+        CachedObj co = container.get(tenant);
+        if (co == null)
+            return null;
+        long startTime = co.getTimestamp();
+        if (!isValidValue(co)) {
+            container.remove(tenant);
+            return null;
+        }
+        List<String> value = co.getObject();
+        return value;
+    }
+
+    public List<String> remove(String tenant) {
+        if (StringUtil.isEmpty(tenant)) {
+            LOGGER.error("Failed to remove value for tenant as tenant is null or empty: {}", tenant);
+            return null;
+        }
+        if (container.containsKey(tenant)) {
+            CachedObj co =  container.remove(tenant);
+            if (isValidValue(co))
+                return co.getObject();
+            else
+                return null;
+        }
+        return null;
+    }
+
+    private boolean isValidValue(CachedObj co) {
+        if (co == null) {
+            return false;
+        }
+        long now = System.currentTimeMillis();
+        if (co.getTimestamp() > now || System.currentTimeMillis() - co.getTimestamp() > EXPIRY_TIME) {
+            LOGGER.debug("Invalid cached object: start time is too late, or it expires for timestamp: {}", co.getTimestamp());
+            return false;
+        }
+        else
+            return true;
+    }
+}
