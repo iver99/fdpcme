a5a065b725c4ffa2580126fcf21c29b21743909d
Guobao Chen <guobao.chen@oracle.com>
2016-11-19 19:35:27 +0800

    add exception handling for dashboard data retrieval

--- dashboards-ui/dashboardsui-webutils/src/main/java/oracle/sysman/emaas/platform/dashboards/ui/webutils/util/DashboardDataAccessUtil.java
+++ dashboards-ui/dashboardsui-webutils/src/main/java/oracle/sysman/emaas/platform/dashboards/ui/webutils/util/DashboardDataAccessUtil.java
@@ -18,61 +18,76 @@ public class DashboardDataAccessUtil {
     public static String getDashboardData(String tenantIdParam,
                                           String userTenant, String referer,
                                           long dashboardId) {
-        long start = System.currentTimeMillis();
-        Link dashboardsLink = RegistryLookupUtil.getServiceInternalLink("Dashboard-API", "1.0+", "static/dashboards.service", null);
-        if (dashboardsLink == null || StringUtils.isEmpty(dashboardsLink.getHref())) {
-            LOGGER.warn("Retrieving dashboard data for tenant {}: null/empty dashboardsLink retrieved from service registry.");
+        try {
+            long start = System.currentTimeMillis();
+            Link dashboardsLink = RegistryLookupUtil.getServiceInternalLink("Dashboard-API", "1.0+", "static/dashboards.service", null);
+            if (dashboardsLink == null || StringUtils.isEmpty(dashboardsLink.getHref())) {
+                LOGGER.warn("Retrieving dashboard data for tenant {}: null/empty dashboardsLink retrieved from service registry.");
+                return null;
+            }
+            LOGGER.info("Dashboard REST API from dashboard-api href is: " + dashboardsLink.getHref());
+            String dashboardHref = dashboardsLink.getHref() + "/" + dashboardId;
+            TenantSubscriptionUtil.RestClient rc = new TenantSubscriptionUtil.RestClient();
+            rc.setHeader("X-USER-IDENTITY-DOMAIN-NAME", tenantIdParam);
+            rc.setHeader("X-REMOTE-USER", userTenant);
+            rc.setHeader("Referer", referer);
+            String response = rc.get(dashboardHref, tenantIdParam);
+            LOGGER.info("Retrieved dashboard data is: {}", response);
+            LOGGER.info("It takes {}ms to retrieve dashboard data from Dashboard-API", (System.currentTimeMillis() - start));
+            return response;
+        } catch (Exception e) {
+            LOGGER.error(e.getLocalizedMessage(), e);
             return null;
         }
-        LOGGER.info("Dashboard REST API from dashboard-api href is: " + dashboardsLink.getHref());
-        String dashboardHref = dashboardsLink.getHref() + "/" + dashboardId;
-        TenantSubscriptionUtil.RestClient rc = new TenantSubscriptionUtil.RestClient();
-        rc.setHeader("X-USER-IDENTITY-DOMAIN-NAME", tenantIdParam);
-        rc.setHeader("X-REMOTE-USER", userTenant);
-        rc.setHeader("Referer", referer);
-        String response = rc.get(dashboardHref, tenantIdParam);
-        LOGGER.info("Retrieved dashboard data is: {}", response);
-        LOGGER.info("It takes {}ms to retrieve dashboard data from Dashboard-API", (System.currentTimeMillis() - start));
-        return response;
     }
 
     public static String getUserTenantInfo(String tenantIdParam,
                                           String userTenant, String referer) {
-        long start = System.currentTimeMillis();
-        Link configurationsLink = RegistryLookupUtil.getServiceInternalLink("Dashboard-API", "1.0+", "static/dashboards.configurations", null);
-        if (configurationsLink == null || StringUtils.isEmpty(configurationsLink.getHref())) {
-            LOGGER.warn("Retrieving configurations links for tenant {}: null/empty configurationsLink retrieved from service registry.");
+        try {
+            long start = System.currentTimeMillis();
+            Link configurationsLink = RegistryLookupUtil.getServiceInternalLink("Dashboard-API", "1.0+", "static/dashboards.configurations", null);
+            if (configurationsLink == null || StringUtils.isEmpty(configurationsLink.getHref())) {
+                LOGGER.warn("Retrieving configurations links for tenant {}: null/empty configurationsLink retrieved from service registry.");
+                return null;
+            }
+            LOGGER.info("Configurations REST API link from dashboard-api href is: " + configurationsLink.getHref());
+            String userInfoHref = configurationsLink.getHref() + "/userInfo";
+            TenantSubscriptionUtil.RestClient rc = new TenantSubscriptionUtil.RestClient();
+            rc.setHeader("X-USER-IDENTITY-DOMAIN-NAME", tenantIdParam);
+            rc.setHeader("X-REMOTE-USER", userTenant);
+            rc.setHeader("Referer", referer);
+            String response = rc.get(userInfoHref, tenantIdParam);
+            LOGGER.info("Retrieved userInfo data is: {}", response);
+            LOGGER.info("It takes {}ms to retrieve userInfo data from Dashboard-API", (System.currentTimeMillis() - start));
+            return response;
+        } catch (Exception e) {
+            LOGGER.error(e.getLocalizedMessage(), e);
             return null;
         }
-        LOGGER.info("Configurations REST API link from dashboard-api href is: " + configurationsLink.getHref());
-        String userInfoHref = configurationsLink.getHref() + "/userInfo";
-        TenantSubscriptionUtil.RestClient rc = new TenantSubscriptionUtil.RestClient();
-        rc.setHeader("X-USER-IDENTITY-DOMAIN-NAME", tenantIdParam);
-        rc.setHeader("X-REMOTE-USER", userTenant);
-        rc.setHeader("Referer", referer);
-        String response = rc.get(userInfoHref, tenantIdParam);
-        LOGGER.info("Retrieved userInfo data is: {}", response);
-        LOGGER.info("It takes {}ms to retrieve userInfo data from Dashboard-API", (System.currentTimeMillis() - start));
-        return response;
     }
 
     public static String getRegistrationData(String tenantIdParam,
                                            String userTenant, String referer) {
-        long start = System.currentTimeMillis();
-        Link configurationsLink = RegistryLookupUtil.getServiceInternalLink("Dashboard-API", "1.0+", "static/dashboards.configurations", null);
-        if (configurationsLink == null || StringUtils.isEmpty(configurationsLink.getHref())) {
-            LOGGER.warn("Retrieving configurations links for tenant {}: null/empty configurationsLink retrieved from service registry.");
+        try {
+            long start = System.currentTimeMillis();
+            Link configurationsLink = RegistryLookupUtil.getServiceInternalLink("Dashboard-API", "1.0+", "static/dashboards.configurations", null);
+            if (configurationsLink == null || StringUtils.isEmpty(configurationsLink.getHref())) {
+                LOGGER.warn("Retrieving configurations links for tenant {}: null/empty configurationsLink retrieved from service registry.");
+                return null;
+            }
+            LOGGER.info("Configurations REST API link from dashboard-api href is: " + configurationsLink.getHref());
+            String userInfoHref = configurationsLink.getHref() + "/registration";
+            TenantSubscriptionUtil.RestClient rc = new TenantSubscriptionUtil.RestClient();
+            rc.setHeader("X-USER-IDENTITY-DOMAIN-NAME", tenantIdParam);
+            rc.setHeader("X-REMOTE-USER", userTenant);
+            rc.setHeader("Referer", referer);
+            String response = rc.get(userInfoHref, tenantIdParam);
+            LOGGER.info("Retrieved registration data is: {}", response);
+            LOGGER.info("It takes {}ms to retrieve registration data from Dashboard-API", (System.currentTimeMillis() - start));
+            return response;
+        } catch (Exception e) {
+            LOGGER.error(e.getLocalizedMessage(), e);
             return null;
         }
-        LOGGER.info("Configurations REST API link from dashboard-api href is: " + configurationsLink.getHref());
-        String userInfoHref = configurationsLink.getHref() + "/registration";
-        TenantSubscriptionUtil.RestClient rc = new TenantSubscriptionUtil.RestClient();
-        rc.setHeader("X-USER-IDENTITY-DOMAIN-NAME", tenantIdParam);
-        rc.setHeader("X-REMOTE-USER", userTenant);
-        rc.setHeader("Referer", referer);
-        String response = rc.get(userInfoHref, tenantIdParam);
-        LOGGER.info("Retrieved registration data is: {}", response);
-        LOGGER.info("It takes {}ms to retrieve registration data from Dashboard-API", (System.currentTimeMillis() - start));
-        return response;
     }
 }
