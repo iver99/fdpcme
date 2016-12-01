b5d39be99558c39be32c5c612e618717569227fe
chehao <chendong.h.hao@oracle.com>
2016-12-01 11:22:43 +0800

    imcrease emcpdf L0 cc

---
+++ dashboards-core/src/integTest/java/oracle/sysman/emaas/platform/dashboards/core/cache/lru/BinaryTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2016 Oracle
+ * All rights reserved.
+ *
+ * $$File: $$
+ * $$DateTime: $$
+ * $$Author: $$
+ * $$Revision: $$
+ */
+ 
+package oracle.sysman.emaas.platform.dashboards.core.cache.lru;
+
+/**
+ * @author chehao
+ *
+ */
+public class BinaryTest
+{
+
+}
