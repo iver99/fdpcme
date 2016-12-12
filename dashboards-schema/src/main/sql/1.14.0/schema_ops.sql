ebc06705a200e041496c9cdaeac83fc871a06685
rex.liang <rex.liang@oracle.com>
2016-12-12 00:11:43 -0800

    move ZDT's sql to 1.14.0

---
+++ dashboards-schema/src/main/sql/1.14.0/schema_ops.sql
@@ -0,0 +1,12 @@
+Rem ----------------------------------------------------------------
+Rem 12/12/2016	REX	Created file
+Rem 
+Rem ----------------------------------------------------------------
+
+@&EMSAAS_SQL_ROOT/1.14.0/upgrade_impl_ddl.sql
+
+SET HEADING OFF
+SET FEEDBACK OFF
+SET LINESIZE 2000
+
+@&EMSAAS_SQL_ROOT/1.14.0/upgrade_impl_dml.sql
