7b8449140333aeae4a94bdc4db12bf2573558ee8
Shangping Wang <shangping.wang@oracle.com>
2016-12-11 23:53:27 -0800

    re-category cases

---
+++ dashboards-uitest/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/ui/TestBrandingBar_BasicTest.java
@@ -0,0 +1,198 @@
+package oracle.sysman.emaas.platform.dashboards.test.ui;
+
+import oracle.sysman.emaas.platform.dashboards.test.ui.util.DashBoardUtils;
+import oracle.sysman.emaas.platform.dashboards.test.ui.util.LoginAndLogout;
+import oracle.sysman.emaas.platform.dashboards.tests.ui.BrandingBarUtil;
+import oracle.sysman.emaas.platform.dashboards.tests.ui.util.WaitUtil;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @version
+ * @author
+ * @since release specific (what release of product did this appear in)
+ */
+
+public class TestBrandingBar_BasicTest extends LoginAndLogout
+{
+
+	public void initTest(String testName)
+	{
+		login(this.getClass().getName() + "." + testName);
+		DashBoardUtils.loadWebDriver(webd);
+	}
+
+	//@Test
+	public void testAdminConsoleLink()
+	{
+		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
+		webd.getLogger().info("start to test in testAdminConsoleLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		// Administration link for Administration Console UI
+		BrandingBarUtil.visitApplicationAdministration(webd, BrandingBarUtil.NAV_LINK_TEXT_ADMIN_ADMINCONSOLE);
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		//verify the url of opened page
+		DashBoardUtils.verifyURL(webd, "admin-console/ac/adminConsole.html");
+	}
+
+	@Test
+	public void testAdminLink()
+	{
+		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
+		webd.getLogger().info("start to test in testAdminLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+	}
+
+	@Test
+	public void testAgentsLink()
+	{
+		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
+		webd.getLogger().info("start to test in testAgentsLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		// Agents link
+		BrandingBarUtil.visitApplicationAdministration(webd, BrandingBarUtil.NAV_LINK_TEXT_ADMIN_AGENT);
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		//verify the url of opened page
+		DashBoardUtils.verifyURL(webd, "tenantmgmt/services/customersoftware");
+	}
+
+	@Test
+	public void testAPMLink()
+	{
+		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
+		webd.getLogger().info("start to test in testAPMLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		// APM link
+		BrandingBarUtil.visitApplicationCloudService(webd, BrandingBarUtil.NAV_LINK_TEXT_CS_APM);
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		//verify the url of opened page
+		DashBoardUtils.verifyURL(webd, "apmUi/index.html");
+	}
+
+	@Test
+	public void testDashBoardHomeLink()
+	{
+		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
+		webd.getLogger().info("start to test in testDashBoardHomeLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		// dashboardHome link
+		BrandingBarUtil.visitDashboardHome(webd);
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		//verify the url of opened page
+		DashBoardUtils.verifyURL(webd, "emcpdfui/home.html");
+	}
+
+	@Test
+	public void testHomeLink()
+	{
+		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
+		webd.getLogger().info("start to test in testHomeLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		// Home link
+		BrandingBarUtil.visitMyHome(webd);
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		//verify the url of opened page
+		DashBoardUtils.verifyURL(webd, "emcpdfui/welcome.html");
+	}
+
+	@Test
+	public void testInfrastructureMonitoringLink()
+	{
+		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
+		webd.getLogger().info("start to test in InfrastructureMonitoringLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		// Monitoring link
+		BrandingBarUtil.visitApplicationCloudService(webd, BrandingBarUtil.NAV_LINK_TEXT_CS_IM);
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		//verify the url of opened page
+		DashBoardUtils.verifyURL(webd, "monitoringservicesui/cms/index.html");
+	}
+
+	@Test
+	public void testITALink()
+	{
+		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
+		webd.getLogger().info("start to test in testITALink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		// IT Analytics link,check checkbox
+		BrandingBarUtil.visitApplicationCloudService(webd, BrandingBarUtil.NAV_LINK_TEXT_CS_ITA);
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		//verify the url of opened page
+		DashBoardUtils.verifyURL_WithPara(webd, "emcpdfui/home.html?filter=ita");
+	}
+
+	@Test
+	public void testLALink()
+	{
+		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
+		webd.getLogger().info("start to test in testLALink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		// Log Analytics link
+		BrandingBarUtil.visitApplicationCloudService(webd, BrandingBarUtil.NAV_LINK_TEXT_CS_LA);
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		//verify the url of opened page
+		DashBoardUtils.verifyURL(webd, "emlacore/html/log-analytics-search.html");
+	}
+
+	@Test
+	public void testLogLink()
+	{
+		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
+		webd.getLogger().info("start to test in testLogLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		// Log link
+		BrandingBarUtil.visitApplicationVisualAnalyzer(webd, BrandingBarUtil.NAV_LINK_TEXT_VA_LA);
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		//verify the url of opened page
+		DashBoardUtils.verifyURL(webd, "emlacore/html/log-analytics-search.html");
+	}
+
+	@Test
+	public void testOrchestrationLink()
+	{
+		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
+		webd.getLogger().info("start to test in testOrchestrationLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		// Monitoring link
+		BrandingBarUtil.visitApplicationCloudService(webd, BrandingBarUtil.NAV_LINK_TEXT_CS_OCS);
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		//verify the url of opened page
+		DashBoardUtils.verifyURL_WithPara(webd, "emcpdfui/home.html?filter=ocs");
+	}
+
+	@Test
+	public void testSearchLink()
+	{
+		initTest(Thread.currentThread().getStackTrace()[1].getMethodName());
+		webd.getLogger().info("start to test in testSearchLink");
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		// Target link
+		BrandingBarUtil.visitApplicationVisualAnalyzer(webd, BrandingBarUtil.NAV_LINK_TEXT_VA_TA);
+		WaitUtil.waitForPageFullyLoaded(webd);
+
+		//verify the url of opened page
+		DashBoardUtils.verifyURL(webd, "emcta/ta/analytics.html");
+	}
+}
