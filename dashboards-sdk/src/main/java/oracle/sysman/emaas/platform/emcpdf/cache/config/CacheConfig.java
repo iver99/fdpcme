c6eb4f2da43e6bc8b40c38f3fd5555e82fe2f523
chendonghao <chendong.h.hao@oracle.com>
2016-12-29 10:13:25 +0800

    change CacheConfig for local test cases

--- dashboards-sdk/src/main/java/oracle/sysman/emaas/platform/emcpdf/cache/config/CacheConfig.java
+++ dashboards-sdk/src/main/java/oracle/sysman/emaas/platform/emcpdf/cache/config/CacheConfig.java
@@ -48,27 +48,26 @@ public class CacheConfig {
     public static final Long LOG_INTERVAL = Long.valueOf(conf.getString("LOG_INTERVAL"));
     //refresh cache interval
     public static final Long REFRESH_CACHE_INTERVAL = Long.valueOf(conf.getString("LOG_INTERVAL"));
-    /**
+
+
+   /**
      * below code are used when you want to run local test,and before you use , please comment above code.
      */
-    /*public static final Integer ADMIN_LINK_CACHE_EXPIRE_TIME = 20;
-    public static final Integer CLOUD_SERVICE_LINK_EXPIRE_TIME =20;
-    public static final Integer HOME_LINK_EXPIRE_TIME = 20;
-    public static final Integer VISUAL_ANALYZER_LINK_EXPIRE_TIME = 20;
-    public static final Integer SERVICE_EXTERNAL_LINK_EXPIRE_TIME = 20;
-    public static final Integer SERVICE_INTERNAL_LINK_EXPIRE_TIME = 20;
-    public static final Integer VANITY_BASE_URL_EXPIRE_TIME = 20;
-    public static final Integer DOMAINS_DATA_EXPIRE_TIME = 20;
-    public static final Integer TENANT_APP_MAPPING_EXPIRE_TIME = 20;
-    public static final Integer TENANT_SUBSCRIBED_SERVICES_EXPIRE_TIME = 20;
-    public static final Integer SCREENSHOT_EXPIRE_TIME = 20;
-    public static final Integer SSO_LOGOUT_EXPIRE_TIME = 20;
-    public static final Integer ASSET_ROOT_EXPIRE_TIME = 20;
-
-    public static final Integer ETERNAL_EXPIRE_TIME = 20;
-    public static final Integer DEFAULT_EXPIRE_TIME = 20;
-
-    //all cache group's cache capacity
+    /*public static final Long ADMIN_LINK_CACHE_EXPIRE_TIME = 20L;
+    public static final Long CLOUD_SERVICE_LINK_EXPIRE_TIME =20L;
+    public static final Long HOME_LINK_EXPIRE_TIME = 20L;
+    public static final Long VISUAL_ANALYZER_LINK_EXPIRE_TIME = 20L;
+    public static final Long SERVICE_EXTERNAL_LINK_EXPIRE_TIME = 20L;
+    public static final Long SERVICE_INTERNAL_LINK_EXPIRE_TIME = 20L;
+    public static final Long VANITY_BASE_URL_EXPIRE_TIME = 20L;
+    public static final Long DOMAINS_DATA_EXPIRE_TIME = 20L;
+    public static final Long TENANT_APP_MAPPING_EXPIRE_TIME = 20L;
+    public static final Long TENANT_SUBSCRIBED_SERVICES_EXPIRE_TIME = 20L;
+    public static final Long SCREENSHOT_EXPIRE_TIME = 20L;
+    public static final Long SSO_LOGOUT_EXPIRE_TIME = 20L;
+    public static final Long ASSET_ROOT_EXPIRE_TIME = 20L;
+    public static final Long ETERNAL_EXPIRE_TIME = 20L;
+    public static final Long DEFAULT_EXPIRE_TIME = 20L;
     public static final Integer ADMIN_LINK_CACHE_CAPACITY = 20;
     public static final Integer CLOUD_SERVICE_LINK_CAPACITY = 20;
     public static final Integer HOME_LINK_EXPIRE_CAPACITY = 20;
@@ -82,11 +81,9 @@ public class CacheConfig {
     public static final Integer SCREENSHOT_CAPACITY = 20;
     public static final Integer SSO_LOGOUT_CAPACITY = 20;
     public static final Integer ASSET_ROOT_CAPACITY = 20;
-
     public static final Integer ETERNAL_CAPACITY = 20;
     public static final Integer DEFAULT_CAPACITY = 20;
-
-    //log cache status time interval
-    public static final Long LOG_INTERVAL = 20L;*/
+    public static final Long LOG_INTERVAL = 20L;
+    public static final Long REFRESH_CACHE_INTERVAL = 20L;*/
 }
 
