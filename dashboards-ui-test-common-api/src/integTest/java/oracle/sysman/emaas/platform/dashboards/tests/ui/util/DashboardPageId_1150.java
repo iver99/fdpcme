2ee0119390ac4990da75abf621a7844f071c4686
qiqia <qiqia>
2017-01-02 17:54:00 +0800

    change the file name of pageid1150

--- dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/util/DashboardPageId_1150.java
+++
@@ -1,7 +0,0 @@
-package oracle.sysman.emaas.platform.dashboards.tests.ui.util;
-
-public class DashBoardPageId_1150 {
-    //tile css locators -> css=<locator>
-    public static final String EDITTILECSS = "li[data-option=edit]";
-    public static final String REMOVETILECSS = "button[data-option=remove-tile]";
-}
