649405cd53a64dfa54326211f319192c7f5419f3
Shangping Wang <shangping.wang@oracle.com>
2017-01-11 16:50:43 -0800

    fix compile error

--- dashboards-uitest/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/ui/TestWelcomePage.java
+++ dashboards-uitest/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/ui/TestWelcomePage.java
@@ -8,6 +8,7 @@ import oracle.sysman.emaas.platform.dashboards.tests.ui.WelcomeUtil;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
