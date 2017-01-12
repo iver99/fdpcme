092cdd8b2f1669a6d0f9a33209846c82b44c3ac0
chendonghao <chendong.h.hao@oracle.com>
2017-01-12 11:01:42 +0800

    add test case

--- dashboards-comparator/dashboardscomparator-webutils/src/integTest/java/oracle/sysman/emaas/platform/dashboards/comparator/webutils/wls/management/AppLoggingManageMXBeanTest.java
+++ dashboards-comparator/dashboardscomparator-webutils/src/integTest/java/oracle/sysman/emaas/platform/dashboards/comparator/webutils/wls/management/AppLoggingManageMXBeanTest.java
@@ -1,39 +1,33 @@
-/*
+
 package oracle.sysman.emaas.platform.dashboards.comparator.webutils.wls.management;
 
 import org.apache.logging.log4j.core.config.Configurator;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.net.URISyntaxException;
 import java.net.URL;
 
-*/
+
 /**
  * Created by chehao on 2017/1/11.
- *//*
+ */
 
 @Test(groups = {"s2"})
 public class AppLoggingManageMXBeanTest {
 
-    private IAppLoggingManageMXBean bean;
 
-    @BeforeMethod
-    public void beforeMethod() throws URISyntaxException
-    {
-        URL url = AppLoggingManageMXBeanTest.class.getResource("/log4j2_dsbcomparator-test.xml");
-        Configurator.initialize("root", AppLoggingManageMXBeanTest.class.getClassLoader(), url.toURI());
-    }
     @BeforeClass
-    public void setUp(){
-        bean = new AppLoggingManageMXBean();
+    public void setUp() throws URISyntaxException {
+        URL url = AppLoggingManageMXBeanTest.class.getResource("/log4j2_dsbcomparator.xml");
+        Configurator.initialize("root", AppLoggingManageMXBeanTest.class.getClassLoader(), url.toURI());
     }
 
     @Test
     public void testGetLogLevel() {
-        String levels = bean.getLogLevels();
+        AppLoggingManageMXBean almmxb = new AppLoggingManageMXBean();
+        String levels = almmxb.getLogLevels();
         Assert.assertTrue(levels.contains("\"oracle.sysman.emaas.platform.dashboards.comparator.webutils\":\"INFO\""));
 
         Assert.assertTrue(levels.contains("\"oracle.sysman.emaas.platform.dashboards.comparator\":\"INFO\""));
@@ -41,25 +35,26 @@ public class AppLoggingManageMXBeanTest {
 
     @Test
     public void testSetLogLevel(){
-        bean.setLogLevel("oracle.sysman.emaas.platform.dashboards.comparator.webutils", "DEBUG");
-        String levels = bean.getLogLevels();
+        AppLoggingManageMXBean almmxb = new AppLoggingManageMXBean();
+        almmxb.setLogLevel("oracle.sysman.emaas.platform.dashboards.comparator.webutils", "DEBUG");
+        String levels = almmxb.getLogLevels();
         Assert.assertTrue(levels.contains("\"oracle.sysman.emaas.platform.dashboards.comparator.webutils\":\"DEBUG\""));
 
-        bean.setLogLevel("oracle.sysman.emaas.platform.dashboards.comparator.webutils", "INFO");
-        levels = bean.getLogLevels();
+        almmxb.setLogLevel("oracle.sysman.emaas.platform.dashboards.comparator.webutils", "INFO");
+        levels = almmxb.getLogLevels();
         Assert.assertTrue(levels.contains("\"oracle.sysman.emaas.platform.dashboards.comparator.webutils\":\"INFO\""));
 
-        bean.setLogLevel("oracle.sysman.emaas.platform.dashboards.comparator.webutils", "WARN");
-        levels = bean.getLogLevels();
+        almmxb.setLogLevel("oracle.sysman.emaas.platform.dashboards.comparator.webutils", "WARN");
+        levels = almmxb.getLogLevels();
         Assert.assertTrue(levels.contains("\"oracle.sysman.emaas.platform.dashboards.comparator.webutils\":\"WARN\""));
 
-        bean.setLogLevel("oracle.sysman.emaas.platform.dashboards.comparator.webutils", "ERROR");
-        levels = bean.getLogLevels();
+        almmxb.setLogLevel("oracle.sysman.emaas.platform.dashboards.comparator.webutils", "ERROR");
+        levels = almmxb.getLogLevels();
         Assert.assertTrue(levels.contains("\"oracle.sysman.emaas.platform.dashboards.comparator.webutils\":\"ERROR\""));
 
-        bean.setLogLevel("oracle.sysman.emaas.platform.dashboards.comparator.webutils", "FATAL");
-        levels = bean.getLogLevels();
+        almmxb.setLogLevel("oracle.sysman.emaas.platform.dashboards.comparator.webutils", "FATAL");
+        levels = almmxb.getLogLevels();
         Assert.assertTrue(levels.contains("\"oracle.sysman.emaas.platform.dashboards.comparator.webutils\":\"FATAL\""));
     }
 }
-*/
+
