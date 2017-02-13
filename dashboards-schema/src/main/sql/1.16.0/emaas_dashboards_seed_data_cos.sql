a797b25ae4f4b4255bd48c2afaf1013242503721
xia_dai <xiaoyuan.d.dai@oracle.com>
2017-02-13 16:32:47 +0800

    EMCPDF-3175

--- dashboards-schema/src/main/sql/1.16.0/emaas_dashboards_seed_data_cos.sql
+++ dashboards-schema/src/main/sql/1.16.0/emaas_dashboards_seed_data_cos.sql
@@ -1,14 +1,14 @@
 Rem
-Rem emaas_dashboards_seed_data_os.sql
+Rem emaas_dashboards_seed_data_cos.sql
 Rem
 Rem Copyright (c) 2013, 2014, 2015, 2016, 2017 Oracle and/or its affiliates.
 Rem All rights reserved.
 Rem
 Rem    NAME
-Rem      emaas_dashboards_seed_data_os.sql
+Rem      emaas_dashboards_seed_data_cos.sql
 Rem
 Rem    DESCRIPTION
-Rem      Seed data for ITAUPDATE
+Rem      Seed data for COSUPDATE
 Rem
 Rem    NOTES
 Rem      None
--- dashboards-schema/src/main/sql/1.16.0/upgrade_impl_dml.sql
+++ dashboards-schema/src/main/sql/1.16.0/upgrade_impl_dml.sql
@@ -2,7 +2,7 @@ Rem ----------------------------------------------------------------
 Rem 02/06/2016	XIADAI	Created file
 Rem 
 Rem ----------------------------------------------------------------
-@&EMSAAS_SQL_ROOT/1.16.0/emaas_dashboards_seed_data_os.sql -1
+@&EMSAAS_SQL_ROOT/1.16.0/emaas_dashboards_seed_data_cos.sql -1
 
 
 COMMIT;
