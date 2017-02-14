a7951fedfa32bb64e705075d3116f78d423eaf5e
xia_dai <xiaoyuan.d.dai@oracle.com>
2017-02-14 16:46:30 +0800

    EMCPDF-3157

---
+++ dashboards-schema/src/main/sql/1.16.0/upgrade_impl_dml.sql
@@ -0,0 +1,9 @@
+Rem ----------------------------------------------------------------
+Rem 02/03/2017	MIAYU	Created file
+Rem 
+Rem ----------------------------------------------------------------
+
+@&EMSAAS_SQL_ROOT/1.16.0/emaas_dashboards_update_uigallery.sql -1
+
+COMMIT;
+
