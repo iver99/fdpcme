a71009705db7852a6a8d9179a6c41e15f94940d2
Monserrat Guzman <monserrat.guzman@oracle.com>
2017-02-21 16:54:19 -0800

    Modifying Logger references to avoid runtime errors in Entity Selector API

--- dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/util/IEntitySelectorUtil.java
+++ dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/util/IEntitySelectorUtil.java
@@ -11,32 +11,35 @@
 package oracle.sysman.emaas.platform.dashboards.tests.ui.util;
 
 import oracle.sysman.qatool.uifwk.webdriver.WebDriver;
+import java.util.logging.Logger;
 
 /**
  * @author cawei
  */
 public interface IEntitySelectorUtil extends IUiTestCommonAPI
 {
+        public static final String CATEGORY_COMPOSITE = "Composite Entities";
+	public static final String CATEGORY_ENTITIES = "Entities";
 	/**
 	 * @param driver
 	 * @param
 	 * @return
 	 */
-	public int getNumberOfPills(WebDriver driver);
+	public int getNumberOfPills(WebDriver driver, Logger logger);
 
 	/**
 	 * @param driver
 	 * @param
 	 * @return
 	 */
-	public void openEntitySelector(WebDriver driver);
+	public void openEntitySelector(WebDriver driver, Logger logger);
 
 	/**
 	 * @param driver
 	 * @param
 	 * @return
 	 */
-	public void removePill(WebDriver driver, int indexOfPillToRemove);
+	public void removePill(WebDriver driver, Logger logger, int indexOfPillToRemove);
         
         /**
          * 
@@ -46,39 +49,39 @@ public interface IEntitySelectorUtil extends IUiTestCommonAPI
          * @param entityType
          * @param category 
          */
-        public void replacePillSelection(WebDriver driver, int pillIndex, String entityName, String entityType, String category);
+        public void replacePillSelection(WebDriver driver, Logger logger, int pillIndex, String entityName, String entityType, String category);
 
 	/**
 	 * @param driver
 	 * @param
 	 * @return
 	 */
-	public void searchText(WebDriver driver, String text);
+	public void searchText(WebDriver driver, Logger logger, String text);
 
 	/**
 	 * @param driver
 	 * @param
 	 * @return
 	 */
-	public void selectCompositeEntity(WebDriver driver, String entityName, String entityType);
+	public void selectCompositeEntity(WebDriver driver, Logger logger, String entityName, String entityType);
 
 	/**
 	 * @param driver
 	 * @param
 	 * @return
 	 */
-	public void selectEntity(WebDriver driver, String entityName, String entityType);
+	public void selectEntity(WebDriver driver, Logger logger, String entityName, String entityType);
 
 	/**
 	 * @param driver
-	 * @param
+	 * @param 
 	 * @return
 	 */
-	public boolean validateReadOnlyMode(WebDriver driver);
+	public boolean validateReadOnlyMode(WebDriver driver, Logger logger);
         
         /**
          * 
          * @param driver 
          */
-        public void clearContext(WebDriver driver);
+        public void clearContext(WebDriver driver, Logger logger);
 }
