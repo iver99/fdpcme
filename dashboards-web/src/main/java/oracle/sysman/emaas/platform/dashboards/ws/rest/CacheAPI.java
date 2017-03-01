88f2fa046f03112c1e7c210dfb556156ff44a59a
chehao <chendong.h.hao@oracle.com>
2017-03-01 11:29:58 +0800

    add new CacheAPI

---
+++ dashboards-web/src/main/java/oracle/sysman/emaas/platform/dashboards/ws/rest/CacheAPI.java
@@ -0,0 +1,81 @@
+package oracle.sysman.emaas.platform.dashboards.ws.rest;
+
+import oracle.sysman.emaas.platform.emcpdf.cache.api.ICache;
+import oracle.sysman.emaas.platform.emcpdf.cache.support.AbstractCacheManager;
+import oracle.sysman.emaas.platform.emcpdf.cache.support.lru.LRUCacheManager;
+import oracle.sysman.emaas.platform.emcpdf.cache.support.lru.LinkedHashMapCache;
+import oracle.sysman.emaas.platform.emcpdf.cache.support.screenshot.LRUScreenshotCacheManager;
+import oracle.sysman.emaas.platform.emcpdf.cache.util.CacheStatus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Created by chehao on 2017/3/1 10:36.
+ */
+@Path("/v1/cache")
+public class CacheAPI {
+    private static final Logger LOGGER = LogManager.getLogger(CacheAPI.class);
+
+    @PUT
+    @Path("/stopCache")
+    public String stopCache() {
+        AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
+        changeCacheGroupStatus(lruCacheManager,CacheStatus.CLOSED);
+        AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
+        changeCacheGroupStatus(screenshotCacheManager,CacheStatus.CLOSED);
+        try {
+            lruCacheManager.close();
+            screenshotCacheManager.close();
+        } catch (IOException e) {
+            LOGGER.error(e);
+        }
+        return null;
+    }
+
+    @PUT
+    @Path("/startCache")
+    public String startCache() {
+        AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
+        changeCacheGroupStatus(lruCacheManager,CacheStatus.AVAILABLE);
+        AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
+        changeCacheGroupStatus(screenshotCacheManager,CacheStatus.AVAILABLE);
+        return "Cache is started!";
+    }
+
+    @PUT
+    @Path("/suspendCache")
+    public String suspendCache() {
+        AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
+        changeCacheGroupStatus(lruCacheManager,CacheStatus.SUSPEND);
+        AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
+        changeCacheGroupStatus(screenshotCacheManager,CacheStatus.SUSPEND);
+        return "Cache is suspended!";
+    }
+
+    @PUT
+    @Path("/resumeCache")
+    public String resumeCache() {
+        AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
+        changeCacheGroupStatus(lruCacheManager,CacheStatus.AVAILABLE);
+        AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
+        changeCacheGroupStatus(screenshotCacheManager,CacheStatus.AVAILABLE);
+        return "Cache is resumed!";
+    }
+
+    private void changeCacheGroupStatus(AbstractCacheManager cacheManager, CacheStatus cacheStatus) {
+        if (cacheManager.getCacheMap() == null) {
+            LOGGER.error("CacheMap in CacheManagers is null!");
+            return;
+        }
+        for (Map.Entry<String, ICache> entry : cacheManager.getCacheMap().entrySet()) {
+            LinkedHashMapCache cache = (LinkedHashMapCache) entry.getValue();
+            cache.setCacheStatus(cacheStatus);
+            LOGGER.info("Cache group {} status has been changed to {}", cache.getName(), cacheStatus);
+        }
+    }
+}
