ffd325efc62fe9342b277089d751782dfe9cec09
Guobao Chen <guobao.chen@oracle.com>
2017-03-02 10:10:38 +0800

    JIRA:EMCPDF-3433 - introduce combine rest api for user info/branding bar/subscribed apps data

--- dashboards-sdk/src/main/java/oracle/sysman/emaas/platform/uifwk/util/DataAccessUtil.java
+++ dashboards-sdk/src/main/java/oracle/sysman/emaas/platform/uifwk/util/DataAccessUtil.java
@@ -93,28 +93,28 @@ public class DataAccessUtil
 	private static final String HTTP_HEADER_X_USER_IDENTITY_DOMAIN_NAME = "X-USER-IDENTITY-DOMAIN-NAME";
 	private static final Logger LOGGER = LogManager.getLogger(DataAccessUtil.class);
 
-	public static String getRegistrationData(String tenantName, String userName, String referer, String sessionExp)
+	public static String getBrandingBarData(String tenantName, String userName, String referer, String sessionExp)
 	{
 		String userTenant = tenantName + "." + userName;
 		long start = System.currentTimeMillis();
-		Tenant cacheTenant = new Tenant(tenantName);
-		Object userTenantKey = DefaultKeyGenerator.getInstance().generate(cacheTenant, new Keys(userTenant));
-		ICacheManager cm = CacheManagers.getInstance().build();
-		ICache cache = cm.getCache(CacheConstants.CACHES_REGISTRY_CACHE);
-		if (cache != null) {
-			try {
-				Object obj = cache.get(userTenantKey);
-				if (obj instanceof String) {
-					String data = (String) obj;
-					LOGGER.info("Retrieved registration data from cache for userTenant {}, cached data is {}", userTenant, data);
-					return data;
-				}
-			}
-			catch (ExecutionException e) {
-				// for cache issue, we'll continue retrieve data and just log a warning message
-				LOGGER.warn(e.getMessage(), e);
-			}
-		}
+//		Tenant cacheTenant = new Tenant(tenantName);
+//		Object userTenantKey = DefaultKeyGenerator.getInstance().generate(cacheTenant, new Keys(userTenant));
+//		ICacheManager cm = CacheManagers.getInstance().build();
+//		ICache cache = cm.getCache(CacheConstants.CACHES_REGISTRY_CACHE);
+//		if (cache != null) {
+//			try {
+//				Object obj = cache.get(userTenantKey);
+//				if (obj instanceof String) {
+//					String data = (String) obj;
+//					LOGGER.info("Retrieved registration data from cache for userTenant {}, cached data is {}", userTenant, data);
+//					return data;
+//				}
+//			}
+//			catch (ExecutionException e) {
+//				// for cache issue, we'll continue retrieve data and just log a warning message
+//				LOGGER.warn(e.getMessage(), e);
+//			}
+//		}
 
 		try {
 			Link configurationsLink = RegistryLookupUtil.getServiceInternalLink("Dashboard-API", "1.0+",
@@ -124,7 +124,7 @@ public class DataAccessUtil
 				return null;
 			}
 			LOGGER.info("Configurations REST API link from dashboard-api href is: " + configurationsLink.getHref());
-			String registrationHref = configurationsLink.getHref() + "/registration";
+			String registrationHref = configurationsLink.getHref() + "/brandingbardata";
 			RestClient rc = new RestClient();
 			rc.setHeader("X-USER-IDENTITY-DOMAIN-NAME", tenantName);
 			rc.setHeader("X-REMOTE-USER", userTenant);
@@ -137,9 +137,9 @@ public class DataAccessUtil
 				rc.setHeader("SESSION_EXP", sessionExp);
 			}
 			String response = rc.get(registrationHref, tenantName);
-			cache.put(userTenantKey, response);
-			LOGGER.info("Retrieved registration data is: {}", response);
-			LOGGER.info("It takes {}ms to retrieve registration data from Dashboard-API", System.currentTimeMillis() - start);
+//			cache.put(userTenantKey, response);
+			LOGGER.info("Retrieved brandingbar data is: {}", response);
+			LOGGER.info("It takes {}ms to retrieve brandingbar data from Dashboard-API", System.currentTimeMillis() - start);
 			return response;
 		}
 		catch (Exception e) {
@@ -148,109 +148,109 @@ public class DataAccessUtil
 		}
 	}
 
-	public static String getTenantSubscribedServices(String tenant, String user)
-	{
-		if (tenant == null) {
-			return null;
-		}
+//	public static String getTenantSubscribedServices(String tenant, String user)
+//	{
+//		if (tenant == null) {
+//			return null;
+//		}
+//
+//		long startTime = System.currentTimeMillis();
+//		Tenant cacheTenant = new Tenant(tenant);
+//		Object tenantKey = DefaultKeyGenerator.getInstance().generate(cacheTenant,
+//				new Keys(CacheConstants.LOOKUP_CACHE_KEY_SUBSCRIBED_APPS_UIFWK));
+//		ICacheManager cm = CacheManagers.getInstance().build();
+//		ICache cache = cm.getCache(CacheConstants.CACHES_SUBSCRIBED_SERVICE_CACHE);
+//		if (cache != null) {
+//			try {
+//				String data = (String) cache.get(tenantKey);
+//				if (data != null) {
+//					LOGGER.info("Retrieved subscribed app information from cache for tenant {}, cached data is {}", tenant, data);
+//					return data;
+//				}
+//			}
+//			catch (ExecutionException e) {
+//				// for cache issue, we'll continue retrieve data and just log a warning message
+//				LOGGER.warn(e.getLocalizedMessage(), e);
+//			}
+//		}
+//
+//		// instead of retrieving subscribed apps from entitynaming, we get that from dashboard api
+//		Link subAppLink = RegistryLookupUtil.getServiceInternalLink("Dashboard-API", "1.0+", "static/dashboards.subscribedapps",
+//				null);
+//		if (subAppLink == null || StringUtil.isEmpty(subAppLink.getHref())) {
+//			LOGGER.warn("Checking tenant (" + tenant
+//					+ ") subscriptions: null/empty subscribedapp link retrieved from dashboard-api.");
+//			return null;
+//		}
+//		LOGGER.info("Checking tenant (" + tenant + ") subscriptions. Subscribedapp link retrieved from dashboard-api href is "
+//				+ subAppLink.getHref());
+//		String subAppHref = subAppLink.getHref();
+//		RestClient rc = new RestClient();
+//		rc.setHeader(HTTP_HEADER_X_USER_IDENTITY_DOMAIN_NAME, tenant);
+//		rc.setHeader("X-REMOTE-USER", tenant + "." + user);
+//		//EMCPDF-3448, FEB20: 3 admin link dif found in farm jobs
+//		rc.setHeader("OAM_REMOTE_USER", tenant + "." + user);
+//		String subAppResponse = rc.get(subAppHref, tenant);
+//		cache.put(tenantKey, subAppResponse);
+//		LOGGER.info("Checking tenant (" + tenant + ") subscriptions. Dashboard-API subscribed app response is " + subAppResponse);
+//		LOGGER.info("It takes {}ms to retrieve subscribed app data from Dashboard-API", System.currentTimeMillis() - startTime);
+//		return subAppResponse;
+//	}
 
-		long startTime = System.currentTimeMillis();
-		Tenant cacheTenant = new Tenant(tenant);
-		Object tenantKey = DefaultKeyGenerator.getInstance().generate(cacheTenant,
-				new Keys(CacheConstants.LOOKUP_CACHE_KEY_SUBSCRIBED_APPS_UIFWK));
-		ICacheManager cm = CacheManagers.getInstance().build();
-		ICache cache = cm.getCache(CacheConstants.CACHES_SUBSCRIBED_SERVICE_CACHE);
-		if (cache != null) {
-			try {
-				String data = (String) cache.get(tenantKey);
-				if (data != null) {
-					LOGGER.info("Retrieved subscribed app information from cache for tenant {}, cached data is {}", tenant, data);
-					return data;
-				}
-			}
-			catch (ExecutionException e) {
-				// for cache issue, we'll continue retrieve data and just log a warning message
-				LOGGER.warn(e.getLocalizedMessage(), e);
-			}
-		}
-
-		// instead of retrieving subscribed apps from entitynaming, we get that from dashboard api
-		Link subAppLink = RegistryLookupUtil.getServiceInternalLink("Dashboard-API", "1.0+", "static/dashboards.subscribedapps",
-				null);
-		if (subAppLink == null || StringUtil.isEmpty(subAppLink.getHref())) {
-			LOGGER.warn("Checking tenant (" + tenant
-					+ ") subscriptions: null/empty subscribedapp link retrieved from dashboard-api.");
-			return null;
-		}
-		LOGGER.info("Checking tenant (" + tenant + ") subscriptions. Subscribedapp link retrieved from dashboard-api href is "
-				+ subAppLink.getHref());
-		String subAppHref = subAppLink.getHref();
-		RestClient rc = new RestClient();
-		rc.setHeader(HTTP_HEADER_X_USER_IDENTITY_DOMAIN_NAME, tenant);
-		rc.setHeader("X-REMOTE-USER", tenant + "." + user);
-		//EMCPDF-3448, FEB20: 3 admin link dif found in farm jobs
-		rc.setHeader("OAM_REMOTE_USER", tenant + "." + user);
-		String subAppResponse = rc.get(subAppHref, tenant);
-		cache.put(tenantKey, subAppResponse);
-		LOGGER.info("Checking tenant (" + tenant + ") subscriptions. Dashboard-API subscribed app response is " + subAppResponse);
-		LOGGER.info("It takes {}ms to retrieve subscribed app data from Dashboard-API", System.currentTimeMillis() - startTime);
-		return subAppResponse;
-	}
-
-	public static String getUserTenantInfo(String tenantName, String userName, String referer, String sessionExp)
-	{
-		String userTenant = tenantName + "." + userName;
-		LOGGER.info("Retrieved tenantName: " + tenantName + ", userTenant: " + userTenant);
-		long start = System.currentTimeMillis();
-		Tenant cacheTenant = new Tenant(tenantName);
-		Object userTenantKey = DefaultKeyGenerator.getInstance().generate(cacheTenant, new Keys(userTenant));
-		ICacheManager cm = CacheManagers.getInstance().build();
-		ICache cache = cm.getCache(CacheConstants.CACHES_TENANT_USER_CACHE);
-		if (cache != null) {
-			try {
-				Object obj = cache.get(userTenantKey);
-				if (obj instanceof String) {
-					String data = (String) obj;
-					LOGGER.info("Retrieved user info from cache for userTenant {}, cached data is {}", userTenant, data);
-					return data;
-				}
-			}
-			catch (ExecutionException e) {
-				// for cache issue, we'll continue retrieve data and just log a warning message
-				LOGGER.warn(e.getMessage(), e);
-			}
-		}
-
-		try {
-			Link configurationsLink = RegistryLookupUtil.getServiceInternalLink("Dashboard-API", "1.0+",
-					"static/dashboards.configurations", null);
-			if (configurationsLink == null || StringUtil.isEmpty(configurationsLink.getHref())) {
-				LOGGER.warn("Retrieving configurations links for tenant {}: null/empty configurationsLink retrieved from service registry.");
-				cache.evict(userTenantKey);
-				return null;
-			}
-			LOGGER.info("Configurations REST API link from dashboard-api href is: " + configurationsLink.getHref());
-			String userInfoHref = configurationsLink.getHref() + "/userInfo";
-			RestClient rc = new RestClient();
-			rc.setHeader("X-USER-IDENTITY-DOMAIN-NAME", tenantName);
-			rc.setHeader("X-REMOTE-USER", userTenant);
-			//EMCPDF-3448, FEB20: 3 admin link dif found in farm jobs
-			rc.setHeader("OAM_REMOTE_USER", userTenant);
-			if (!StringUtil.isEmpty(referer)) {
-				rc.setHeader("Referer", referer);
-			}
-			if (!StringUtil.isEmpty(sessionExp)) {
-				rc.setHeader("SESSION_EXP", sessionExp);
-			}
-			String response = rc.get(userInfoHref, tenantName);
-			cache.put(userTenantKey, response);
-			LOGGER.info("Retrieved userInfo data is: {}", response);
-			LOGGER.info("It takes {}ms to retrieve userInfo data from Dashboard-API", System.currentTimeMillis() - start);
-			return response;
-		}
-		catch (Exception e) {
-			LOGGER.error(e.getMessage(), e);
-			return null;
-		}
-	}
+//	public static String getUserTenantInfo(String tenantName, String userName, String referer, String sessionExp)
+//	{
+//		String userTenant = tenantName + "." + userName;
+//		LOGGER.info("Retrieved tenantName: " + tenantName + ", userTenant: " + userTenant);
+//		long start = System.currentTimeMillis();
+//		Tenant cacheTenant = new Tenant(tenantName);
+//		Object userTenantKey = DefaultKeyGenerator.getInstance().generate(cacheTenant, new Keys(userTenant));
+//		ICacheManager cm = CacheManagers.getInstance().build();
+//		ICache cache = cm.getCache(CacheConstants.CACHES_TENANT_USER_CACHE);
+//		if (cache != null) {
+//			try {
+//				Object obj = cache.get(userTenantKey);
+//				if (obj instanceof String) {
+//					String data = (String) obj;
+//					LOGGER.info("Retrieved user info from cache for userTenant {}, cached data is {}", userTenant, data);
+//					return data;
+//				}
+//			}
+//			catch (ExecutionException e) {
+//				// for cache issue, we'll continue retrieve data and just log a warning message
+//				LOGGER.warn(e.getMessage(), e);
+//			}
+//		}
+//
+//		try {
+//			Link configurationsLink = RegistryLookupUtil.getServiceInternalLink("Dashboard-API", "1.0+",
+//					"static/dashboards.configurations", null);
+//			if (configurationsLink == null || StringUtil.isEmpty(configurationsLink.getHref())) {
+//				LOGGER.warn("Retrieving configurations links for tenant {}: null/empty configurationsLink retrieved from service registry.");
+//				cache.evict(userTenantKey);
+//				return null;
+//			}
+//			LOGGER.info("Configurations REST API link from dashboard-api href is: " + configurationsLink.getHref());
+//			String userInfoHref = configurationsLink.getHref() + "/userInfo";
+//			RestClient rc = new RestClient();
+//			rc.setHeader("X-USER-IDENTITY-DOMAIN-NAME", tenantName);
+//			rc.setHeader("X-REMOTE-USER", userTenant);
+//			//EMCPDF-3448, FEB20: 3 admin link dif found in farm jobs
+//			rc.setHeader("OAM_REMOTE_USER", userTenant);
+//			if (!StringUtil.isEmpty(referer)) {
+//				rc.setHeader("Referer", referer);
+//			}
+//			if (!StringUtil.isEmpty(sessionExp)) {
+//				rc.setHeader("SESSION_EXP", sessionExp);
+//			}
+//			String response = rc.get(userInfoHref, tenantName);
+//			cache.put(userTenantKey, response);
+//			LOGGER.info("Retrieved userInfo data is: {}", response);
+//			LOGGER.info("It takes {}ms to retrieve userInfo data from Dashboard-API", System.currentTimeMillis() - start);
+//			return response;
+//		}
+//		catch (Exception e) {
+//			LOGGER.error(e.getMessage(), e);
+//			return null;
+//		}
+//	}
 }
--- dashboards-web/src/main/java/oracle/sysman/emaas/platform/dashboards/ws/rest/ConfigurationAPI.java
+++ dashboards-web/src/main/java/oracle/sysman/emaas/platform/dashboards/ws/rest/ConfigurationAPI.java
@@ -24,10 +24,13 @@ import javax.ws.rs.core.Response.Status;
 import oracle.sysman.emaas.platform.dashboards.core.exception.DashboardException;
 import oracle.sysman.emaas.platform.dashboards.core.exception.resource.EntityNamingDependencyUnavailableException;
 import oracle.sysman.emaas.platform.dashboards.core.util.JsonUtil;
+import oracle.sysman.emaas.platform.dashboards.core.util.StringUtil;
+import oracle.sysman.emaas.platform.dashboards.core.util.TenantSubscriptionUtil;
 import oracle.sysman.emaas.platform.dashboards.webutils.dependency.DependencyStatus;
 import oracle.sysman.emaas.platform.dashboards.ws.ErrorEntity;
 import oracle.sysman.emaas.platform.dashboards.ws.rest.model.RegistrationEntity;
 import oracle.sysman.emaas.platform.dashboards.ws.rest.model.UserInfoEntity;
+import oracle.sysman.emaas.platform.dashboards.ws.rest.TenantSubscriptionsAPI.SubscribedAppsEntity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -40,6 +43,64 @@ public class ConfigurationAPI extends APIBase
 {
 	private static final Logger _LOGGER = LogManager.getLogger(ConfigurationAPI.class);
 
+	private static class CombinedBrandingBarData {
+		public CombinedBrandingBarData(UserInfoEntity userInfo, RegistrationEntity registration, SubscribedAppsEntity subscribedApps) {
+			this.userInfo = userInfo;
+			this.registration = registration;
+			this.subscribedApps = subscribedApps;
+		}
+
+		private UserInfoEntity userInfo;
+		private RegistrationEntity registration;
+		private SubscribedAppsEntity subscribedApps;
+
+		public UserInfoEntity getUserInfo() {
+			return userInfo;
+		}
+
+		public void setUserInfo(UserInfoEntity userInfo) {
+			this.userInfo = userInfo;
+		}
+
+		public SubscribedAppsEntity getSubscribedApps() {
+			return subscribedApps;
+		}
+
+		public void setSubscribedApps(SubscribedAppsEntity subscribedApps) {
+			this.subscribedApps = subscribedApps;
+		}
+
+		public RegistrationEntity getRegistration() {
+			return registration;
+		}
+
+		public void setRegistration(RegistrationEntity registration) {
+			this.registration = registration;
+		}
+
+		public String getBrandingbarInjectedJS() {
+			StringBuilder sb = new StringBuilder();
+			if (userInfo != null) {
+				sb.append("window._uifwk.cachedData.userInfo=");
+				sb.append(JsonUtil.buildNormalMapper().toJson(userInfo));
+				sb.append(";");
+			}
+
+			if (registration != null) {
+				sb.append("window._uifwk.cachedData.registrations=");
+				sb.append(JsonUtil.buildNormalMapper().toJson(registration));
+				sb.append(";");
+			}
+
+			if (subscribedApps != null) {
+				sb.append("window._uifwk.cachedData.subscribedapps=");
+				sb.append(JsonUtil.buildNormalMapper().toJson(subscribedApps));
+				sb.append(";");
+			}
+			return sb.toString();
+		}
+	}
+
 	@Path("/registration")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -51,7 +112,7 @@ public class ConfigurationAPI extends APIBase
 
 		try {
 			initializeUserContext(tenantIdParam, userTenant);
-			String regEntity = JsonUtil.buildNonNullMapper().toJson(new RegistrationEntity(sessionExpiryTime));
+			String regEntity = JsonUtil.buildNonNullMapper().toJson(new RegistrationEntity(sessionExpiryTime, null));
 			_LOGGER.info("Response for [GET] /v1/configurations/registration is \"{}\"", regEntity);
 			Response resp = Response.status(Status.OK).entity(regEntity).build();
 			return resp;
@@ -87,6 +148,46 @@ public class ConfigurationAPI extends APIBase
 		}
 		catch (DashboardException e) {
 			_LOGGER.error(e.getLocalizedMessage(), e);
+			ErrorEntity ee = new ErrorEntity(e);
+			return Response.status(ee.getStatusCode()).entity(JsonUtil.buildNormalMapper().toJson(ee)).build();
+		}
+	}
+
+	/**
+	 * This API returned a combined data for branding bar, including user info, registration data, and subscribed application for the tenant.
+	 * This API is supposed to be used internally by OMC services for html data injection for PSR consideration.
+	 * This API returns the js String directly containing the branding bar data, thus additional object deserialization on the caller service side is avoided.
+	 *
+	 * @param tenantIdParam tenant for the request, from header "X-USER-IDENTITY-DOMAIN-NAME"
+	 * @param userTenant <tenant.user> for the requesst, from header "X-REMOTE-USER"
+	 * @param referer referer for the request
+	 * @param sessionExpiryTime session expiry time for the request, from header "SESSION_EXP"
+     * @return
+     */
+	@Path("/brandingbardata")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response queryCombinedBrandingBarData(@HeaderParam(value = "X-USER-IDENTITY-DOMAIN-NAME") String tenantIdParam,
+												 @HeaderParam(value = "X-REMOTE-USER") String userTenant, @HeaderParam(value = "Referer") String referer,
+												 @HeaderParam(value = "SESSION_EXP") String sessionExpiryTime)
+	{
+		infoInteractionLogAPIIncomingCall(tenantIdParam, referer, "Service call to [GET] /v1/configurations/brandingbardata");
+		try {
+			initializeUserContext(tenantIdParam, userTenant);
+			UserInfoEntity uie = new UserInfoEntity();
+			// this ensures subscribed app data inside cache, and reused by registration data retrieval
+			List<String> apps = TenantSubscriptionUtil.getTenantSubscribedServices(tenantIdParam);
+			SubscribedAppsEntity sae = apps == null ? null : new SubscribedAppsEntity(apps);
+			RegistrationEntity re = new RegistrationEntity(sessionExpiryTime, uie.getUserRoles());
+			CombinedBrandingBarData cbbd = new CombinedBrandingBarData(uie, re, new SubscribedAppsEntity(apps));
+			String brandingBarData = cbbd.getBrandingbarInjectedJS();
+			_LOGGER.info("Response for [GET] /v1/configurations/brandingbardata is \"{}\"", brandingBarData);
+			Response resp = Response.status(Status.OK).entity(brandingBarData).build();
+			return resp;
+
+		}
+		catch (DashboardException e) {
+			_LOGGER.error(e);
 			ErrorEntity ee = new ErrorEntity(e);
 			return Response.status(ee.getStatusCode()).entity(JsonUtil.buildNormalMapper().toJson(ee)).build();
 		}
