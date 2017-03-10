f96390af5e3b4067c0e173c300411d1165ce0795
xia_dai <xiaoyuan.d.dai@oracle.com>
2017-03-10 14:51:02 +0800

    EMCPDF-2937

---
+++ dashboards-schema/src/main/sql/1.17.0/schema_ops.sql
@@ -0,0 +1,12 @@
+Rem ----------------------------------------------------------------
+Rem 02/06/2017	XIADAI	Created file
+Rem ----------------------------------------------------------------
+
+@&EMSAAS_SQL_ROOT/1.17.0/upgrade_impl_ddl.sql
+
+SET HEADING OFF
+SET FEEDBACK OFF
+SET LINESIZE 2000
+
+@&EMSAAS_SQL_ROOT/1.17.0/upgrade_impl_dml.sql
+
