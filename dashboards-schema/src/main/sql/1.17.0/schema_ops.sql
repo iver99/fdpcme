647caa79ffaa545bf2c88d7d277f73e77acf9ff1
rex.liang <rex.liang@oracle.com>
2017-03-15 00:36:49 -0700

    EMCPDF-3463: update the sql's version from 1.16 to 1.17

--- dashboards-schema/src/main/sql/1.17.0/schema_ops.sql
+++ dashboards-schema/src/main/sql/1.17.0/schema_ops.sql
@@ -2,11 +2,11 @@ Rem ----------------------------------------------------------------
 Rem 02/03/2017	MIAYU	Created file
 Rem ----------------------------------------------------------------
 
-@&EMSAAS_SQL_ROOT/1.16.0/upgrade_impl_ddl.sql
+@&EMSAAS_SQL_ROOT/1.17.0/upgrade_impl_ddl.sql
 
 SET HEADING OFF
 SET FEEDBACK OFF
 SET LINESIZE 2000
 
-@&EMSAAS_SQL_ROOT/1.16.0/upgrade_impl_dml.sql
+@&EMSAAS_SQL_ROOT/1.17.0/upgrade_impl_dml.sql
 
