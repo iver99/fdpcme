f98bd4ddbc15f66825a2a887c78442afcb749c6f
xia_dai <xiaoyuan.d.dai@oracle.com>
2017-03-27 18:10:43 +0800

    EMCPDF-3693

---
+++ dashboards-schema/src/main/sql/1.18.0/upgrade_impl_dml.sql
@@ -0,0 +1,8 @@
+Rem ----------------------------------------------------------------
+Rem 03/27/2017	XIADAI	Created file
+Rem
+Rem ----------------------------------------------------------------
+
+@&EMSAAS_SQL_ROOT/1.18.0/emaas_dashboards_seed_data_cos.sql -1
+COMMIT;
+
--- dashboards-schema/src/main/sql/emaas_dashboards_tenant_onboarding.sql
+++ dashboards-schema/src/main/sql/emaas_dashboards_tenant_onboarding.sql
@@ -96,6 +96,8 @@ END;
 @&EMSAAS_SQL_ROOT/1.17.0/emaas_dashboards_seed_data_cos.sql &TENANT_ID
 @&EMSAAS_SQL_ROOT/1.17.0/emaas_dashboards_update_cos_entitySel.sql &TENANT_ID
 
+@&EMSAAS_SQL_ROOT/1.18.0/emaas_dashboards_seed_data_cos.sql &TENANT_ID
+
 /**
 --IMPORTANT: NO DDL is allowed in tenant onboarding process!!!!
 */
