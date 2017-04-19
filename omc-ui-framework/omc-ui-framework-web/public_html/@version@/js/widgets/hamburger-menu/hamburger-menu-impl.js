e65074946018cfa2bbda82493a320570e7e57f13
alvin.duan <alvin.duan@oracle.com>
2017-04-19 16:16:44 +0800

    Avoid potential issue for NLS support for hamburger menu

--- omc-ui-framework/omc-ui-framework-web/public_html/@version@/js/widgets/hamburger-menu/hamburger-menu-impl.js
+++ omc-ui-framework/omc-ui-framework-web/public_html/@version@/js/widgets/hamburger-menu/hamburger-menu-impl.js
@@ -402,6 +402,9 @@ define('uifwk/@version@/js/widgets/hamburger-menu/hamburger-menu-impl', [
                                         serviceItem.serviceAdminMenus = applyNlsOnMenu(serviceItem.serviceAdminMenus, _nls, serviceItem.appId);
                                         self.allServiceData.push(serviceItem);
                                         self.loadedServiceCnt(self.loadedServiceCnt() + 1);
+                                    }, 
+                                    function() {
+                                        self.loadedServiceCnt(self.loadedServiceCnt() + 1);
                                     });
                                 },
                                 error: function (xhr, textStatus, errorThrown) {
