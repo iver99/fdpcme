4297283b75ac4f6ac1f2b878794bd446f29060a6
Jiangxia Nan <jiangxia.nan@oracle.com>
2017-04-27 01:54:37 -0700

    EMCPDF-3906: correct comment

--- dashboards-schema/src/main/sql/1.19.0/upgrade_impl_dml.sql
+++ dashboards-schema/src/main/sql/1.19.0/upgrade_impl_dml.sql
@@ -3,7 +3,7 @@ Rem 03/27/2017	MIAYU	Created file
 Rem
 Rem ----------------------------------------------------------------
 
-Rem --fix EMCPDF-3906 in 1.18.0
+Rem --fix EMCPDF-3906 in 1.19.0
 @&EMSAAS_SQL_ROOT/1.17.0/emaas_dashboards_update_uigallery.sql -1
 COMMIT;
 
