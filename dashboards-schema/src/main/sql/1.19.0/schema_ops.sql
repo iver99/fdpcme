ac5b883228743c0b88299caeb97e08f7ed9b2ec5
Jiangxia Nan <jiangxia.nan@oracle.com>
2017-04-27 01:52:36 -0700

    merge fix for EMCPDF-3906 and change sql folder

---
+++ dashboards-schema/src/main/sql/1.19.0/schema_ops.sql
@@ -0,0 +1,10 @@
+Rem ----------------------------------------------------------------
+Rem 02/03/2017	MIAYU	Created file
+Rem ----------------------------------------------------------------
+
+SET HEADING OFF
+SET FEEDBACK OFF
+SET LINESIZE 2000
+
+@&EMSAAS_SQL_ROOT/1.19.0/upgrade_impl_dml.sql
+
