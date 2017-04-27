d4e79498e9786876832aaaad8fb7a89486fea2d7
Jiangxia Nan <jiangxia.nan@oracle.com>
2017-04-27 02:00:28 -0700

    EMCPDF-3906: add ddl file for 1.19

--- dashboards-schema/src/main/sql/1.19.0/schema_ops.sql
+++ dashboards-schema/src/main/sql/1.19.0/schema_ops.sql
@@ -2,6 +2,8 @@ Rem ----------------------------------------------------------------
 Rem 02/03/2017	MIAYU	Created file
 Rem ----------------------------------------------------------------
 
+@&EMSAAS_SQL_ROOT/1.19.0/upgrade_impl_ddl.sql
+
 SET HEADING OFF
 SET FEEDBACK OFF
 SET LINESIZE 2000
