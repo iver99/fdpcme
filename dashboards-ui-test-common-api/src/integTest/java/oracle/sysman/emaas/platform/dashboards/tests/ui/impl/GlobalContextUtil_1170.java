2d3379dada736a8b7ff89cfcb41544aee53aa10b
hui.li.li@oracle.com <hbli@slc09xqe.us.oracle.com>
2017-05-14 20:37:05 -0700

    update GlobalContextUtil

--- dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/impl/GlobalContextUtil_1170.java
+++ dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/impl/GlobalContextUtil_1170.java
@@ -21,7 +21,7 @@ public class GlobalContextUtil_1170 extends GlobalContextUtil_1160
 {
 	public static final String GLBCTX_TOPBTN = "topologyButtonWrapper";
 	public static final String GLBCTX_CTXSEL = "emcta-ctxtSel_conveyorBelt";
-	public static final String GLBCTX_CTXSEL_TEXT = "Start typing entity name(s)..."; 
+	public static final String GLBCTX_CTXSEL_TEXT = "//span[text()='Start typing entity name(s)...']"; 
 	/* (non-Javadoc)
 	 * @see oracle.sysman.emaas.platform.dashboards.tests.ui.util.IGlobalContextUtil#isGlobalContextExisted(oracle.sysman.qatool.uifwk.webdriver.WebDriver)
 	 */
