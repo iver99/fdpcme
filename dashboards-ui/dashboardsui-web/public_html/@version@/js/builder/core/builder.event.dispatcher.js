8ba88dc33ce779f01089c54d05d24b5f4c2e12b9
Guobao Chen <guobao.chen@oracle.com>
2017-05-25 23:12:03 -0700

    EMCPDF-4121 - introduce code to defer internal event to avoid repeated operations

--- dashboards-ui/dashboardsui-web/public_html/@version@/js/builder/core/builder.event.dispatcher.js
+++ dashboards-ui/dashboardsui-web/public_html/@version@/js/builder/core/builder.event.dispatcher.js
@@ -23,7 +23,40 @@ define([], function() {
             window.DEV_MODE && console.debug('Dashboard builder event registration. [Event]' + event + ' [Handler]' + handler);
         };
 
+        self.delayResizeListenerQueue = [];
         self.triggerEvent = function(event, p1, p2, p3, p4) {
+            if (event === Builder.EVENT_BUILDER_RESIZE) {
+                var delayedEvent = {
+                    event: event,
+                    p1: p1,
+                    p2: p2,
+                    p3: p3,
+                    p4: p4,
+                    timestamp: performance.now()
+                };
+                self.delayResizeListenerQueue.push(delayedEvent);
+                setTimeout(function() {
+                    self.executeDelayedEvent();
+                }, 500);
+            }
+            else
+                self.executeListenersImmediately(event, p1, p2, p3, p4);
+        };
+        
+        self.executeDelayedEvent = function() {
+            var delayedEvent = self.delayResizeListenerQueue.shift();
+            if (delayedEvent) {
+                if (self.delayResizeListenerQueue.length <= 0) {// execute the delay event now
+                    var delay = performance.now() - delayedEvent.timestamp;
+                    console.info("Execute delayed event for builder resize after delay of " + delay + "ms");
+                    self.executeListenersImmediately(delayedEvent.event, delayedEvent.p1, delayedEvent.p2, delayedEvent.p3, delayedEvent.p4);
+                }
+            } else {
+                console.warn("Unexpected, after delay the delayevent is missing: " + delayedEvent + ", just ingore the event");
+            }
+        };
+        
+        self.executeListenersImmediately = function(event, p1, p2, p3, p4) {
             if (!event || !self.queue[event]){
                 return;
             }
