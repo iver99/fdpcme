9f3f4aad0f556a14706891f00d32d20aaf39814e
Shangping Wang <shangping.wang@oracle.com>
2017-06-06 17:43:04 -0700

    fix dif

--- dashboards-uitest/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/ui/BugVerification.java
+++ dashboards-uitest/src/integTest/java/oracle/sysman/emaas/platform/dashboards/test/ui/BugVerification.java
@@ -377,15 +377,23 @@ public class BugVerification extends LoginAndLogout
     		//open APM oob dashboard
     		webd.getLogger().info("Open APM oob dashboard");
     		DashboardHomeUtil.selectDashboard(webd, "Application Performance Monitoring");
-    			
-    		//below to verify the fix for EMCPDF-4039
-    		webd.getLogger().info("Hide the hamburger menu");
-    		Assert.assertFalse(BrandingBarUtil.toggleHamburgerMenu(webd) , "Hamburger menu should be hidden");
-    		webd.getLogger().info("Display the hamburger menu");
-    		Assert.assertTrue(BrandingBarUtil.toggleHamburgerMenu(webd) , "Hamburger menu should be displayed");
-    		webd.getLogger().info("Check the current hamburger menu");
-    		currenMenuHeader = BrandingBarUtil.getCurrentMenuHeader(webd);
-    		Assert.assertEquals(currenMenuHeader.trim(), "APM");			
+
+		if(DashBoardUtils.isHamburgerMenuEnabled(webd))
+		{
+			webd.getlLogger().info("Verify in APM page");    			
+	    		//below to verify the fix for EMCPDF-4039
+	    		webd.getLogger().info("Hide the hamburger menu");
+	    		Assert.assertFalse(BrandingBarUtil.toggleHamburgerMenu(webd) , "Hamburger menu should be hidden");
+	    		webd.getLogger().info("Display the hamburger menu");
+	    		Assert.assertTrue(BrandingBarUtil.toggleHamburgerMenu(webd) , "Hamburger menu should be displayed");
+	    		webd.getLogger().info("Check the current hamburger menu");
+	    		currenMenuHeader = BrandingBarUtil.getCurrentMenuHeader(webd);
+	    		Assert.assertEquals(currenMenuHeader.trim(), "APM");
+		}
+		else
+		{
+			webd.getLogger().info("Hamburger menu is not enabled in APM, do need to verify the fix for EMCPDF-4039");
+		}		
     	}
     	else
     	{
