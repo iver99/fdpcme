fec67c502a00708230266c33734b65665ad066bf
chendonghao <chendong.h.hao@oracle.com>
2017-06-09 00:20:21 -0700

    fix cache api issue

--- dashboards-web/src/main/java/oracle/sysman/emaas/platform/dashboards/ws/rest/CacheAPI.java
+++ dashboards-web/src/main/java/oracle/sysman/emaas/platform/dashboards/ws/rest/CacheAPI.java
@@ -1,6 +1,7 @@
 package oracle.sysman.emaas.platform.dashboards.ws.rest;
 
-import com.oracle.omc.omctf.testsdk.utils.StringUtils;
+import oracle.sysman.emaas.platform.dashboards.ws.rest.model.CacheGrpModel;
+import org.apache.commons.lang3.StringUtils;
 import oracle.sysman.emaas.platform.emcpdf.cache.api.ICache;
 import oracle.sysman.emaas.platform.emcpdf.cache.support.AbstractCache;
 import oracle.sysman.emaas.platform.emcpdf.cache.support.AbstractCacheManager;
@@ -12,23 +13,34 @@ import oracle.sysman.emaas.platform.emcpdf.cache.util.CacheStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import java.util.concurrent.ConcurrentMap;
+import java.util.LinkedHashMap;
 
 /**
  * Created by chehao on 2017/3/1 10:36.
  */
 @Path("/v1/cache")
-public class CacheAPI {
+public class CacheAPI extends APIBase{
     private static final Logger LOGGER = LogManager.getLogger(CacheAPI.class);
 
+    /**
+     * this is not enabled yet.
+     * @param cacheGroup
+     * @return
+     */
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/cachedItem/{cacheGroup}")
+    @Deprecated
     public Response getCacheGroupCachedItem(@PathParam("cacheGroup") String cacheGroup){
+        LOGGER.info("Service to call [GET] /v1/cache/cachedItem/{}:", cacheGroup);
         if(StringUtils.isEmpty(cacheGroup)){
             LOGGER.warn("cache group name is null or empty!");
             return Response.status(Response.Status.NOT_FOUND).entity(new MsgModel(false, "Cache group name is null or empty!")).build();
@@ -43,17 +55,25 @@ public class CacheAPI {
             map = lruCacheManager.getCacheMap();
         }
         cache = (LinkedHashMapCache) map.get(cacheGroup);
+        LinkedHashMap map1 = new LinkedHashMap(cache.getCacheMap());
+        CacheGrpModel cm = new CacheGrpModel(map1);
+        LOGGER.info("cache grp size is {}",map1.size());
+//        JsonUtil ju = JsonUtil.buildNormalMapper();
+//        ju.
         if(cache == null){
             LOGGER.error("Cache group named {} is not existed!", cacheGroup);
             return Response.status(Response.Status.NOT_FOUND).entity(new MsgModel(false, "Cache group name is not existed!")).build();
         }else{
-            return Response.status(Response.Status.OK).entity(cache.toString()).build();
+//            return Response.status(Response.Status.OK).entity(getJsonUtil().toJson(map1)).build();
+            return Response.ok(getJsonUtil().toJson(cm)).build();
         }
     }
 
     @PUT
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/stopCacheGroup/{cacheGroup}")
     public Response stopCacheGrp(@PathParam("cacheGroup") String cacheGroup) {
+        LOGGER.info("Service to call [PUT] /v1/cache/stopCacheGroup/{}:", cacheGroup);
         if(StringUtils.isEmpty(cacheGroup)){
             LOGGER.warn("cache group name is null or empty!");
             return Response.status(Response.Status.NOT_FOUND).entity(new MsgModel(false, "Cache group name is null or empty!")).build();
@@ -74,12 +94,14 @@ public class CacheAPI {
             //close all cache groups
             changeCacheGroupStatusByName(lruCacheManager,CacheStatus.CLOSED, cacheGroup);
         }
-        return Response.status(Response.Status.OK).entity(new MsgModel(true, "Cache group" + cacheGroup+ " is stopped successfully!")).build();
+        return Response.ok(getJsonUtil().toJson(new MsgModel(true, "Cache group" + cacheGroup+ " is stopped successfully!"))).build();
     }
 
     @PUT
-    @Path("/startCache/{cacheGroup}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/startCacheGroup/{cacheGroup}")
     public Response startCache(@PathParam("cacheGroup") String cacheGroup) {
+        LOGGER.info("Service to call [PUT] /v1/cache/startCache/{}:", cacheGroup);
         if(StringUtils.isEmpty(cacheGroup)){
             LOGGER.warn("cache group name is null or empty!");
             return Response.status(Response.Status.NOT_FOUND).entity(new MsgModel(false, "Cache group name is null or empty!")).build();
@@ -92,12 +114,14 @@ public class CacheAPI {
             AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
             changeCacheGroupStatusByName(lruCacheManager,CacheStatus.AVAILABLE, cacheGroup);
         }
-        return Response.status(Response.Status.OK).entity(new MsgModel(true, "All cache groups are started successfully!")).build();
+        return Response.ok(getJsonUtil().toJson(new MsgModel(true, "Cache group" + cacheGroup+ " is started successfully!"))).build();
     }
 
     @PUT
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/stopCache")
     public Response stopCache() {
+        LOGGER.info("Service to call [PUT] /v1/cache/stopCache");
         AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
         AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
         //clear all cache groups
@@ -109,37 +133,43 @@ public class CacheAPI {
         //close all cache groups
         changeAllCacheGroupStatus(lruCacheManager,CacheStatus.CLOSED);
         changeAllCacheGroupStatus(screenshotCacheManager,CacheStatus.CLOSED);
-        return Response.status(Response.Status.OK).entity(new MsgModel(true, "All cache groups are stopped successfully!")).build();
+        return Response.ok(getJsonUtil().toJson(new MsgModel(true, "All cache groups are stopped successfully!"))).build();
     }
 
     @PUT
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/startCache")
     public Response startCache() {
+        LOGGER.info("Service to call [PUT] /v1/cache/startCache");
         AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
         changeAllCacheGroupStatus(lruCacheManager,CacheStatus.AVAILABLE);
         AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
         changeAllCacheGroupStatus(screenshotCacheManager,CacheStatus.AVAILABLE);
-        return Response.status(Response.Status.OK).entity(new MsgModel(true, "All cache groups are started successfully!")).build();
+        return Response.ok(getJsonUtil().toJson(new MsgModel(true, "All cache groups are started successfully!"))).build();
     }
 
     @PUT
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/suspendCache")
     public Response suspendCache() {
+        LOGGER.info("Service to call [PUT] /v1/cache/suspendCache");
         AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
         changeAllCacheGroupStatus(lruCacheManager,CacheStatus.SUSPEND);
         AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
         changeAllCacheGroupStatus(screenshotCacheManager,CacheStatus.SUSPEND);
-        return Response.status(Response.Status.OK).entity(new MsgModel(true, "All cache groups are suspended successfully!")).build();
+        return Response.ok(getJsonUtil().toJson(new MsgModel(true, "All cache groups are suspended successfully!"))).build();
     }
 
     @PUT
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/resumeCache")
     public Response resumeCache() {
+        LOGGER.info("Service to call [PUT] /v1/cache/resumeCache");
         AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
         changeAllCacheGroupStatus(lruCacheManager,CacheStatus.AVAILABLE);
         AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
         changeAllCacheGroupStatus(screenshotCacheManager,CacheStatus.AVAILABLE);
-        return Response.status(Response.Status.OK).entity(new MsgModel(true, "All cache groups are resumed successfully!")).build();
+        return Response.ok(getJsonUtil().toJson(new MsgModel(true, "All cache groups are resumed successfully!"))).build();
     }
 
     private void changeAllCacheGroupStatus(AbstractCacheManager cacheManager, CacheStatus cacheStatus) {
