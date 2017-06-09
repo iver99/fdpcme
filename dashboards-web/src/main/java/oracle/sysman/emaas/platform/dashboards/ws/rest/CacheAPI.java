7fb3e280d088e6fe666bd5397425a2c77015c181
chehao <chendong.h.hao@oracle.com>
2017-06-09 11:16:29 +0800

    add new api in CacheAPI

--- dashboards-web/src/main/java/oracle/sysman/emaas/platform/dashboards/ws/rest/CacheAPI.java
+++ dashboards-web/src/main/java/oracle/sysman/emaas/platform/dashboards/ws/rest/CacheAPI.java
@@ -1,19 +1,22 @@
 package oracle.sysman.emaas.platform.dashboards.ws.rest;
 
+import com.oracle.omc.omctf.testsdk.utils.StringUtils;
 import oracle.sysman.emaas.platform.emcpdf.cache.api.ICache;
 import oracle.sysman.emaas.platform.emcpdf.cache.support.AbstractCache;
 import oracle.sysman.emaas.platform.emcpdf.cache.support.AbstractCacheManager;
 import oracle.sysman.emaas.platform.emcpdf.cache.support.lru.LRUCacheManager;
 import oracle.sysman.emaas.platform.emcpdf.cache.support.lru.LinkedHashMapCache;
 import oracle.sysman.emaas.platform.emcpdf.cache.support.screenshot.LRUScreenshotCacheManager;
+import oracle.sysman.emaas.platform.emcpdf.cache.util.CacheConstants;
 import oracle.sysman.emaas.platform.emcpdf.cache.util.CacheStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import java.io.IOException;
-import java.util.Map;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 import java.util.concurrent.ConcurrentMap;
 
 /**
@@ -23,54 +26,123 @@ import java.util.concurrent.ConcurrentMap;
 public class CacheAPI {
     private static final Logger LOGGER = LogManager.getLogger(CacheAPI.class);
 
+    @GET
+    @Path("/cachedItem/{cacheGroup}")
+    public Response getCacheGroupCachedItem(@PathParam("cacheGroup") String cacheGroup){
+        if(StringUtils.isEmpty(cacheGroup)){
+            LOGGER.warn("cache group name is null or empty!");
+            return Response.status(Response.Status.NOT_FOUND).entity(new MsgModel(false, "Cache group name is null or empty!")).build();
+        }
+        LinkedHashMapCache cache = null;
+        ConcurrentMap<String, ICache> map = null;
+        if(CacheConstants.CACHES_SCREENSHOT_CACHE.equals(cacheGroup)){
+            AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
+             map = screenshotCacheManager.getCacheMap();
+        }else{
+            AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
+            map = lruCacheManager.getCacheMap();
+        }
+        cache = (LinkedHashMapCache) map.get(cacheGroup);
+        if(cache == null){
+            LOGGER.error("Cache group named {} is not existed!", cacheGroup);
+            return Response.status(Response.Status.NOT_FOUND).entity(new MsgModel(false, "Cache group name is not existed!")).build();
+        }else{
+            return Response.status(Response.Status.OK).entity(cache.toString()).build();
+        }
+    }
+
+    @PUT
+    @Path("/stopCacheGroup/{cacheGroup}")
+    public Response stopCacheGrp(@PathParam("cacheGroup") String cacheGroup) {
+        if(StringUtils.isEmpty(cacheGroup)){
+            LOGGER.warn("cache group name is null or empty!");
+            return Response.status(Response.Status.NOT_FOUND).entity(new MsgModel(false, "Cache group name is null or empty!")).build();
+        }
+        if(CacheConstants.CACHES_SCREENSHOT_CACHE.equals(cacheGroup)){
+            LOGGER.info("Closing screenshot cache group...");
+            AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
+            clearCacheGroupDataByName(screenshotCacheManager, cacheGroup);
+            resetCacheStatisticsByName(screenshotCacheManager, cacheGroup);
+            changeCacheGroupStatusByName(screenshotCacheManager,CacheStatus.CLOSED, cacheGroup);
+
+        }else{
+            AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
+            //clear all cache groups
+            clearCacheGroupDataByName(lruCacheManager, cacheGroup);
+            //reset all cache group statistics data
+            resetCacheStatisticsByName(lruCacheManager, cacheGroup);
+            //close all cache groups
+            changeCacheGroupStatusByName(lruCacheManager,CacheStatus.CLOSED, cacheGroup);
+        }
+        return Response.status(Response.Status.OK).entity(new MsgModel(true, "Cache group" + cacheGroup+ " is stopped successfully!")).build();
+    }
+
+    @PUT
+    @Path("/startCache/{cacheGroup}")
+    public Response startCache(@PathParam("cacheGroup") String cacheGroup) {
+        if(StringUtils.isEmpty(cacheGroup)){
+            LOGGER.warn("cache group name is null or empty!");
+            return Response.status(Response.Status.NOT_FOUND).entity(new MsgModel(false, "Cache group name is null or empty!")).build();
+        }
+        if(CacheConstants.CACHES_SCREENSHOT_CACHE.equals(cacheGroup)){
+            AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
+            changeCacheGroupStatusByName(screenshotCacheManager,CacheStatus.AVAILABLE, cacheGroup);
+
+        }else{
+            AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
+            changeCacheGroupStatusByName(lruCacheManager,CacheStatus.AVAILABLE, cacheGroup);
+        }
+        return Response.status(Response.Status.OK).entity(new MsgModel(true, "All cache groups are started successfully!")).build();
+    }
+
     @PUT
     @Path("/stopCache")
-    public String stopCache() {
+    public Response stopCache() {
         AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
         AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
         //clear all cache groups
         clearAllCacheGroupData(lruCacheManager);
         clearAllCacheGroupData(screenshotCacheManager);
         //reset all cache group statistics data
-        resetCacheStatistics(lruCacheManager);
-        resetCacheStatistics(screenshotCacheManager);
+        resetAllCacheStatistics(lruCacheManager);
+        resetAllCacheStatistics(screenshotCacheManager);
         //close all cache groups
-        changeCacheGroupStatus(lruCacheManager,CacheStatus.CLOSED);
-        changeCacheGroupStatus(screenshotCacheManager,CacheStatus.CLOSED);
-        return "Cache is stopped";
+        changeAllCacheGroupStatus(lruCacheManager,CacheStatus.CLOSED);
+        changeAllCacheGroupStatus(screenshotCacheManager,CacheStatus.CLOSED);
+        return Response.status(Response.Status.OK).entity(new MsgModel(true, "All cache groups are stopped successfully!")).build();
     }
 
     @PUT
     @Path("/startCache")
-    public String startCache() {
+    public Response startCache() {
         AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
-        changeCacheGroupStatus(lruCacheManager,CacheStatus.AVAILABLE);
+        changeAllCacheGroupStatus(lruCacheManager,CacheStatus.AVAILABLE);
         AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
-        changeCacheGroupStatus(screenshotCacheManager,CacheStatus.AVAILABLE);
-        return "Cache is started!";
+        changeAllCacheGroupStatus(screenshotCacheManager,CacheStatus.AVAILABLE);
+        return Response.status(Response.Status.OK).entity(new MsgModel(true, "All cache groups are started successfully!")).build();
     }
 
     @PUT
     @Path("/suspendCache")
-    public String suspendCache() {
+    public Response suspendCache() {
         AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
-        changeCacheGroupStatus(lruCacheManager,CacheStatus.SUSPEND);
+        changeAllCacheGroupStatus(lruCacheManager,CacheStatus.SUSPEND);
         AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
-        changeCacheGroupStatus(screenshotCacheManager,CacheStatus.SUSPEND);
-        return "Cache is suspended!";
+        changeAllCacheGroupStatus(screenshotCacheManager,CacheStatus.SUSPEND);
+        return Response.status(Response.Status.OK).entity(new MsgModel(true, "All cache groups are suspended successfully!")).build();
     }
 
     @PUT
     @Path("/resumeCache")
-    public String resumeCache() {
+    public Response resumeCache() {
         AbstractCacheManager lruCacheManager = LRUCacheManager.getInstance();
-        changeCacheGroupStatus(lruCacheManager,CacheStatus.AVAILABLE);
+        changeAllCacheGroupStatus(lruCacheManager,CacheStatus.AVAILABLE);
         AbstractCacheManager screenshotCacheManager = LRUScreenshotCacheManager.getInstance();
-        changeCacheGroupStatus(screenshotCacheManager,CacheStatus.AVAILABLE);
-        return "Cache is resumed!";
+        changeAllCacheGroupStatus(screenshotCacheManager,CacheStatus.AVAILABLE);
+        return Response.status(Response.Status.OK).entity(new MsgModel(true, "All cache groups are resumed successfully!")).build();
     }
 
-    private void changeCacheGroupStatus(AbstractCacheManager cacheManager, CacheStatus cacheStatus) {
+    private void changeAllCacheGroupStatus(AbstractCacheManager cacheManager, CacheStatus cacheStatus) {
         ConcurrentMap<String, ICache> map = cacheManager.getCacheMap();
         if (map == null) {
             LOGGER.error("CacheMap in CacheManagers is null!");
@@ -81,6 +153,21 @@ public class CacheAPI {
             cache.setCacheStatus(cacheStatus);
             LOGGER.info("Cache group {} status has been changed to {}", cache.getName(), cacheStatus);
         }
+    }
+
+    private void changeCacheGroupStatusByName(AbstractCacheManager cacheManager, CacheStatus cacheStatus, String cacheGroupName) {
+        ConcurrentMap<String, ICache> map = cacheManager.getCacheMap();
+        if (map == null) {
+            LOGGER.error("CacheMap in CacheManagers is null!");
+            return;
+        }
+        LinkedHashMapCache cache = (LinkedHashMapCache) map.get(cacheGroupName);
+        if(cache == null){
+            LOGGER.error("Cache group named {} is not existed!", cacheGroupName);
+            return ;
+        }
+        cache.setCacheStatus(cacheStatus);
+        LOGGER.info("Cache group {} status has been changed to {}", cacheGroupName, cacheStatus);
     }
 
     private void clearAllCacheGroupData(AbstractCacheManager cacheManager){
@@ -95,8 +182,22 @@ public class CacheAPI {
             LOGGER.info("Cache group {} data has been cleared!", cache.getName());
         }
     }
+    private void clearCacheGroupDataByName(AbstractCacheManager cacheManager, String cacheGroupName){
+        ConcurrentMap<String, ICache> map = cacheManager.getCacheMap();
+        if (map == null) {
+            LOGGER.error("CacheMap in CacheManagers is null!");
+            return;
+        }
+        LinkedHashMapCache cache = (LinkedHashMapCache) map.get(cacheGroupName);
+        if(cache == null){
+            LOGGER.error("Cache group named {} is not existed!", cacheGroupName);
+            return ;
+        }
+        LOGGER.info("Cache group named {} data has been cleared!", cache.getName());
+        cache.clear();
+    }
 
-    private void resetCacheStatistics(AbstractCacheManager cacheManager){
+    private void resetAllCacheStatistics(AbstractCacheManager cacheManager){
         ConcurrentMap<String, ICache> map = cacheManager.getCacheMap();
         if (map == null) {
             LOGGER.error("CacheMap in CacheManagers is null!");
@@ -106,6 +207,50 @@ public class CacheAPI {
             AbstractCache cache = (AbstractCache) value;
             cache.getCacheCounter().reset();
             LOGGER.info("Cache group {} data has been cleared!", cache.getName());
+        }
+    }
+
+    private void resetCacheStatisticsByName(AbstractCacheManager cacheManager, String cacheGroupName){
+        ConcurrentMap<String, ICache> map = cacheManager.getCacheMap();
+        if (map == null) {
+            LOGGER.error("CacheMap in CacheManagers is null!");
+            return;
+        }
+        AbstractCache cache = (AbstractCache) map.get(cacheGroupName);
+        if(cache == null){
+            LOGGER.error("Cache group named {} is not existed!", cacheGroupName);
+            return ;
+        }
+        cache.getCacheCounter().reset();
+        LOGGER.info("Cache group {} data has been cleared!", cache.getName());
+    }
+
+
+
+    class MsgModel{
+
+        public MsgModel(boolean success, String msg) {
+            this.success = success;
+            this.msg = msg;
+        }
+
+        private boolean success;
+        private String msg;
+
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public void setSuccess(boolean success) {
+            this.success = success;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public void setMsg(String msg) {
+            this.msg = msg;
         }
     }
 }
