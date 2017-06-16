794cdffefb8adde3895f8ee0ec13b5d7bd00c419
Shangping Wang <shangping.wang@oracle.com>
2017-06-15 20:47:37 -0700

    fix common api

--- dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/impl/GlobalContextUtil_1180.java
+++ dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/impl/GlobalContextUtil_1180.java
@@ -10,7 +10,17 @@
 
 package oracle.sysman.emaas.platform.dashboards.tests.ui.impl;
 
-public class GlobalContextUtil_1180 extends GlobalContextUtil_1170
-{
+import org.testng.Assert;
 
+import oracle.sysman.qatool.uifwk.webdriver.WebDriver;
+
+public class GlobalContextUtil_1180 extends GlobalContextUtil_1170
+{	
+	@Override
+	public String getGlobalContextName(WebDriver driver)
+	{
+		Assert.assertTrue(false, "This method is not available in the current version, please use EntitySelectorUtil.getPillContents(WebDriver driver, Logger logger)");
+		driver.getLogger().info("Method not available in the current version");
+		return "";
+	}
 }
