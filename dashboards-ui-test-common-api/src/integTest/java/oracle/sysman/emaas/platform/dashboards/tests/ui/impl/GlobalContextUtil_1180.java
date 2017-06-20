bdbe3e8b2a5cf48a3c94905174c1f93f961e31fc
Shangping Wang <shangping.wang@oracle.com>
2017-06-19 23:17:30 -0700

    improve the common api to avoid breaking dependent lrgs

--- dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/impl/GlobalContextUtil_1180.java
+++ dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/impl/GlobalContextUtil_1180.java
@@ -10,8 +10,11 @@
 
 package oracle.sysman.emaas.platform.dashboards.tests.ui.impl;
 
+import java.util.logging.Logger;
+
 import org.testng.Assert;
 
+import oracle.sysman.emaas.platform.dashboards.tests.ui.EntitySelectorUtil;
 import oracle.sysman.qatool.uifwk.webdriver.WebDriver;
 
 public class GlobalContextUtil_1180 extends GlobalContextUtil_1170
@@ -19,8 +22,10 @@ public class GlobalContextUtil_1180 extends GlobalContextUtil_1170
 	@Override
 	public String getGlobalContextName(WebDriver driver)
 	{
-		Assert.assertTrue(false, "This method is not available in the current version, please use EntitySelectorUtil.getPillContents(WebDriver driver, Logger logger)");
-		driver.getLogger().info("Method not available in the current version");
-		return "";
+		//Assert.assertTrue(false, "This method is not available in the current version, please use EntitySelectorUtil.getPillContents(WebDriver driver, Logger logger)");
+		driver.getLogger().warning("This method not supported in the current version, please use EntitySelectorUtil.getPillContents(WebDriver driver, Logger logger)");
+		Logger logger = driver.getLogger();
+				
+		return EntitySelectorUtil.getPillContents(driver, logger).get(0);
 	}
 }
