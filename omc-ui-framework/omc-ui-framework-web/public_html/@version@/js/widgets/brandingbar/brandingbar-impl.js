3d2cfa359225e0f5d4ed2073b2bd3f13e63ccbe8
Roberto Trevino <roberto.trevino@oracle.com>
2017-06-21 10:06:30 -0700

    return event listener reference, only close topology if ude has not yet landed

--- omc-ui-framework/omc-ui-framework-web/public_html/@version@/js/widgets/brandingbar/brandingbar-impl.js
+++ omc-ui-framework/omc-ui-framework-web/public_html/@version@/js/widgets/brandingbar/brandingbar-impl.js
@@ -1531,13 +1531,15 @@ define('uifwk/@version@/js/widgets/brandingbar/brandingbar-impl', [
                     //show enterprise topology
                     self.showEnterpriseTopology = true;
                     window.globalpillsempty = true;
-                    if (self.isTopologyDisplayed() && !self.topologyDisabled()) {
-                        self.showTopology();
-                    }
                     if (self.udeEnterPriseTopologyLanded) {
                         self.topologyDisabled(false);
                     }
                     else {
+                        // only hide the topology(when displayed)
+                        //if ude enterprise topology has not yet landed
+                        if (self.isTopologyDisplayed() && !self.topologyDisabled()) {
+                            self.showTopology();
+                        }
                         self.topologyDisabled(true);
                     }
 
