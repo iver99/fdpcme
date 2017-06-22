7f7d1f040b2fd09d961153f18ed5293bec9f92f4
rex.liang <rex.liang@oracle.com>
2017-06-22 00:55:04 -0700

    Revert EMCPDF-3901: don't need to separate and delete the SQL for the Service who has cut over to use new OOB API

--- dashboards-schema/src/main/sql/1.12.0/emaas_dashboards_seed_data.sql
+++ dashboards-schema/src/main/sql/1.12.0/emaas_dashboards_seed_data.sql
@@ -5,7 +5,7 @@ Rem Copyright (c) 2013, 2014, 2015, 2016, Oracle and/or its affiliates.
 Rem All rights reserved.
 Rem
 Rem    NAME
-Rem      emaas_dashboards_seed_data_ita.sql
+Rem      emaas_dashboards_seed_data.sql 
 Rem
 Rem    DESCRIPTION
 Rem      Seed data for TAUPDATE
--- dashboards-schema/src/main/sql/1.7.5/upgrade_impl_dml.sql
+++ dashboards-schema/src/main/sql/1.7.5/upgrade_impl_dml.sql
@@ -2,6 +2,6 @@ DEFINE TENANT_ID = '&1'
 
 REM --update OOB data
 
-@&EMSAAS_SQL_ROOT/1.7.5/emaas_dashboards_seed_data_ita.sql '&TENANT_ID'
+@&EMSAAS_SQL_ROOT/1.7.5/emaas_dashboards_seed_data.sql '&TENANT_ID'
 
 COMMIT;
