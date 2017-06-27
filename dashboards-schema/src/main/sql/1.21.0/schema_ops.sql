a3d90d6efa7ae9f5c2aa534ea34ce69cbe57bc44
rex.liang <rex.liang@oracle.com>
2017-06-26 22:35:37 -0700

    EMCPDF-2879: change the related SQL's version

--- dashboards-schema/src/main/sql/1.21.0/schema_ops.sql
+++ dashboards-schema/src/main/sql/1.21.0/schema_ops.sql
@@ -2,11 +2,11 @@ Rem ----------------------------------------------------------------
 Rem 02/03/2017	MIAYU	Created file
 Rem ----------------------------------------------------------------
 
-@&EMSAAS_SQL_ROOT/1.20.0/upgrade_impl_ddl.sql
+@&EMSAAS_SQL_ROOT/1.21.0/upgrade_impl_ddl.sql
 
 SET HEADING OFF
 SET FEEDBACK OFF
 SET LINESIZE 2000
 
-@&EMSAAS_SQL_ROOT/1.20.0/upgrade_impl_dml.sql
+@&EMSAAS_SQL_ROOT/1.21.0/upgrade_impl_dml.sql
 
