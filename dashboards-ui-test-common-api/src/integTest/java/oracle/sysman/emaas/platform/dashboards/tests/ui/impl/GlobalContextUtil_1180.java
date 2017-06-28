1810af1babf6ec3d9ad1fac689332a283c9cc956
Shangping Wang <shangping.wang@oracle.com>
2017-06-27 18:04:42 -0700

    fix common api

--- dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/impl/GlobalContextUtil_1180.java
+++ dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/impl/GlobalContextUtil_1180.java
@@ -16,6 +16,7 @@ import java.util.logging.Logger;
 import org.testng.Assert;
 
 import oracle.sysman.emaas.platform.dashboards.tests.ui.EntitySelectorUtil;
+import oracle.sysman.emaas.platform.dashboards.tests.ui.util.DashBoardPageId;
 import oracle.sysman.qatool.uifwk.webdriver.WebDriver;
 
 public class GlobalContextUtil_1180 extends GlobalContextUtil_1170
@@ -23,20 +24,29 @@ public class GlobalContextUtil_1180 extends GlobalContextUtil_1170
 	@Override
 	public String getGlobalContextName(WebDriver driver)
 	{
-		//Assert.assertTrue(false, "This method is not available in the current version, please use EntitySelectorUtil.getPillContents(WebDriver driver, Logger logger)");
-		driver.getLogger().warning("This method not supported in the current version, please use EntitySelectorUtil.getPillContents(WebDriver driver, Logger logger)");
 		Logger logger = driver.getLogger();
 		
-		ArrayList<String> gcname = EntitySelectorUtil.getPillContents(driver, logger);
-		
-		if (gcname.size()>0)
+		if (!EntitySelectorUtil.validateReadOnlyMode(driver, logger))
 		{
-			driver.getLogger().info("return the first global context pill: " + gcname.get(0));
-			return gcname.get(0);
+			//Assert.assertTrue(false, "This method is not available in the current version, please use EntitySelectorUtil.getPillContents(WebDriver driver, Logger logger)");
+			driver.getLogger().warning("This method not supported in the current version, please use EntitySelectorUtil.getPillContents(WebDriver driver, Logger logger)");
+		
+			ArrayList<String> gcname = EntitySelectorUtil.getPillContents(driver, logger);
+			
+			if (gcname.size()>0)
+			{
+				driver.getLogger().info("return the first global context pill: " + gcname.get(0));
+				return gcname.get(0);
+			}
+			else
+			{
+				return "";
+			}
 		}
 		else
 		{
-			return "";
-		}		
+			driver.getLogger().info("It is global context read only mode");
+			return driver.getText(DashBoardPageId.EntSelReadOnlyPill);
+		}
 	}
 }
