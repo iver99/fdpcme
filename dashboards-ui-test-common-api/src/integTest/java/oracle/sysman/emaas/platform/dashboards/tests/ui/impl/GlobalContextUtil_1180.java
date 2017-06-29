4af48c30faf21805ce792e8142c11001a15b292d
Shangping Wang <shangping.wang@oracle.com>
2017-06-28 19:17:02 -0700

    fix EMCPDF-4286

--- dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/impl/GlobalContextUtil_1180.java
+++ dashboards-ui-test-common-api/src/integTest/java/oracle/sysman/emaas/platform/dashboards/tests/ui/impl/GlobalContextUtil_1180.java
@@ -10,7 +10,36 @@
 
 package oracle.sysman.emaas.platform.dashboards.tests.ui.impl;
 
+import oracle.sysman.qatool.uifwk.webdriver.WebDriver;
+
 public class GlobalContextUtil_1180 extends GlobalContextUtil_1170
 {
-
+	public static final String GLBCTXTBUTTON = "//div[@id='topologyButtonWrapper']/span/label";
+	/* (non-Javadoc)
+	 * @see oracle.sysman.emaas.platform.dashboards.tests.ui.util.IGlobalContextUtil#showTopology(oracle.sysman.qatool.uifwk.webdriver.WebDriver)
+	 */
+	@Override
+	public void showTopology(WebDriver driver)
+	{
+		if (driver.isDisplayed(GLBCTXTPLDIV)) {
+			driver.getLogger().info("the topology already got displayed");
+		}
+		else {
+			driver.click(GLBCTXTBUTTON);
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see oracle.sysman.emaas.platform.dashboards.tests.ui.util.IGlobalContextUtil#hideTopology(oracle.sysman.qatool.uifwk.webdriver.WebDriver)
+	 */
+	@Override
+	public void hideTopology(WebDriver driver)
+	{
+		if (driver.isDisplayed(GLBCTXTPLDIV)) {
+			driver.click(GLBCTXTBUTTON);
+		}
+		else {
+			driver.getLogger().info("the topology already got hidden");
+		}
+	}
 }
