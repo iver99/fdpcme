0dedd03af261b49a4407f5f0219b73cc5c41e5f5
chehao <chendong.h.hao@oracle.com>
2017-07-05 17:29:27 +0800

    EMCPDF-4447 fix ajust log

--- dashboards-sdk/src/main/java/oracle/sysman/emaas/platform/emcpdf/tenant/TenantSubscriptionUtil.java
+++ dashboards-sdk/src/main/java/oracle/sysman/emaas/platform/emcpdf/tenant/TenantSubscriptionUtil.java
@@ -116,10 +116,14 @@ public class TenantSubscriptionUtil {
                     rc.setHeader("X-USER-IDENTITY-DOMAIN-NAME", tenant);
                     appsResponse = rc.getWithException(queryHref, tenant, lookupLink.getAuthToken());
                 } catch (UniformInterfaceException e) {
-                    if (e.getResponse() != null && e.getResponse().getStatus() == 503) {
-                        LOGGER.error("Got status code {} when getting tenant {} subscribed apps", e.getResponse().getStatus(), tenant);
-                        LOGGER.error(e);
-                        throw new RetryableLookupClient.RetryableLookupException(e);
+                    // will print HTTP response code when response is not null.
+                    if (e.getResponse() != null) {
+                        LOGGER.error("Got HTTP response code {} when getting tenant {} subscribed apps", e.getResponse().getStatus(), tenant);
+                        //only resp code 503 will trigger retry logic.
+                        if(e.getResponse().getStatus() == 503){
+                            LOGGER.error(e);
+                            throw new RetryableLookupClient.RetryableLookupException(e);
+                        }
                     }
                     LOGGER.error(e);
                     throw e;
