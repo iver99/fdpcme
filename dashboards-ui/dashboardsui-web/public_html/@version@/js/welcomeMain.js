36cf60102c374c82fb9185aa2668c90624e5e558
qiqia <qiqia>
2017-07-14 15:03:54 +0800

    fix NLS scanned hardcode issues

--- dashboards-ui/dashboardsui-web/public_html/@version@/js/welcomeMain.js
+++ dashboards-ui/dashboardsui-web/public_html/@version@/js/welcomeMain.js
@@ -496,6 +496,7 @@ require(['ojs/ojcore',
             }
 
             $(document).ready(function () {
+                ko.applyBindings({LOADING : getNlsString('DBS_HOME_LOADING')}, $('#loading')[0]);  //to make text binding on loading work
                 dfu.getSubscribedApps2WithEdition(function(apps) {
                     if (apps && (!apps.applications || apps.applications.length === 0)) {
                         oj.Logger.error("Tenant subscribes to no service. Redirect to dashboard error page", true);
