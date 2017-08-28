5d4c76f42fe163eaccff27923bba65f236397d2e
Jiangxia Nan <jiangxia.nan@oracle.com>
2017-08-27 22:53:06 -0700

    EMCPDF-4757: 'Use time range on dashboard page' changes GC time

--- dashboards-ui/dashboardsui-web/public_html/@version@/js/builder/builder.functions.js
+++ dashboards-ui/dashboardsui-web/public_html/@version@/js/builder/builder.functions.js
@@ -820,7 +820,6 @@ define(['knockout',
                                 if (TargetSelectorUtils) {
                                     TargetSelectorUtils.registerComponents();
                                 }
-                                ctxUtil.setOMCContext(omcContext);
                                 $.when(TargetSelectorUtils.getCriteriaFromOmcContext()).done(function (criteria) {
                                     entityContext = {criteria: criteria};
                                     dtd.resolve(entityContext);
