9ad94244a016c9946c30830df6b3164298e52100
chehao <chendong.h.hao@oracle.com>
2017-09-23 17:51:19 +0800

    fix response sync pattern

--- dashboards-comparator/dashboardscomparator-web/src/main/java/oracle/sysman/emaas/platform/dashboards/comparator/ws/rest/ZDTAPI.java
+++ dashboards-comparator/dashboardscomparator-web/src/main/java/oracle/sysman/emaas/platform/dashboards/comparator/ws/rest/ZDTAPI.java
@@ -382,8 +382,8 @@ public class ZDTAPI
 				return Response.status(Status.INTERNAL_SERVER_ERROR).entity(JsonUtil.buildNormalMapper().toJson(new ErrorEntity(ZDTErrorConstants.FAIL_TO_SYNC_ERROR_CODE, ZDTErrorConstants.FAIL_TO_SYNC_ERROR_MESSAGE))).build();
 			}
 			JSONObject object = new JSONObject();
-			object.put(dcc.getKey1(), message1.contains("successful")?"Sync is successful!":"");
-			object.put(dcc.getKey2(), message2.contains("successful")?"Sync is successful!":"");
+			object.put(dcc.getKey1(), message1);
+			object.put(dcc.getKey2(), message2);
 			return Response.ok(object).build();
 	    }catch (Exception e) {
 			logger.error(e);
